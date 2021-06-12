b1920ade2416d4312588ea932d9b16742d6b603a
Sam Wilson <sam.wilson@mesh.xyz>
2021-06-12 17:21:51 -0400

    Little bit of packaging clean-up

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-import pathlib
-
 import setuptools
 
-here = pathlib.Path(__file__).parent.resolve()
-
-long_description = (here / "README.md").read_text(encoding="utf-8")
-
-setuptools.setup(
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-)
+setuptools.setup()
