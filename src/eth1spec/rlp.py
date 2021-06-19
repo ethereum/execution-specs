c4213dc336ae6c6c1cdc7702cf256cda3157ca8b
Somu Bhargava <somubhargava97@gmail.com>
2021-06-19 17:18:47 +0530

    Make rlp module more readable and organized.         - Also fixes potential bugs in rlp module         - Add tests and integrate with ethereum test fixtures

--- src/eth1spec/rlp.py
+++ src/eth1spec/rlp.py
@@ -14,270 +14,338 @@ Defines the serialization and deserialization format used throughout Ethereum.
 
 from __future__ import annotations
 
-from typing import List, Sequence, Tuple, Union, cast
+from typing import List, Sequence, Union, cast
 
 from .base_types import U256, Uint
 from .eth_types import Bytes
 
-verbose = False
-debug = False
-
-
 RLP = Union[Bytes, Uint, U256, Sequence["RLP"]]  # type: ignore
 
 
-def encode(x: RLP) -> Bytes:
+#
+# RLP Encode
+#
+
+
+def encode(raw_data: RLP) -> Bytes:
     """
-    Encodes `x` into a sequence of bytes using RLP.
+    Encodes `raw_data` into a sequence of bytes using RLP.
 
     Parameters
     ----------
-    x : `RLP`
-        A `Bytes`, `Uint`, or sequence of `RLP` encodable objects.
+    raw_data : `RLP`
+        A `Bytes`, `Uint`, `Uint256` or sequence of `RLP` encodable
+        objects.
 
     Returns
     -------
     encoded : `eth1spec.eth_types.Bytes`
-        The RLP encoded bytes representing `x`.
+        The RLP encoded bytes representing `raw_data`.
     """
-    if verbose:
-        print("RLP(", x, ")", "type: ", type(x))
-    if isinstance(x, (bytearray, bytes)):
-        return R_b(x)
-    elif isinstance(x, (Uint, U256)):
-        return encode(x.to_be_bytes())
-    elif isinstance(x, Sequence):
-        return R_l(cast(Sequence[RLP], x))
+    if isinstance(raw_data, (bytearray, bytes)):
+        return encode_bytes(raw_data)
+    elif isinstance(raw_data, (Uint, U256)):
+        return encode(raw_data.to_be_bytes())
+    elif isinstance(raw_data, Sequence) and not isinstance(raw_data, str):
+        return encode_sequence(cast(Sequence[RLP], raw_data))
     else:
-        raise TypeError()
+        raise TypeError(
+            "RLP Encoding of type {} is not supported".format(type(raw_data))
+        )
 
 
-# binary encoding/decoding
-def R_b(x: Bytes) -> Bytes:
+def encode_bytes(raw_bytes: Bytes) -> Bytes:
     """
-    Encodes `x`, a sequence of bytes, using RLP.
+    Encodes `raw_bytes`, a sequence of bytes, using RLP.
 
     Parameters
     ----------
-    x : `eth1spec.eth_types.Bytes`
+    raw_bytes : `eth1spec.eth_types.Bytes`
         Bytes to encode with RLP.
 
     Returns
     -------
     encoded : `eth1spec.eth_types.Bytes`
-        The RLP encoded bytes representing `x`.
+        The RLP encoded bytes representing `raw_bytes`.
     """
-    if verbose:
-        print("R_b(", x, ")")
-    len_x = Uint(len(x))
-    if len_x == 1 and x[0] < 128:
-        return x  # bytearray([x[0] + 0x80]) # noqa: SC100
-    elif len_x < 56:
-        return bytearray([128 + len_x]) + x
+    len_raw_data = Uint(len(raw_bytes))
+
+    if len_raw_data == 1 and raw_bytes[0] < 128:
+        return raw_bytes
+    elif len_raw_data < 56:
+        return bytearray([128 + len_raw_data]) + raw_bytes
     else:
-        big_endian = len_x.to_be_bytes()
-        return bytearray([183 + len(big_endian)]) + big_endian + x
+        # length of raw data represented as big endian bytes
+        len_raw_data_as_be = len_raw_data.to_be_bytes()
+        return (
+            bytearray([183 + len(len_raw_data_as_be)])
+            + len_raw_data_as_be
+            + raw_bytes
+        )
 
 
-# list encoding/decoding
-def R_l(x: Sequence[RLP]) -> Bytes:
+def encode_sequence(raw_sequence: Sequence[RLP]) -> Bytes:
     """
-    Encodes `x`, a list of RLP encodable objects, using RLP.
+    Encodes a sequence of RLP encodable objects (`raw_sequence`) using RLP.
 
     Parameters
     ----------
-    x : `List[RLP]`
-        List to encode with RLP.
+    raw_sequence : `Sequence[RLP]`
+            Sequence of RLP encodable objects.
 
     Returns
     -------
     encoded : `eth1spec.eth_types.Bytes`
-        The RLP encoded bytes representing `x`.
+        The RLP encoded bytes representing `raw_sequence`.
     """
-    if verbose:
-        print("R_l(", x, ")")
-    partial_x = s(x)
-    len_partial_x = Uint(len(partial_x))
-    if len_partial_x < 56:
-        return bytearray([192 + len_partial_x]) + partial_x
+    joined_encodings = get_joined_encodings(raw_sequence)
+    len_joined_encodings = Uint(len(joined_encodings))
+
+    if len_joined_encodings < 56:
+        return bytearray([192 + len_joined_encodings]) + joined_encodings
     else:
-        big_endian = len_partial_x.to_be_bytes()
-        return bytearray([247 + len(big_endian)]) + big_endian + partial_x
+        len_joined_encodings_as_be = len_joined_encodings.to_be_bytes()
+        return (
+            bytearray([247 + len(len_joined_encodings_as_be)])
+            + len_joined_encodings_as_be
+            + joined_encodings
+        )
 
 
-# for a list, recursively call RLP or RLP_inverse
-def s(x: Sequence[RLP]) -> Bytes:
+def get_joined_encodings(raw_sequence: Sequence[RLP]) -> Bytes:
     """
-    Partially encodes `x`, a list of RLP encodable objects, using RLP,
-    excluding the length prefix.
+    Obtain concatenation of rlp encoding for each item in the sequence
+    raw_sequence.
 
     Parameters
     ----------
-    x : `List[RLP]`
-        List to encode with RLP.
+    raw_sequence : `Sequence[RLP]`
+        Sequence to encode with RLP.
 
     Returns
     -------
-    partially_encoded : `eth1spec.eth_types.Bytes`
-        The partially RLP encoded bytes representing `x`.
+    joined_encodings : `eth1spec.eth_types.Bytes`
+        The concatenated RLP encoded bytes for each item in sequence
+        raw_sequence.
     """
-    if verbose:
-        print("s(", x, ")")
-    partially_encoded = bytearray([])
-    for xi in x:
-        partially_encoded += encode(xi)
-    return partially_encoded
+    joined_encodings = bytearray()
+    for item in raw_sequence:
+        joined_encodings += encode(item)
+
+    return joined_encodings
 
 
-# inverses of above
+#
+# RLP Decode
+#
 
 
-def decode(b: Bytes) -> RLP:
+def decode(encoded_data: Bytes) -> RLP:
     """
-    Decodes an integer, byte sequence, or list of RLP encodable objects from
-    the byte sequence `b`, using RLP.
+    Decodes an integer, byte sequence, or list of RLP encodable objects
+    from the byte sequence `encoded_data`, using RLP.
 
     Parameters
     ----------
-    b : `eth1spec.eth_types.Bytes`
+    encoded_data : `eth1spec.eth_types.Bytes`
         A sequence of bytes, in RLP form.
 
     Returns
     -------
-    decoded : `RLP`
-        Object decoded from `b`.
+    decoded_data : `RLP`
+        Object decoded from `encoded_data`.
     """
-    if verbose:
-        print("RLP_inverse(", b.hex(), ")")
-    if len(b) == 0:
-        return bytearray([0x80])
-    if b[0] < 0xC0:  # bytes
-        return R_b_inverse(b)
+    # Raising error as there can never be empty encoded data for any
+    # given raw data (including empty raw data)
+    # RLP Encoding(b'') -> [128]
+    # RLP Encoding([]) -> [192]
+    assert len(encoded_data) > 0
+
+    if encoded_data[0] <= 191:
+        # This means that the raw data is of type bytes
+        return decode_to_bytes(encoded_data)
     else:
-        return R_l_inverse(b)
+        # This means that the raw data is of type sequence
+        return decode_to_sequence(encoded_data)
 
 
-def R_b_inverse(b: Bytes) -> Bytes:
+def decode_to_bytes(encoded_bytes: Bytes) -> Bytes:
     """
-    Decodes a byte sequence from the byte sequence `b`, using RLP.
+    Decodes a rlp encoded byte stream assuming that the decoded data
+    should be of type `bytes`.
 
     Parameters
     ----------
-    b : `eth1spec.eth_types.Bytes`
-        A sequence of bytes, in RLP form.
+    encoded_bytes : `eth1spec.eth_types.Bytes`
+        RLP encoded byte stream.
 
     Returns
     -------
     decoded : `eth1spec.eth_types.Bytes`
-        Bytes decoded from `b`
+        RLP decoded Bytes data
     """
-    if verbose:
-        print("R_b_inverse(", b.hex(), ")")
-    if len(b) == 1 and b[0] < 0x80:
-        return b  # bytearray([b[0]-0x80]) # noqa: SC100
-    elif b[0] <= 0xB7:
-        return b[1 : 1 + b[0] - 0x80]
+    if len(encoded_bytes) == 1 and encoded_bytes[0] < 128:
+        return encoded_bytes
+    elif encoded_bytes[0] <= 183:
+        len_raw_data = encoded_bytes[0] - 128
+        assert len_raw_data < len(encoded_bytes)
+        raw_data = encoded_bytes[1 : 1 + len_raw_data]
+        assert not (len_raw_data == 1 and raw_data[0] < 128)
+        return raw_data
     else:
-        length_len = b[0] - 183
-        len_x = Uint.from_be_bytes(b[1 : length_len + 1])
-        return b[length_len + 1 : length_len + 1 + len_x]
+        # This is the index in the encoded data at which decoded data
+        # starts from.
+        decoded_data_start_idx = 1 + encoded_bytes[0] - 183
+        assert decoded_data_start_idx - 1 < len(encoded_bytes)
+        # Expectation is that the big endian bytes shouldn't start with 0
+        # while trying to decode using RLP, in which case is an error.
+        assert encoded_bytes[1] != 0
+        len_decoded_data = Uint.from_be_bytes(
+            encoded_bytes[1:decoded_data_start_idx]
+        )
+        assert len_decoded_data >= 56
+        decoded_data_end_idx = decoded_data_start_idx + len_decoded_data
+        assert decoded_data_end_idx - 1 < len(encoded_bytes)
+        return encoded_bytes[decoded_data_start_idx:decoded_data_end_idx]
 
 
-def R_l_inverse(b: Bytes) -> List[RLP]:
+def decode_to_sequence(encoded_sequence: Bytes) -> List[RLP]:
     """
-    Decodes `b` into a list of RLP encodable objects, using RLP.
+    Decodes a rlp encoded byte stream assuming that the decoded data
+    should be of type `Sequence` of objects.
 
     Parameters
     ----------
-    b : `eth1spec.eth_types.Bytes`
-        An RLP encoded list.
+    encoded_sequence : `eth1spec.eth_types.Bytes`
+        An RLP encoded Sequence.
 
     Returns
     -------
-    decoded : `List[RLP]`
-        List of objects decoded from `b`.
+    decoded : `Sequence[RLP]`
+        Sequence of objects decoded from `encoded_sequence`.
     """
-    if verbose:
-        print("R_l_inverse(", b.hex(), ")")
-    if b[0] <= 0xF7:
-        len_partial_x = b[0] - 0xC0
-        partial_x = b[1 : 1 + len_partial_x]
+    if encoded_sequence[0] <= 247:
+        len_joined_encodings = encoded_sequence[0] - 192
+        assert len_joined_encodings < len(encoded_sequence)
+        joined_encodings = encoded_sequence[1 : 1 + len_joined_encodings]
     else:
-        len_len_partial_x = b[0] - 247
-        len_partial_x = Uint.from_be_bytes(b[1 : 1 + len_len_partial_x])
-        partial_x = b[
-            1 + len_len_partial_x : 1 + len_len_partial_x + len_partial_x
+        joined_encodings_start_idx = 1 + encoded_sequence[0] - 247
+        assert joined_encodings_start_idx - 1 < len(encoded_sequence)
+        # Expectation is that the big endian bytes shouldn't start with 0
+        # while trying to decode using RLP, in which case is an error.
+        assert encoded_sequence[1] != 0
+        len_joined_encodings = Uint.from_be_bytes(
+            encoded_sequence[1:joined_encodings_start_idx]
+        )
+        assert len_joined_encodings >= 56
+        joined_encodings_end_idx = (
+            joined_encodings_start_idx + len_joined_encodings
+        )
+        assert joined_encodings_end_idx - 1 < len(encoded_sequence)
+        joined_encodings = encoded_sequence[
+            joined_encodings_start_idx:joined_encodings_end_idx
         ]
-    return s_inverse(partial_x)
+
+    return decode_joined_encodings(joined_encodings)
 
 
-def s_inverse(b: Bytes) -> List[RLP]:
+def decode_joined_encodings(joined_encodings: Bytes) -> List[RLP]:
     """
-    Decodes `b`, a partially encoded list of RLP encodable objects, excluding
-    the length prefix.
+    Decodes `joined_encodings`, which is a concatenation of RLP encoded
+    objects.
 
     Parameters
     ----------
-    b : `eth1spec.eth_types.Bytes`
-        A list of objects, encoded in RLP, without the length prefix.
+    joined_encodings : `eth1spec.eth_types.Bytes`
+        concatenation of RLP encoded objects
 
     Returns
     -------
     decoded : `List[RLP]`
-        A list of objects decoded from `b`.
+        A list of objects decoded from `joined_encodings`.
     """
-    if verbose:
-        print("s_inverse(", b.hex(), ")")
-    x = []
-    i = 0
-    len_ = len(b)
-    while i < len_:
-        len_cur, len_len_cur = decode_length(b[i:])
-        x += [decode(b[i : i + len_len_cur + len_cur])]
-        i += len_cur + len_len_cur
-        if debug:
-            print("  s_inverse() returning", x)
-    if debug:
-        print("  s_inverse() returning", x)
-    return x
+    decoded_sequence = []
+
+    item_start_idx = 0
+    while item_start_idx < len(joined_encodings):
+        encoded_item_length = decode_item_length(
+            joined_encodings[item_start_idx:]
+        )
+        assert item_start_idx + encoded_item_length - 1 < len(joined_encodings)
+        encoded_item = joined_encodings[
+            item_start_idx : item_start_idx + encoded_item_length
+        ]
+        decoded_sequence.append(decode(encoded_item))
+        item_start_idx += encoded_item_length
+
+    return decoded_sequence
 
 
-# this is a helper function not described in the spec
-# but the spec does not discuss the inverse to he RLP function, so never has
-# the opportunity to discuss this returns the length of an encoded rlp object
-def decode_length(b: Bytes) -> Tuple[Uint, Uint]:
+def decode_item_length(encoded_data: Bytes) -> int:
     """
-    Decodes the length prefix from the byte sequence `b`.
+    Find the length of the rlp encoding for the first object in the
+    encoded sequence.
+    Here `encoded_data` refers to concatenation of rlp encoding for each
+    item in a sequence.
+
+    NOTE - This is a helper function not described in the spec. It was
+    introduced as the spec doesn't discuss about decoding the RLP encoded
+    data.
 
     Parameters
     ----------
-    b : `eth1spec.eth_types.Bytes`
-        Sequence of RLP bytes.
+    encoded_data : `eth1spec.eth_types.Bytes`
+        RLP encoded data for a sequence of objects.
 
     Returns
     -------
-    rlp_length : `eth1spec.base_types.Uint`
-        TODO
-    length_length : `eth1spec.base_types.Uint`
-        TODO
+    rlp_length : `int`
     """
-    if verbose:
-        print("length_inverse(", b.hex(), ")")
-    if len(b) == 0:
-        return Uint(0), Uint(0)  # TODO: this may be an error
+    # Can't decode item length for empty encoding
+    assert len(encoded_data) > 0
+
+    first_rlp_byte = Uint(encoded_data[0])
+
+    # This is the length of the big endian representation of the length of
+    # rlp encoded object byte stream.
     length_length = Uint(0)
-    first_rlp_byte = Uint(b[0])
-    if first_rlp_byte < 0x80:
-        rlp_length = Uint(1)
-        return rlp_length, length_length
-    elif first_rlp_byte <= 0xB7:
-        rlp_length = first_rlp_byte - 0x80
-    elif first_rlp_byte <= 0xBF:
-        length_length = first_rlp_byte - 0xB7
-        rlp_length = Uint.from_be_bytes(b[1 : 1 + length_length])
-    elif first_rlp_byte <= 0xF7:
-        rlp_length = first_rlp_byte - 0xC0
-    elif first_rlp_byte <= 0xBF:
-        length_length = first_rlp_byte - 0xB7
-        rlp_length = Uint.from_be_bytes(b[1 : 1 + length_length])
-    return rlp_length, Uint(1 + length_length)
+    decoded_data_length = 0
+
+    # This occurs only when the raw_data is a single byte whose value < 128
+    if first_rlp_byte < 128:
+        # We return 1 here, as the end formula
+        # 1 + length_length + decoded_data_length would be invalid for
+        # this case.
+        return 1
+    # This occurs only when the raw_data is a byte stream with length < 56
+    # and doesn't fall into the above cases
+    elif first_rlp_byte <= 183:
+        decoded_data_length = first_rlp_byte - 128
+    # This occurs only when the raw_data is a byte stream and doesn't fall
+    # into the above cases
+    elif first_rlp_byte <= 191:
+        length_length = first_rlp_byte - 183
+        assert length_length < len(encoded_data)
+        # Expectation is that the big endian bytes shouldn't start with 0
+        # while trying to decode using RLP, in which case is an error.
+        assert encoded_data[1] != 0
+        decoded_data_length = Uint.from_be_bytes(
+            encoded_data[1 : 1 + length_length]
+        )
+    # This occurs only when the raw_data is a sequence of objects with
+    # length(concatenation of encoding of each object) < 56
+    elif first_rlp_byte <= 247:
+        decoded_data_length = first_rlp_byte - 192
+    # This occurs only when the raw_data is a sequence of objects and
+    # doesn't fall into the above cases.
+    elif first_rlp_byte <= 255:
+        length_length = first_rlp_byte - 247
+        assert length_length < len(encoded_data)
+        # Expectation is that the big endian bytes shouldn't start with 0
+        # while trying to decode using RLP, in which case is an error.
+        assert encoded_data[1] != 0
+        decoded_data_length = Uint.from_be_bytes(
+            encoded_data[1 : 1 + length_length]
+        )
+
+    return 1 + length_length + decoded_data_length
---
+++ tests/test_rlp.py
@@ -0,0 +1,361 @@
+import json
+import os
+from typing import List, Sequence, Tuple, Union
+
+import pytest
+
+from eth1spec import rlp
+from eth1spec.eth_types import Bytes, Uint
+from eth1spec.rlp import RLP
+
+from .helpers import hex2bytes
+
+#
+# Tests for RLP decode
+#
+
+
+@pytest.mark.parametrize(
+    "raw_data, expected_encoded_data",
+    [
+        # Empty raw data
+        (b"", bytearray([128])),
+        (bytearray(), bytearray([128])),
+        # raw data of length 1 with byte val < 128
+        (b"x", bytearray([120])),
+        (bytearray(b"x"), bytearray([120])),
+        # raw data of length 1 with byte val >= 128
+        (b"\x83", bytearray([129, 131])),
+        (bytearray(b"\x83"), bytearray([129, 131])),
+        # raw data of length 55
+        (b"\x83" * 55, bytearray([183]) + bytearray(b"\x83" * 55)),
+        (bytearray(b"\x83") * 55, bytearray([183]) + bytearray(b"\x83" * 55)),
+        # raw data of length 2**20 (I would have done 2**64 - 1, but that needs a
+        # lot of RAM to just test).
+        (
+            b"\x83" * 2 ** 20,
+            bytearray([186])
+            + bytearray(b"\x10\x00\x00")
+            + bytearray(b"\x83" * 2 ** 20),
+        ),
+        (
+            bytearray(b"\x83") * 2 ** 20,
+            bytearray([186])
+            + bytearray(b"\x10\x00\x00")
+            + bytearray(b"\x83" * 2 ** 20),
+        ),
+    ],
+)
+def test_rlp_encode_bytes(
+    raw_data: Bytes, expected_encoded_data: Bytes
+) -> None:
+    assert rlp.encode_bytes(raw_data) == expected_encoded_data
+
+
+@pytest.mark.parametrize(
+    "raw_data, expected_encoded_data",
+    [
+        (Uint(0), b"\x80"),
+        (Uint(255), b"\x81\xff"),
+    ],
+)
+def test_rlp_encode_uint(raw_data: Uint, expected_encoded_data: Bytes) -> None:
+    assert rlp.encode(raw_data) == expected_encoded_data
+
+
+@pytest.mark.parametrize(
+    ("raw_data", "expected_encoded_data"),
+    [
+        # Empty sequence
+        ([], bytearray([192])),
+        # Sequence of single element of each bytes and Uint
+        ([b"hello"], bytearray([198]) + bytearray(b"\x85hello")),
+        ([Uint(255)], bytearray([194]) + bytearray(b"\x81\xff")),
+        # Sequence of 5 bytes and 5 ints
+        (
+            [b"hello"] * 5 + [Uint(35)] * 5,  # type: ignore
+            bytearray([227])
+            + bytearray(b"\x85hello\x85hello\x85hello\x85hello\x85hello#####"),
+        ),
+        # Sequence of 10 bytes and 10 ints
+        (
+            [b"hello"] * 10 + [Uint(35)] * 10,  # type: ignore
+            bytearray([248])
+            + bytearray(b"F")
+            + bytearray(
+                b"\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello##########"
+            ),
+        ),
+        # Nested Sequence
+        (
+            [b"hello", Uint(255), [b"how", [b"are", b"you", [b"doing"]]]],
+            bytearray(
+                b"\xdd\x85hello\x81\xff\xd4\x83how\xcf\x83are\x83you\xc6\x85doing"
+            ),
+        ),
+    ],
+)
+def test_rlp_encode_sequence(
+    raw_data: Sequence[RLP], expected_encoded_data: Bytes
+) -> None:
+    assert rlp.encode_sequence(raw_data) == expected_encoded_data
+
+
+@pytest.mark.parametrize(
+    "raw_data, expected_encoded_data",
+    [
+        (b"", bytearray([128])),
+        (b"\x83" * 55, bytearray([183]) + bytearray(b"\x83" * 55)),
+        (Uint(0), b"\x80"),
+        (Uint(255), b"\x81\xff"),
+        ([], bytearray([192])),
+        (
+            [b"hello"] * 5 + [Uint(35)] * 5,  # type: ignore
+            bytearray([227])
+            + bytearray(b"\x85hello\x85hello\x85hello\x85hello\x85hello#####"),
+        ),
+        (
+            [b"hello", Uint(255), [b"how", [b"are", b"you", [b"doing"]]]],
+            bytearray(
+                b"\xdd\x85hello\x81\xff\xd4\x83how\xcf\x83are\x83you\xc6\x85doing"
+            ),
+        ),
+    ],
+)
+def test_rlp_encode_successfully(
+    raw_data: RLP, expected_encoded_data: Bytes
+) -> None:
+    assert rlp.encode(raw_data) == expected_encoded_data
+
+
+@pytest.mark.parametrize(
+    "raw_data",
+    [
+        "hello",
+        123,
+        [b"hello", Uint(255), [b"how", [b"are", [b"you", ["doing"]]]]],
+    ],
+)
+def test_rlp_encode_fails(raw_data: RLP) -> None:
+    with pytest.raises(TypeError):
+        rlp.encode(raw_data)
+
+
+#
+# Tests for RLP decode
+#
+
+
+@pytest.mark.parametrize(
+    "encoded_data, expected_raw_data",
+    [
+        # Empty raw data
+        (bytearray([128]), bytearray()),
+        # raw data of length 1 with byte val < 128
+        (bytearray([120]), bytearray(b"x")),
+        # raw data of length 1 with byte val >= 128
+        (bytearray([129, 131]), bytearray(b"\x83")),
+        # raw data of length 55
+        (bytearray([183]) + bytearray(b"\x83" * 55), bytearray(b"\x83") * 55),
+        # raw data of length 2**20 (I would have done 2**64 - 1, but that needs a
+        # lot of RAM to just test).
+        (
+            bytearray([186])
+            + bytearray(b"\x10\x00\x00")
+            + bytearray(b"\x83" * (2 ** 20)),
+            bytearray(b"\x83") * (2 ** 20),
+        ),
+    ],
+)
+def test_rlp_decode_bytes(
+    encoded_data: Bytes, expected_raw_data: Bytes
+) -> None:
+    assert rlp.decode_to_bytes(encoded_data) == expected_raw_data
+
+
+@pytest.mark.parametrize(
+    "encoded_data, expected_raw_data",
+    [
+        (b"\x80", Uint(0).to_be_bytes()),
+        (b"\x81\xff", Uint(255).to_be_bytes()),
+    ],
+)
+def test_rlp_decode_uint(
+    encoded_data: Bytes, expected_raw_data: Bytes
+) -> None:
+    assert rlp.decode(encoded_data) == expected_raw_data
+
+
+@pytest.mark.parametrize(
+    "encoded_data, expected_raw_data",
+    [
+        # Empty sequence
+        (bytearray([192]), []),
+        # Sequence of single element of each bytes and Uint
+        (bytearray([198]) + bytearray(b"\x85hello"), [b"hello"]),
+        (bytearray([194]) + bytearray(b"\x81\xff"), [Uint(255).to_be_bytes()]),
+        (
+            bytearray([210]) + bytearray(b"\x85hello\x85hello\x85hello"),
+            [b"hello"] * 3,
+        ),
+        (
+            bytearray([198]) + bytearray(b"\x81\xff\x81\xff\x81\xff"),
+            [Uint(255).to_be_bytes()] * 3,
+        ),
+        # Sequence of 5 bytes and 5 ints
+        (
+            bytearray([227])
+            + bytearray(b"\x85hello\x85hello\x85hello\x85hello\x85hello#####"),
+            [b"hello"] * 5 + [Uint(35).to_be_bytes()] * 5,
+        ),
+        # Sequence of 10 bytes and 10 ints
+        (
+            bytearray([248])
+            + bytearray(b"F")
+            + bytearray(
+                b"\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello\x85hello##########"
+            ),
+            [b"hello"] * 10 + [Uint(35).to_be_bytes()] * 10,
+        ),
+        # Nested Sequence
+        (
+            b"\xdf\x85hello\x81\xff\xd6\x83how\xd1\x83are\x83you\xc8\x85doing\xc1#",
+            [
+                b"hello",
+                Uint(255).to_be_bytes(),
+                [
+                    b"how",
+                    [b"are", b"you", [b"doing", [Uint(35).to_be_bytes()]]],
+                ],
+            ],
+        ),
+    ],
+)
+def test_rlp_decode_sequence(
+    encoded_data: Bytes, expected_raw_data: Sequence[RLP]
+) -> None:
+    assert rlp.decode_to_sequence(encoded_data) == expected_raw_data
+
+
+@pytest.mark.parametrize(
+    "encoded_data, expected_raw_data",
+    [
+        (bytearray([128]), bytearray()),
+        (bytearray([183]) + bytearray(b"\x83" * 55), bytearray(b"\x83") * 55),
+        (bytearray([192]), []),
+        (
+            b"\xdb\x85hello\xd4\x83how\xcf\x83are\x83you\xc6\x85doing",
+            [b"hello", [b"how", [b"are", b"you", [b"doing"]]]],
+        ),
+    ],
+)
+def test_rlp_decode_successfully(
+    encoded_data: Bytes, expected_raw_data: RLP
+) -> None:
+    assert rlp.decode(encoded_data) == expected_raw_data
+
+
+@pytest.mark.parametrize(
+    "encoded_data",
+    [
+        b"",
+    ],
+)
+def test_rlp_decode_failure(encoded_data: Bytes) -> None:
+    with pytest.raises(Exception):
+        rlp.decode(encoded_data)
+
+
+@pytest.mark.parametrize(
+    "raw_data",
+    [
+        b"",
+        b"h",
+        b"hello how are you doing today?",
+        Uint(35).to_be_bytes(),
+        Uint(255).to_be_bytes(),
+        [],
+        [
+            b"hello",
+            [b"how", [b"are", b"you", [b"doing", [Uint(255).to_be_bytes()]]]],
+        ],
+    ],
+)
+def test_roundtrip_encoding_and_decoding(raw_data: RLP) -> None:
+    assert rlp.decode(rlp.encode(raw_data)) == raw_data
+
+
+#
+# Running ethereum/tests for rlp
+#
+
+
+def convert_to_rlp_native(
+    obj: Union[str, int, Sequence[Union[str, int]]]
+) -> RLP:
+    if isinstance(obj, str):
+        return bytes(obj, "utf-8")
+    elif isinstance(obj, int):
+        return Uint(obj)
+
+    # It's a sequence
+    return [convert_to_rlp_native(element) for element in obj]
+
+
+def ethtest_fixtures_as_pytest_fixtures(
+    *test_files: str,
+) -> List[Tuple[RLP, Bytes]]:
+    base_path = "tests/fixtures/RLPTests/"
+
+    test_data = dict()
+    for test_file in test_files:
+        with open(os.path.join(base_path, test_file), "r") as fp:
+            test_data.update(json.load(fp))
+
+    pytest_fixtures = []
+    for test_details in test_data.values():
+        if isinstance(test_details["in"], str) and test_details[
+            "in"
+        ].startswith("#"):
+            test_details["in"] = int(test_details["in"][1:])
+
+        pytest_fixtures.append(
+            (
+                convert_to_rlp_native(test_details["in"]),
+                hex2bytes(test_details["out"]),
+            )
+        )
+
+    return pytest_fixtures
+
+
+@pytest.mark.parametrize(
+    "raw_data, expected_encoded_data",
+    ethtest_fixtures_as_pytest_fixtures("rlptest.json"),
+)
+def test_ethtest_fixtures_for_rlp_encoding(
+    raw_data: RLP, expected_encoded_data: Bytes
+) -> None:
+    assert rlp.encode(raw_data) == expected_encoded_data
+
+
+@pytest.mark.parametrize(
+    "raw_data, encoded_data",
+    ethtest_fixtures_as_pytest_fixtures("RandomRLPTests/example.json"),
+)
+def test_ethtest_fixtures_for_successfull_rlp_decoding(
+    raw_data: Bytes, encoded_data: Bytes
+) -> None:
+    decoded_data = rlp.decode(encoded_data)
+    assert rlp.encode(decoded_data) == encoded_data
+
+
+@pytest.mark.parametrize(
+    "raw_data, encoded_data",
+    ethtest_fixtures_as_pytest_fixtures("invalidRLPTest.json"),
+)
+def test_ethtest_fixtures_for_fails_in_rlp_decoding(
+    raw_data: Bytes, encoded_data: Bytes
+) -> None:
+    with pytest.raises(Exception):
+        rlp.decode(encoded_data)
