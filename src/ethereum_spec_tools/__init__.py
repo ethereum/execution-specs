4995bd1df364ce8fa54f4b0b9ce73f8095c3677b
Sam Wilson <sam.wilson@mesh.xyz>
2021-07-09 09:14:47 -0400

    Add tool that enforces patch hygiene between hardforks.
    Ideally we want to support meaningful diffs between different hardforks,
    and generating meaningful diffs is a difficult problem.
    
    This tool scans the sources of all hardforks and ensures that the
    relative order of functions, classes, and constants doesn't change
    between forks.
    
    Hopefully this'll give us enough similarity for `rstdiff` to work its
    magic.

---
+++ src/ethereum_spec_tools/__init__.py
@@ -0,0 +1,7 @@
+"""
+Ethereum Specification Tools
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Library of utilities and tools necessary for rendering (or otherwise working
+with) the Ethereum specifications.
+"""
