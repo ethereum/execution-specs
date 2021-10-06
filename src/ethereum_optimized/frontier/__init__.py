0b4652c88c74f3ba66d2ae94723465438802dff5
Peter Davies <ultratwo@gmail.com>
2021-10-06 12:20:20 +0100

    Remove the old non-database optimized state

--- src/ethereum_optimized/frontier/__init__.py
+++ src/ethereum_optimized/frontier/__init__.py
@@ -8,19 +8,7 @@ Optimized Implementations (Frontier)
 """
 
 import ethereum.frontier.state as normal_state
-import ethereum_optimized.frontier.state as optimized_state
 import ethereum_optimized.frontier.state_db as optimized_state_db
-import ethereum_optimized.frontier.trie as optimized_trie
-
-optimized_state_patches = {
-    "Trie": optimized_trie.Trie,
-    "root": optimized_trie.root,
-    "trie_get": optimized_trie.trie_get,
-    "trie_set": optimized_trie.trie_set,
-    "State": optimized_state.State,
-    "set_storage": optimized_state.set_storage,
-    "get_storage": optimized_state.get_storage,
-}
 
 optimized_state_db_patches = {
     "State": optimized_state_db.State,
@@ -37,18 +25,6 @@ optimized_state_db_patches = {
     "commit_transaction": optimized_state_db.commit_transaction,
     "close_state": optimized_state_db.close_state,
 }
-
-
-def monkey_patch_optimized_state() -> None:
-    """
-    Replace the state interface with one that supports high performance
-    updates.
-
-    This function must be called before the state interface is imported
-    anywhere.
-    """
-    for (name, value) in optimized_state_patches.items():
-        setattr(normal_state, name, value)
 
 
 def monkey_patch_optimized_state_db() -> None:
