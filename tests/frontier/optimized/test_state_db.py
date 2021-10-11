a6024cfd18da5bcbbf88ad8f0d9827631eb1f7ac
Peter Davies <ultratwo@gmail.com>
2021-10-11 12:27:28 +0100

    Add regression tests

--- tests/frontier/optimized/test_state_db.py
+++ tests/frontier/optimized/test_state_db.py
@@ -1,9 +1,14 @@
 from pathlib import Path
-from typing import List, Tuple
+from typing import Any, List, Tuple
 
+import ethereum.frontier.state as state
 import ethereum.frontier.trie as normal_trie
 import ethereum_optimized.frontier.state_db as state_db
-from ethereum.base_types import Bytes, Optional
+from ethereum.base_types import U256, Bytes, Optional
+from ethereum.frontier.eth_types import EMPTY_ACCOUNT
+from ethereum.frontier.utils.hexadecimal import hex_to_address
+
+ADDRESS_FOO = hex_to_address("0x00000000219ab540356cbb839cbe05303d7705fa")
 
 operations: List[List[Tuple[Bytes, Optional[Bytes]]]] = [
     [],
@@ -40,3 +45,42 @@ def test_trie() -> None:
                 state_db.set_account_debug(state, key, value)
             root = normal_trie.root(trie_normal)
             assert root == state_db.state_root(state)
+
+
+def test_storage_key() -> None:
+    def actions(impl: Any) -> Any:
+        obj = impl.State()
+        impl.set_account(obj, ADDRESS_FOO, EMPTY_ACCOUNT)
+        impl.set_storage(obj, ADDRESS_FOO, b"", U256(42))
+        impl.state_root(obj)
+        return obj
+
+    state_normal = actions(state)
+    state_optimized = actions(state_db)
+    assert state.get_storage(
+        state_normal, ADDRESS_FOO, b""
+    ) == state_db.get_storage(state_optimized, ADDRESS_FOO, b"")
+    assert state.state_root(state_normal) == state_db.state_root(
+        state_optimized
+    )
+
+
+def test_resurrection() -> None:
+    def actions(impl: Any) -> Any:
+        obj = impl.State()
+        impl.set_account(obj, ADDRESS_FOO, EMPTY_ACCOUNT)
+        impl.set_storage(obj, ADDRESS_FOO, b"", U256(42))
+        impl.state_root(obj)
+        impl.destroy_account(obj, ADDRESS_FOO)
+        impl.state_root(obj)
+        impl.set_account(obj, ADDRESS_FOO, EMPTY_ACCOUNT)
+        return obj
+
+    state_normal = actions(state)
+    state_optimized = actions(state_db)
+    assert state.get_storage(
+        state_normal, ADDRESS_FOO, b""
+    ) == state_db.get_storage(state_optimized, ADDRESS_FOO, b"")
+    assert state.state_root(state_normal) == state_db.state_root(
+        state_optimized
+    )
