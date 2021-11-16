28c3cbd888bced22f3f69726e18d93a28a21436b
lightclient@protonmail.com <lightclient@protonmail.com>
2021-11-16 01:05:19 +0100

    implement wrapper for b11r

---
+++ evm_block_builder/__init__.py
@@ -0,0 +1,63 @@
+"""
+Python wrapper for the `evm b11r` tool.
+"""
+
+import json
+import subprocess
+from pathlib import Path
+from typing import Any, Optional, Tuple
+
+
+class BlockBuilder:
+    """
+    Block builder frontend.
+    """
+
+    binary: Path = Path("evm")
+
+    def build(
+        self,
+        env: Any,
+        txs: Any,
+        ommers: Any,
+        clique: Optional[Any],
+        ethash: bool = False,
+        ethashMode: str = "normal",
+    ) -> Tuple[str, str]:
+        """
+        Executes `evm b11r` with the specified arguments.
+        """
+        args = [
+            str(self.binary),
+            "b11r",
+            "--input.header=stdin",
+            "--input.txs=stdin",
+            "--input.ommers=stdin",
+            "--seal.clique=stdin",
+            "--output.block=stdout",
+        ]
+
+        if ethash:
+            args.append("--seal.ethash")
+            args.append("--seal.ethash.mode=" + ethashMode)
+
+        stdin = {
+            "header": env,
+            "txs": txs,
+            "uncles": ommers,
+            "clique": clique,
+        }
+        print(str.encode(json.dumps(stdin)))
+        result = subprocess.run(
+            args, input=str.encode(json.dumps(stdin)), stdout=subprocess.PIPE
+        )
+
+        if result.returncode != 0:
+            raise Exception("Failed to evaluate: " + str(result.stderr))
+
+        output = json.loads(result.stdout)
+
+        if "rlp" not in output or "hash" not in output:
+            Exception("Malformed result")
+
+        return (output["rlp"], output["hash"])
