062fb9fc5565444e1422bf9e323a7fc531a942eb
lightclient@protonmail.com <lightclient@protonmail.com>
2021-11-20 04:57:00 +0100

    relax type strictness, e2e test filled

--- tests/ethereum_test/test_filler.py
+++ tests/ethereum_test/test_filler.py
@@ -1,29 +1,50 @@
+import json
+
 from ethereum_test.filler import fill_fixture, StateTest
-from ethereum_test.helpers import AddrAA
-from ethereum_test.types import Account, Environment, Transaction
+from ethereum_test.types import Account, Environment, JSONEncoder, Transaction
 
 
 def test_fill_state_test():
+    env = Environment(
+        coinbase="0x2adc25665018aa1fe0e6bc666dac8fc2697ff9ba",
+        difficulty=0x20000,
+        gas_limit=10000000000,
+        number=1,
+        timestamp=1000,
+        previous="0x92230ce5476ae868e98c7979cfc165a93f8b6ad1922acf2df62e340916efd49d",  # noqa: E501
+        extra_data="0x00",
+    )
+
     pre = {
-        "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b": Account(
-            nonce=1, balance=10000
+        "0x1000000000000000000000000000000000000000": Account(
+            code="0x4660015500"
         ),
-        "0x00000000000000000000000000000000000000aa": Account(
-            code="0x6001600101600055"
+        "0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b": Account(
+            balance=1000000000000000000000
         ),
     }
 
     tx = Transaction(
-        ty=0x2,
-        nonce=1,
-        to=AddrAA,
-        gas_limit=30000,
-        max_fee_per_gas=100,
-        max_priority_fee_per_gas=101,
+        ty=0x0,
+        chain_id=0x0,
+        nonce=0,
+        to="0x1000000000000000000000000000000000000000",
+        gas_limit=100000000,
+        gas_price=10,
+        protected=False,
     )
 
-    test = StateTest(Environment(base_fee=0x7), pre, pre, [tx])
-    fixture = fill_fixture(test, "London", "ethash")
+    post = {
+        "0x1000000000000000000000000000000000000000": Account(
+            code="0x46600155", storage={"0x01": "0x01"}
+        ),
+    }
 
-    print(fixture)
-    assert False
+    test = StateTest(env, pre, post, [tx])
+    fixture = fill_fixture(test, "Istanbul", "NoProof")
+    with open("tests/ethereum_test/fixtures/chainid_filled.json") as f:
+        expected = json.load(f)
+
+    #  print(json.loads(json.dumps(fixture, cls=JSONEncoder)))
+    #  print(expected)
+    assert json.loads(json.dumps(fixture, cls=JSONEncoder)) == expected
--- tests/evm_transition_tool/test_evaluate.py
+++ tests/evm_transition_tool/test_evaluate.py
@@ -7,7 +7,7 @@ from evm_transition_tool import TransitionTool
 FIXTURES_ROOT = Path("tests/evm_transition_tool/fixtures")
 
 
-def test_simple() -> None:
+def test_t8n() -> None:
     t8n = TransitionTool()
 
     for test_dir in os.listdir(path=FIXTURES_ROOT):
@@ -19,11 +19,13 @@ def test_simple() -> None:
         with open(alloc_path, "r") as alloc, open(txs_path, "r") as txs, open(
             env_path, "r"
         ) as env, open(expected_path, "r") as exp:
+            print(expected_path)
             alloc = json.load(alloc)
             txs = json.load(txs)
             env = json.load(env)
             expected = json.load(exp)
 
             (alloc, result) = t8n.evaluate(alloc, txs, env, "Berlin")
+            print(result)
             assert alloc == expected.get("alloc")
             assert result == expected.get("result")
