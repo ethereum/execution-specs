d5e0a1aaca3565d73c392d7d6ab5096a899617e8
Guruprasad Kamath <guru241987@gmail.com>
2022-01-18 11:07:51 +0100

    Add Transaction Tests

---
+++ tests/homestead/test_transaction.py
@@ -0,0 +1,56 @@
+import json
+import os
+import sys
+from functools import partial
+
+import pytest
+
+from ethereum import rlp
+from ethereum.homestead.eth_types import Transaction
+from ethereum.homestead.spec import (
+    calculate_intrinsic_cost,
+    validate_transaction,
+)
+from ethereum.utils.hexadecimal import hex_to_bytes, hex_to_uint
+from tests.homestead.blockchain_st_test_helpers import load_test_transaction
+
+test_dir = "tests/fixtures/TransactionTests"
+
+load_homestead_transaction = partial(
+    load_test_transaction, network="Homestead"
+)
+
+
+@pytest.mark.parametrize(
+    "test_file_high_nonce",
+    [
+        "ttNonce/TransactionWithHighNonce64Minus1.json",
+        "ttNonce/TransactionWithHighNonce64.json",
+        "ttNonce/TransactionWithHighNonce64Plus1.json",
+    ],
+)
+def test_high_nonce(test_file_high_nonce: str) -> None:
+    test = load_homestead_transaction(test_dir, test_file_high_nonce)
+
+    assert validate_transaction(test["transaction"]) == False
+
+
+@pytest.mark.parametrize(
+    "test_file_nonce",
+    [
+        "ttNonce/TransactionWithHighNonce32.json",
+        "ttNonce/TransactionWithHighNonce64Minus2.json",
+    ],
+)
+def test_nonce(test_file_nonce: str) -> None:
+    test = load_homestead_transaction(test_dir, test_file_nonce)
+
+    result_intrinsic_gas_cost = hex_to_uint(
+        test["test_result"]["intrinsicGas"]
+    )
+
+    assert validate_transaction(test["transaction"]) == True
+    assert (
+        calculate_intrinsic_cost(test["transaction"])
+        == result_intrinsic_gas_cost
+    )
