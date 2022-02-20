de4fdcfcc1eda401fbfe7fbb371019da877749e7
Guruprasad Kamath <guru241987@gmail.com>
2022-02-20 17:55:02 +0100

    create state_test_helpers

--- tests/spurious_dragon/blockchain_st_test_helpers.py
+++ tests/spurious_dragon/blockchain_st_test_helpers.py
@@ -1,6 +1,6 @@
 from functools import partial
 
-from ..helpers.load import Load, run_blockchain_st_test
+from ..helpers.load_state_tests import Load, run_blockchain_st_test
 
 run_spurious_dragon_blockchain_st_tests = partial(
     run_blockchain_st_test, load=Load("EIP158", "spurious_dragon")
