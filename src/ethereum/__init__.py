b43182b330ebd903db8fcbb87e5ecca4a6174476
Peter Davies <ultratwo@gmail.com>
2022-07-12 10:35:03 +0100

    Add incantations to make ripemd work on OpenSSL 3

--- src/ethereum/__init__.py
+++ src/ethereum/__init__.py
@@ -19,7 +19,31 @@ possible, to aid in defining the behavior of Ethereum clients.
 import sys
 from typing import Any
 
+import cffi
+
 __version__ = "0.1.0"
+
+try:
+    # These incantations are necessary to make ripemd160 work on OpenSSL 3
+    ffi = cffi.FFI()
+    ffi.cdef("void *OSSL_PROVIDER_load(void *libctx, const char *name);")
+    lib = ffi.dlopen("crypto")
+    assert (
+        lib.OSSL_PROVIDER_load(
+            ffi.NULL, ffi.new("char[]", "default".encode("ascii"))
+        )
+        is not ffi.NULL
+    )
+    assert (
+        lib.OSSL_PROVIDER_load(
+            ffi.NULL, ffi.new("char[]", "legacy".encode("ascii"))
+        )
+        is not ffi.NULL
+    )
+except Exception:
+    # Ignore all failures, the code above is fragile and will fail in
+    # situations when it is not needed (e.g. you don't have OpenSSL 3).
+    pass
 
 #
 #  Ensure we can reach 1024 frames of recursion
