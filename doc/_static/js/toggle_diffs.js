7fee3307afb33ee3e127be7302690de6d04a1ee3
Guruprasad Kamath <guru241987@gmail.com>
2022-07-26 17:30:03 +0200

    review feedback

--- doc/_static/js/toggle_diffs.js
+++ doc/_static/js/toggle_diffs.js
@@ -1,5 +1,15 @@
+// Click Event Listner for toggle button
+window.addEventListener("load", (e) => {
+    const btn = document.getElementById("diff-toggle")
+    if (btn){
+        btn.addEventListener("click", toggleElements);
+    }
+})
+
 // All diffs will have one of the below classes
-const changeClasses = ["change-replaced", "change-replacement", "change-added", "change-removed"];
+const changeClassList = ["change-replaced", "change-replacement", "change-added", "change-removed"];
+const changeClassString = "." + changeClassList.join(", .");
+
 // The list of components to be scanned for diffs - selected by ID
 const componentsList = [
     '#module-details > *',
@@ -11,14 +21,14 @@ const componentsList = [
     '#table-of-contents',
     '#introduction'
 ];
-const components = componentsList.join(", ")
+const componentsString = componentsList.join(", ");
 
 function toggleElements() {
 
     // Check if the entire page is replaced. If yes, do nothing
     var fullPageReplaced = false;
     const pageElement = document.querySelector('span.target');
-    if (changeClasses.some(r => pageElement.classList.value.includes(r))){
+    if (changeClassList.some(r => pageElement.classList.value.includes(r))){
         fullPageReplaced = true;
     }
 
@@ -26,7 +36,7 @@ function toggleElements() {
     if (!fullPageReplaced){
         // Examine the contents of the individual sections
         // Hide sections that do not have diffs in any of the children
-        const all = document.querySelectorAll(components);
+        const all = document.querySelectorAll(componentsString);
 
         for (i=0; i < all.length; i++){
     
@@ -38,7 +48,7 @@ function toggleElements() {
     }
 
     // Toggle button text depending on what is currently displayed
-    const btn = document.querySelector('.diff-toggle');
+    const btn = document.getElementById('diff-toggle');
     if (btn.innerHTML === "Only Show Diffs") {
         btn.innerHTML = "Show All";
     } else {
@@ -47,22 +57,15 @@ function toggleElements() {
 }
 
 function childrenHaveDiffs(element){
-    
-    
-    var hasDiffs = false;
 
-    if (changeClasses.some(r => element.classList.value.includes(r))){
-        hasDiffs = true;
+    if (changeClassList.some(r => element.classList.value.includes(r))){
+        return true;
     } else {
-        const children = element.querySelectorAll('*');
-
-        for (j = 0; j < children.length; j++){
-            if (changeClasses.some(r => children[j].classList.value.includes(r))){
-                hasDiffs = true;
-                break;
-            } 
+        const children = element.querySelectorAll(changeClassString);
+        if (children.length > 0){
+                return true;
         }
     }
 
-    return hasDiffs;
+    return false;
 }
