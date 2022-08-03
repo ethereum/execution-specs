3901670a1b061258a9af030a8d798a0ddb1aa9d5
Peter Davies <ultratwo@gmail.com>
2022-08-03 12:46:00 +0100

    Patch tool to homestead

--- src/ethereum/homestead/vm/gas.py
+++ src/ethereum/homestead/vm/gas.py
@@ -13,59 +13,59 @@ EVM gas constants and calculators.
 """
 from ethereum.base_types import U256, Uint
 from ethereum.utils.numeric import ceil32
-from ethereum.utils.safe_arithmetic import u256_safe_add
 
 from ..eth_types import Address
 from ..state import State, account_exists
+from . import Evm
 from .exceptions import OutOfGasError
 
-GAS_JUMPDEST = U256(1)
-GAS_BASE = U256(2)
-GAS_VERY_LOW = U256(3)
-GAS_SLOAD = U256(50)
-GAS_STORAGE_SET = U256(20000)
-GAS_STORAGE_UPDATE = U256(5000)
-GAS_STORAGE_CLEAR_REFUND = U256(15000)
-GAS_LOW = U256(5)
-GAS_MID = U256(8)
-GAS_HIGH = U256(10)
-GAS_EXPONENTIATION = U256(10)
-GAS_EXPONENTIATION_PER_BYTE = U256(10)
-GAS_MEMORY = U256(3)
-GAS_KECCAK256 = U256(30)
-GAS_KECCAK256_WORD = U256(6)
-GAS_COPY = U256(3)
-GAS_BLOCK_HASH = U256(20)
-GAS_EXTERNAL = U256(20)
-GAS_BALANCE = U256(20)
-GAS_LOG = U256(375)
-GAS_LOG_DATA = U256(8)
-GAS_LOG_TOPIC = U256(375)
-GAS_CREATE = U256(32000)
-GAS_CODE_DEPOSIT = U256(200)
-GAS_ZERO = U256(0)
-GAS_CALL = U256(40)
-GAS_NEW_ACCOUNT = U256(25000)
-GAS_CALL_VALUE = U256(9000)
-GAS_CALL_STIPEND = U256(2300)
-REFUND_SELF_DESTRUCT = U256(24000)
-GAS_ECRECOVER = U256(3000)
-GAS_SHA256 = U256(60)
-GAS_SHA256_WORD = U256(12)
-GAS_RIPEMD160 = U256(600)
-GAS_RIPEMD160_WORD = U256(120)
-GAS_IDENTITY = U256(15)
-GAS_IDENTITY_WORD = U256(3)
+GAS_JUMPDEST = Uint(1)
+GAS_BASE = Uint(2)
+GAS_VERY_LOW = Uint(3)
+GAS_SLOAD = Uint(50)
+GAS_STORAGE_SET = Uint(20000)
+GAS_STORAGE_UPDATE = Uint(5000)
+GAS_STORAGE_CLEAR_REFUND = Uint(15000)
+GAS_LOW = Uint(5)
+GAS_MID = Uint(8)
+GAS_HIGH = Uint(10)
+GAS_EXPONENTIATION = Uint(10)
+GAS_EXPONENTIATION_PER_BYTE = Uint(10)
+GAS_MEMORY = Uint(3)
+GAS_KECCAK256 = Uint(30)
+GAS_KECCAK256_WORD = Uint(6)
+GAS_COPY = Uint(3)
+GAS_BLOCK_HASH = Uint(20)
+GAS_EXTERNAL = Uint(20)
+GAS_BALANCE = Uint(20)
+GAS_LOG = Uint(375)
+GAS_LOG_DATA = Uint(8)
+GAS_LOG_TOPIC = Uint(375)
+GAS_CREATE = Uint(32000)
+GAS_CODE_DEPOSIT = Uint(200)
+GAS_ZERO = Uint(0)
+GAS_CALL = Uint(40)
+GAS_NEW_ACCOUNT = Uint(25000)
+GAS_CALL_VALUE = Uint(9000)
+GAS_CALL_STIPEND = Uint(2300)
+REFUND_SELF_DESTRUCT = Uint(24000)
+GAS_ECRECOVER = Uint(3000)
+GAS_SHA256 = Uint(60)
+GAS_SHA256_WORD = Uint(12)
+GAS_RIPEMD160 = Uint(600)
+GAS_RIPEMD160_WORD = Uint(120)
+GAS_IDENTITY = Uint(15)
+GAS_IDENTITY_WORD = Uint(3)
 
 
-def subtract_gas(gas_left: U256, amount: U256) -> U256:
+def charge_gas(evm: Evm, amount: Uint) -> None:
     """
-    Subtracts `amount` from `gas_left`.
+    Subtracts `amount` from `evm.gas_left`.
 
     Parameters
     ----------
-    gas_left :
-        The amount of gas left in the current frame.
+    evm :
+        The current EVM.
     amount :
         The amount of gas the current operation requires.
 
@@ -74,13 +74,13 @@ def subtract_gas(gas_left: U256, amount: U256) -> U256:
     :py:class:`~ethereum.homestead.vm.exceptions.OutOfGasError`
         If `gas_left` is less than `amount`.
     """
-    if gas_left < amount:
+    if evm.gas_left < amount:
         raise OutOfGasError
+    else:
+        evm.gas_left -= U256(amount)
 
-    return gas_left - amount
 
-
-def calculate_memory_gas_cost(size_in_bytes: Uint) -> U256:
+def calculate_memory_gas_cost(size_in_bytes: Uint) -> Uint:
     """
     Calculates the gas cost for allocating memory
     to the smallest multiple of 32 bytes,
@@ -93,7 +93,7 @@ def calculate_memory_gas_cost(size_in_bytes: Uint) -> U256:
 
     Returns
     -------
-    total_gas_cost : `ethereum.base_types.U256`
+    total_gas_cost : `ethereum.base_types.Uint`
         The gas cost for storing data in memory.
     """
     size_in_words = ceil32(size_in_bytes) // 32
@@ -101,14 +101,14 @@ def calculate_memory_gas_cost(size_in_bytes: Uint) -> U256:
     quadratic_cost = size_in_words**2 // 512
     total_gas_cost = linear_cost + quadratic_cost
     try:
-        return U256(total_gas_cost)
+        return Uint(total_gas_cost)
     except ValueError:
         raise OutOfGasError
 
 
 def calculate_gas_extend_memory(
-    memory: bytearray, start_position: Uint, size: U256
-) -> U256:
+    memory: bytearray, start_position: U256, size: U256
+) -> Uint:
     """
     Calculates the gas amount to extend memory
 
@@ -123,18 +123,18 @@ def calculate_gas_extend_memory(
 
     Returns
     -------
-    to_be_paid : `ethereum.base_types.U256`
+    to_be_paid : `ethereum.base_types.Uint`
         returns `0` if size=0 or if the
         size after extending memory is less than the size before extending
         else it returns the amount that needs to be paid for extendinng memory.
     """
     if size == 0:
-        return U256(0)
+        return Uint(0)
     memory_size = Uint(len(memory))
     before_size = ceil32(memory_size)
-    after_size = ceil32(start_position + size)
+    after_size = ceil32(Uint(start_position) + Uint(size))
     if after_size <= before_size:
-        return U256(0)
+        return Uint(0)
     already_paid = calculate_memory_gas_cost(before_size)
     total_cost = calculate_memory_gas_cost(after_size)
     to_be_paid = total_cost - already_paid
@@ -142,8 +142,8 @@ def calculate_gas_extend_memory(
 
 
 def calculate_call_gas_cost(
-    state: State, gas: U256, to: Address, value: U256
-) -> U256:
+    state: State, gas: Uint, to: Address, value: U256
+) -> Uint:
     """
     Calculates the gas amount for executing Opcodes `CALL` and `CALLCODE`.
 
@@ -160,22 +160,16 @@ def calculate_call_gas_cost(
 
     Returns
     -------
-    call_gas_cost: `ethereum.base_types.U256`
+    call_gas_cost: `ethereum.base_types.Uint`
         The total gas amount for executing Opcodes `CALL` and `CALLCODE`.
     """
     _account_exists = account_exists(state, to)
-    create_gas_cost = U256(0) if _account_exists else GAS_NEW_ACCOUNT
-    transfer_gas_cost = U256(0) if value == 0 else GAS_CALL_VALUE
-    return u256_safe_add(
-        GAS_CALL,
-        gas,
-        create_gas_cost,
-        transfer_gas_cost,
-        exception_type=OutOfGasError,
-    )
+    create_gas_cost = Uint(0) if _account_exists else GAS_NEW_ACCOUNT
+    transfer_gas_cost = Uint(0) if value == 0 else GAS_CALL_VALUE
+    return GAS_CALL + gas + create_gas_cost + transfer_gas_cost
 
 
-def calculate_message_call_gas_stipend(value: U256) -> U256:
+def calculate_message_call_gas_stipend(value: U256) -> Uint:
     """
     Calculates the gas stipend for making the message call
     with the given value.
@@ -186,7 +180,7 @@ def calculate_message_call_gas_stipend(value: U256) -> U256:
         The amount of `ETH` that needs to be transferred.
     Returns
     -------
-    message_call_gas_stipend : `ethereum.base_types.U256`
+    message_call_gas_stipend : `ethereum.base_types.Uint`
         The gas stipend for making the message-call.
     """
-    return U256(0) if value == 0 else GAS_CALL_STIPEND
+    return Uint(0) if value == 0 else GAS_CALL_STIPEND
