f80448dee9ddac3b8a1eb3660da500c9cffa70a0
Guruprasad Kamath <guru241987@gmail.com>
2022-10-29 11:25:08 +0200

    formatting

--- tests/arrow_glacier/test_diffs.py
+++ tests/arrow_glacier/test_diffs.py
@@ -1,11 +1,11 @@
 import os
+
 from tests.helpers.diff_test_helpers import run_diff_test
 
 expected_diffs_path = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    "expected_diffs"
+    os.path.dirname(os.path.realpath(__file__)), "expected_diffs"
 )
 
 
-def test_diffs():
-    run_diff_test("arrow_glacier", expected_diffs_path)
+def test_diffs(tmp_path: str) -> None:
+    run_diff_test("arrow_glacier", expected_diffs_path, tmp_path)
--- tests/helpers/diff_test_helpers.py
+++ tests/helpers/diff_test_helpers.py
@@ -1,9 +1,8 @@
 import os
 import re
-import sys
 import shutil
 import subprocess
-
+import sys
 from typing import Tuple
 
 from ethereum_spec_tools.forks import Hardfork
@@ -22,7 +21,9 @@ def get_fork_paths(fork_name: str) -> Tuple:
 
     for i, fork in enumerate(forks):
         if fork.short_name == fork_name:
-            return forks[i-1].path[0], fork.path[0]
+            return forks[i - 1].path[0], fork.path[0]  # type: ignore
+
+    return None, None
 
 
 def find_source_files(path: str) -> set:
@@ -36,47 +37,53 @@ def find_source_files(path: str) -> set:
                 file_path = os.path.join(directory, filename)
                 file_path = file_path[len(path) + 1 :]
                 file_list.append(file_path)
-    
+
     return set(file_list)
 
-def process_source_code(path, name):
+
+def process_source_code(path: str, name: str) -> None:
     """
     Clean up the source code.
     Removes comments and empty lines.
     """
     if not os.path.isfile(path):
-        open(name, 'a').close()
+        open(name, "a").close()
         return
 
     with open(path) as f:
         code = f.read()
 
-    code = re.sub(re.compile("\"\"\".*?\"\"\"", re.DOTALL), "", code)
+    code = re.sub(re.compile('""".*?"""', re.DOTALL), "", code)
     code = re.sub(re.compile("#.*?\n"), "\n", code)
     code = re.sub(re.compile("\n\s*\n"), "\n", code)
 
-    with open(name, 'w') as f:
+    with open(name, "w") as f:
         f.write(code)
-
 
 
 class DiffTester:
     """
     Test the diffs in source code and compare it with expected diffs.
     """
-    def __init__(self, fork_name, expected_diffs_path, diffs_dir=None):
+
+    def __init__(
+        self,
+        fork_name: str,
+        expected_diffs_path: str,
+        found_diffs_path: str = None,
+    ) -> None:
         self.old_fork_path, self.new_fork_path = get_fork_paths(fork_name)
-
-        if diffs_dir:
-            self.diffs_dir = diffs_dir
-        else:
-            self.diffs_dir = os.path.dirname(os.path.realpath(__file__))
-
-        self.found_diffs_path = os.path.join(self.diffs_dir, "expected_diffs")
         self.expected_diffs_path = expected_diffs_path
 
-    
-    def create_diffs(self):
+        if found_diffs_path:
+            self.found_diffs_path = found_diffs_path
+        else:
+            self.found_diffs_path = os.path.join(
+                os.path.dirname(os.path.realpath(__file__)), "found_diffs"
+            )
+        os.makedirs(self.found_diffs_path, exist_ok=True)
+
+    def create_diffs(self) -> None:
         """
         Create all the diffs of a fork wrt. the previous fork.
         This will be the found diffs that can be compared with expected in `compare_diffs`
@@ -88,40 +95,53 @@ class DiffTester:
 
         for file in source_files:
             old_source_path = os.path.join(self.old_fork_path, file)
-            old_processed_path = os.path.join(self.diffs_dir, "old.py")
+            old_processed_path = os.path.join(self.found_diffs_path, "old.py")
             new_source_path = os.path.join(self.new_fork_path, file)
-            new_processed_path = os.path.join(self.diffs_dir, "new.py")
+            new_processed_path = os.path.join(self.found_diffs_path, "new.py")
 
             process_source_code(old_source_path, old_processed_path)
             process_source_code(new_source_path, new_processed_path)
 
-            diffs = subprocess.run(["diff", old_processed_path, new_processed_path], capture_output=True, text=True)
+            diffs = subprocess.run(
+                ["diff", old_processed_path, new_processed_path],
+                capture_output=True,
+                text=True,
+            )
             if diffs.stdout:
-                diff_file = os.path.join(self.found_diffs_path, file.split(".")[0])
+                diff_file = os.path.join(
+                    self.found_diffs_path, file.split(".")[0]
+                )
                 os.makedirs(os.path.dirname(diff_file), exist_ok=True)
 
-                output_diff = re.sub(re.compile("(^|\n)(?![<|>]).*?\n"), "\n\n", diffs.stdout)
-                with open(diff_file, 'w') as f:
+                # Ignore anything other than <, > which denote deletion, insertion
+                output_diff = re.sub(
+                    re.compile("(^|\n)(?![<|>]).*?\n"), "\n\n", diffs.stdout
+                )
+                with open(diff_file, "w") as f:
                     f.write(output_diff)
 
             os.remove(old_processed_path)
             os.remove(new_processed_path)
 
-
-    def compare_diffs(self):
+    def compare_diffs(self) -> None:
         """
         Compare found diffs with expected. Clean up the folders after test
         """
-        diffs = subprocess.run(["diff", self.expected_diffs_path, self.found_diffs_path], capture_output=True, text=True)
+        diffs = subprocess.run(
+            ["diff", self.expected_diffs_path, self.found_diffs_path],
+            capture_output=True,
+            text=True,
+        )
         shutil.rmtree(self.found_diffs_path)
         assert diffs.stdout == ""
 
 
-
-def run_diff_test(fork_name, expected_diffs_path):
+def run_diff_test(
+    fork_name: str, expected_diffs_path: str, found_diffs_path: str = None
+) -> None:
     """
     Run the diff test for a particular fork.
     """
-    diff_tester = DiffTester(fork_name, expected_diffs_path)
+    diff_tester = DiffTester(fork_name, expected_diffs_path, found_diffs_path)
     diff_tester.create_diffs()
     diff_tester.compare_diffs()
