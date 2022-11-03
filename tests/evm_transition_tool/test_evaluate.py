e91dbe2f80853d9220928cb3829f20cb46538126
marioevz <marioevz@gmail.com>
2022-11-03 15:52:00 -0600

    Parametrize existing tests

--- tests/evm_transition_tool/test_evaluate.py
+++ tests/evm_transition_tool/test_evaluate.py
@@ -76,24 +76,24 @@ def test_calc_state_root(
 
 
 @pytest.mark.parametrize("t8n", [EvmTransitionTool()])
-def test_evm_t8n(t8n: TransitionTool) -> None:
+@pytest.mark.parametrize("test_dir", os.listdir(path=FIXTURES_ROOT))
+def test_evm_t8n(t8n: TransitionTool, test_dir: str) -> None:
 
-    for test_dir in os.listdir(path=FIXTURES_ROOT):
-        alloc_path = Path(FIXTURES_ROOT, test_dir, "alloc.json")
-        txs_path = Path(FIXTURES_ROOT, test_dir, "txs.json")
-        env_path = Path(FIXTURES_ROOT, test_dir, "env.json")
-        expected_path = Path(FIXTURES_ROOT, test_dir, "exp.json")
+    alloc_path = Path(FIXTURES_ROOT, test_dir, "alloc.json")
+    txs_path = Path(FIXTURES_ROOT, test_dir, "txs.json")
+    env_path = Path(FIXTURES_ROOT, test_dir, "env.json")
+    expected_path = Path(FIXTURES_ROOT, test_dir, "exp.json")
 
-        with open(alloc_path, "r") as alloc, open(txs_path, "r") as txs, open(
-            env_path, "r"
-        ) as env, open(expected_path, "r") as exp:
-            print(expected_path)
-            alloc = json.load(alloc)
-            txs = json.load(txs)
-            env = json.load(env)
-            expected = json.load(exp)
+    with open(alloc_path, "r") as alloc, open(txs_path, "r") as txs, open(
+        env_path, "r"
+    ) as env, open(expected_path, "r") as exp:
+        print(expected_path)
+        alloc = json.load(alloc)
+        txs = json.load(txs)
+        env = json.load(env)
+        expected = json.load(exp)
 
-            (alloc, result) = t8n.evaluate(alloc, txs, env, "Berlin")
-            print(result)
-            assert alloc == expected.get("alloc")
-            assert result == expected.get("result")
+        (alloc, result) = t8n.evaluate(alloc, txs, env, "Berlin")
+        print(result)
+        assert alloc == expected.get("alloc")
+        assert result == expected.get("result")
