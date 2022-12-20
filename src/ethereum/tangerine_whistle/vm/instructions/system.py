8118c9f673717d1480b04b16d29b95730b110fd7
Guruprasad Kamath <guru241987@gmail.com>
2022-12-20 17:32:24 +0000

    re-factor tangerine whistle

--- src/ethereum/tangerine_whistle/vm/instructions/system.py
+++ src/ethereum/tangerine_whistle/vm/instructions/system.py
@@ -31,12 +31,12 @@ from ..gas import (
     GAS_SELF_DESTRUCT,
     GAS_SELF_DESTRUCT_NEW_ACCOUNT,
     GAS_ZERO,
-    calculate_call_gas_cost,
-    calculate_message_call_gas_stipend,
+    calculate_gas_extend_memory,
+    calculate_message_call_gas,
     charge_gas,
     max_message_call_gas,
 )
-from ..memory import extend_memory, memory_read_bytes, memory_write
+from ..memory import memory_read_bytes, memory_write
 from ..stack import pop, push
 
 
@@ -59,13 +59,17 @@ def create(evm: Evm) -> None:
     memory_size = pop(evm.stack)
 
     # GAS
-    extend_memory(evm, memory_start_position, memory_size)
-    charge_gas(evm, GAS_CREATE)
+    extend_memory = calculate_gas_extend_memory(
+        evm.memory, [(memory_start_position, memory_size)]
+    )
+
+    charge_gas(evm, GAS_CREATE + extend_memory.cost)
 
     create_message_gas = max_message_call_gas(Uint(evm.gas_left))
     evm.gas_left -= U256(create_message_gas)
 
     # OPERATION
+    evm.memory += b"\x00" * extend_memory.expand_by
     sender_address = evm.message.current_target
     sender = get_account(evm.env.state, sender_address)
 
@@ -133,10 +137,14 @@ def return_(evm: Evm) -> None:
     memory_size = pop(evm.stack)
 
     # GAS
-    extend_memory(evm, memory_start_position, memory_size)
-    charge_gas(evm, GAS_ZERO)
+    extend_memory = calculate_gas_extend_memory(
+        evm.memory, [(memory_start_position, memory_size)]
+    )
+
+    charge_gas(evm, GAS_ZERO + extend_memory.cost)
 
     # OPERATION
+    evm.memory += b"\x00" * extend_memory.expand_by
     evm.output = memory_read_bytes(
         evm.memory, memory_start_position, memory_size
     )
@@ -224,33 +232,37 @@ def call(evm: Evm) -> None:
     memory_output_size = pop(evm.stack)
 
     # GAS
-    extend_memory(evm, memory_input_start_position, memory_input_size)
-    extend_memory(evm, memory_output_start_position, memory_output_size)
+    extend_memory = calculate_gas_extend_memory(
+        evm.memory,
+        [
+            (memory_input_start_position, memory_input_size),
+            (memory_output_start_position, memory_output_size),
+        ],
+    )
     _account_exists = account_exists(evm.env.state, to)
     create_gas_cost = Uint(0) if _account_exists else GAS_NEW_ACCOUNT
     transfer_gas_cost = Uint(0) if value == 0 else GAS_CALL_VALUE
-    call_gas_fee = calculate_call_gas_cost(
-        gas, Uint(evm.gas_left), GAS_CALL + create_gas_cost + transfer_gas_cost
-    )
-    message_call_gas = calculate_message_call_gas_stipend(
+    message_call_gas = calculate_message_call_gas(
         value,
         gas,
         Uint(evm.gas_left),
+        extend_memory.cost,
         GAS_CALL + create_gas_cost + transfer_gas_cost,
     )
-    charge_gas(evm, call_gas_fee)
+    charge_gas(evm, message_call_gas.cost + extend_memory.cost)
 
     # OPERATION
+    evm.memory += b"\x00" * extend_memory.expand_by
     sender_balance = get_account(
         evm.env.state, evm.message.current_target
     ).balance
     if sender_balance < value:
         push(evm.stack, U256(0))
-        evm.gas_left += message_call_gas
+        evm.gas_left += message_call_gas.stipend
     else:
         generic_call(
             evm,
-            message_call_gas,
+            message_call_gas.stipend,
             value,
             evm.message.current_target,
             to,
@@ -287,31 +299,35 @@ def callcode(evm: Evm) -> None:
     # GAS
     to = evm.message.current_target
 
-    extend_memory(evm, memory_input_start_position, memory_input_size)
-    extend_memory(evm, memory_output_start_position, memory_output_size)
-    transfer_gas_cost = Uint(0) if value == 0 else GAS_CALL_VALUE
-    call_gas_fee = calculate_call_gas_cost(
-        gas, Uint(evm.gas_left), GAS_CALL + transfer_gas_cost
+    extend_memory = calculate_gas_extend_memory(
+        evm.memory,
+        [
+            (memory_input_start_position, memory_input_size),
+            (memory_output_start_position, memory_output_size),
+        ],
     )
-    message_call_gas = calculate_message_call_gas_stipend(
+    transfer_gas_cost = Uint(0) if value == 0 else GAS_CALL_VALUE
+    message_call_gas = calculate_message_call_gas(
         value,
         gas,
         Uint(evm.gas_left),
+        extend_memory.cost,
         GAS_CALL + transfer_gas_cost,
     )
-    charge_gas(evm, call_gas_fee)
+    charge_gas(evm, message_call_gas.cost + extend_memory.cost)
 
     # OPERATION
+    evm.memory += b"\x00" * extend_memory.expand_by
     sender_balance = get_account(
         evm.env.state, evm.message.current_target
     ).balance
     if sender_balance < value:
         push(evm.stack, U256(0))
-        evm.gas_left += message_call_gas
+        evm.gas_left += message_call_gas.stipend
     else:
         generic_call(
             evm,
-            message_call_gas,
+            message_call_gas.stipend,
             value,
             evm.message.current_target,
             to,
@@ -387,18 +403,23 @@ def delegatecall(evm: Evm) -> None:
     memory_output_size = pop(evm.stack)
 
     # GAS
-    extend_memory(evm, memory_input_start_position, memory_input_size)
-    extend_memory(evm, memory_output_start_position, memory_output_size)
-    call_gas_fee = calculate_call_gas_cost(gas, Uint(evm.gas_left), GAS_CALL)
-    message_call_gas = calculate_message_call_gas_stipend(
-        U256(0), gas, Uint(evm.gas_left), GAS_CALL
+    extend_memory = calculate_gas_extend_memory(
+        evm.memory,
+        [
+            (memory_input_start_position, memory_input_size),
+            (memory_output_start_position, memory_output_size),
+        ],
     )
-    charge_gas(evm, call_gas_fee)
+    message_call_gas = calculate_message_call_gas(
+        U256(0), gas, Uint(evm.gas_left), extend_memory.cost, GAS_CALL
+    )
+    charge_gas(evm, message_call_gas.cost + extend_memory.cost)
 
     # OPERATION
+    evm.memory += b"\x00" * extend_memory.expand_by
     generic_call(
         evm,
-        message_call_gas,
+        message_call_gas.stipend,
         evm.message.value,
         evm.message.caller,
         evm.message.current_target,
