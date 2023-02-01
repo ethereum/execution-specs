baf23c78fad877a555ec63751c9d35fa8da0b60d
Guruprasad Kamath <guru241987@gmail.com>
2023-02-01 11:08:19 +0100

    fetch fixtures at pytest sessionstart
    This change fetches the test fixtures from the relevant repositories/releases
    as and when required. As pytest is run, it checks at the beginning of the session
    if the test fixtures are already available. If not, it fetches them. The following forms
    of fetching are supported
    
    1. Download the fixtures from a certain relase artifact (.tar.gz)
    2. Clone the fixtures given a repository link and commit. Submodules will also be recursively
    cloned in this case.
    
    The fetching process is made safe for parallel process runs (with xdist for example) by
    using a signal file which essentially tells the other processes that a process is already
    fetching the relevant fixtures and they just have to wait.
    
    The test run phase starts only once all the fixtures have been downloaded/cloned. In order to change
    the links/commit hashes of the fixtures update tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,27 @@
+import os
+import tarfile
+import time
+import urllib.request
+
+import git
 from _pytest.config import Config
 from _pytest.config.argparsing import Parser
+from pytest import Session
 
 import ethereum
 from ethereum_spec_tools.evm_trace import evm_trace
+
+# Update the links and commit has in order to consume
+# newer/other tests
+test_fixtures = {
+    "execution-spec-generated-tests": {
+        "url": "https://github.com/ethereum/execution-spec-tests/releases/download/v0.2.1/fixtures.tar.gz",
+    },
+    "fixtures": {
+        "url": "https://github.com/ethereum/tests.git",
+        "commit_hash": "afba21c63bcfb9b1ba3bd7cc1db95aef9452384e",
+    },
+}
 
 
 def pytest_addoption(parser: Parser) -> None:
@@ -39,3 +58,87 @@ def pytest_configure(config: Config) -> None:
         import ethereum_optimized
 
         ethereum_optimized.monkey_patch(None)
+
+
+def download_fixtures(url: str, location: str) -> None:
+
+    # xdist processes will all try to download the fixtures.
+    # The signal file will indicate whether some other
+    # process is already running this. That way it can
+    # be parallel safe
+    signal_file = f"{location}.txt"
+
+    if not os.path.exists(signal_file):
+        # Create signal file to stop other processes
+        # from running the same code
+        with open(signal_file, "w") as f:
+            f.write(f"Downloading {location}...")
+
+        if not os.path.exists(location):
+
+            print(f"Downloading {location}...")
+            urllib.request.urlretrieve(url, "temp.tar.gz")
+
+            with tarfile.open("temp.tar.gz", "r:gz") as tar:
+                tar.extractall(location)
+
+            os.remove("temp.tar.gz")
+        else:
+            print(f"{location} already available.")
+
+        os.remove(signal_file)
+    else:
+        # Some other process is downloading the fixtures
+        # already
+        while os.path.exists(signal_file):
+            time.sleep(1)
+
+
+def git_clone_fixtures(url: str, commit_hash: str, location: str) -> None:
+
+    # xdist processes will all try to download the fixtures.
+    # The signal file will indicate whether some other
+    # process is already running this. That way it can
+    # be parallel safe
+    signal_file = f"{location}.txt"
+
+    if not os.path.exists(signal_file):
+        # Create signal file to stop other processes
+        # from running the same code
+        with open(signal_file, "w") as f:
+            f.write(f"Downloading {location}...")
+
+        if not os.path.exists(location):
+            print(f"Cloning {location}...")
+            repo = git.Repo.clone_from(url, to_path=location)
+        else:
+            print(f"{location} already available.")
+            repo = git.Repo(location)
+
+        print(f"Checking out the correct commit {commit_hash}...")
+        branch = repo.heads["develop"]
+        repo.remotes.origin.pull(branch.name)
+        repo.git.checkout(commit_hash)
+
+        repo.submodule_update(init=True, recursive=True)
+        os.remove(signal_file)
+    else:
+        # Some other process is cloning the fixtures
+        # already
+        while os.path.exists(signal_file):
+            time.sleep(1)
+
+
+def pytest_sessionstart(session: Session) -> None:
+
+    fixtures_location = "tests"
+
+    for idx, props in test_fixtures.items():
+        fixture_path = f"{fixtures_location}/{idx}"
+
+        if "commit_hash" in props:
+            git_clone_fixtures(
+                props["url"], props["commit_hash"], fixture_path
+            )
+        else:
+            download_fixtures(props["url"], fixture_path)
