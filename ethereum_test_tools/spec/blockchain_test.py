bc93eaed3c0e1ee2f77d09600c469e28eee10843
spencer-tb <spencer@spencertaylorbrown.uk>
2023-03-15 14:47:36 -0600

    Re-add prev_alloc for blocktests, tox pass.

--- ethereum_test_tools/spec/blockchain_test.py
+++ ethereum_test_tools/spec/blockchain_test.py
@@ -210,7 +210,7 @@ class BlockchainTest(BaseTest):
                         block_number=header.number,
                     ),
                     previous_env,
-                    next_alloc,
+                    previous_alloc,
                     previous_head,
                 )
         else:
