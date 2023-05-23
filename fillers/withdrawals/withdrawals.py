7d7aafcc49df890c99ee593d865076bde45c09cc
danceratopz <danceratopz@gmail.com>
2023-05-23 11:25:37 +0200

    style: remove unused and now defunct global variable

--- fillers/withdrawals/withdrawals.py
+++ fillers/withdrawals/withdrawals.py
@@ -25,7 +25,6 @@ from ethereum_test_tools.vm.opcode import Opcodes as Op
 REFERENCE_SPEC_GIT_PATH = "EIPS/eip-4895.md"
 REFERENCE_SPEC_VERSION = "81af3b60b632bc9c03513d1d137f25410e3f4d34"
 
-WITHDRAWALS_FORK = Shanghai
 pytestmark = pytest.mark.parametrize("fork", forks_from(Shanghai))
 
 ONE_GWEI = 10**9
