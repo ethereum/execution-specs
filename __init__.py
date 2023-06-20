c2bf6e576121fcf0656f0107c4e14a7e1366ead1
danceratopz <danceratopz@gmail.com>
2023-06-20 12:24:33 +0200

    docs: add 'generate fixtures for dev fork' to gen doc
    This adds 'example' admonitions informing how to generate fixtures for dev forks for the top-level 'tests' page (additionally) and any dev forks page (instead of deployed forks).

--- __init__.py
+++ __init__.py
@@ -2,10 +2,4 @@
 Cross-client test cases organized by fork. Each directory underneath `tests/` contains test cases
 corresponding to the fork in which the functionality under test was introduced a specific area of
 EVM functionality.
-
-example: Execute all test cases for all forks up to and including the latest deployed fork with:
-    ```
-    fill
-    ```
-
 """
