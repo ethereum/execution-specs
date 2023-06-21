fa7f26314e7856e205ab501605c022b5b4d7713a
danceratopz <danceratopz@gmail.com>
2023-06-21 14:15:50 +0200

    docs: add links to github for test paths for the generated doc version

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """
-Cross-client test cases organized by fork. Each directory underneath `tests/` contains test cases
-corresponding to the fork in which the functionality under test was introduced a specific area of
-EVM functionality.
+Cross-client test cases organized by fork. Each directory underneath `tests/`
+contains test cases corresponding [to the fork](https://ethereum.org/en/history)
+in which the functionality-under-test was introduced.
 """
