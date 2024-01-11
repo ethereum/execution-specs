300604adfc4ba4aa749bc18d42c893085b73c6bf
Sam Wilson <sam@binarycake.ca>
2024-01-10 20:22:23 -0500

    Rewrite fork docstrings in markdown

--- src/ethereum/paris/__init__.py
+++ src/ethereum/paris/__init__.py
@@ -1,8 +1,10 @@
 """
-Ethereum Paris Hardfork
-^^^^^^^^^^^^^^^^^^^^^^^
+The Paris fork transitions Ethereum from a proof-of-work consensus model to a
+proof-of-stake one. This fork is often referred to as "The Merge" because it
+marks the integration of the [consensus layer] with the execution layer
+(defined in this project.)
 
-The Fourteenth Ethereum hardfork.
+[consensus layer]: https://github.com/ethereum/consensus-specs
 """
 
 from ethereum.fork_criteria import ByBlockNumber
--- src/ethereum/shanghai/__init__.py
+++ src/ethereum/shanghai/__init__.py
@@ -1,8 +1,7 @@
 """
-Ethereum Shanghai Hardfork
-^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-The Fourteenth Ethereum hardfork.
+The Shanghai fork brings staking withdrawals to the execution layer, adds a
+push-zero EVM instruction, limits the maximum size of initialization
+bytecode, and deprecates the self-destruct EVM instruction.
 """
 
 from ethereum.fork_criteria import ByTimestamp
