13bd33d910e1400ec91ad4e663d7a29e84d6c9c9
Guruprasad Kamath <guru241987@gmail.com>
2024-01-25 10:20:40 +0100

    Impement EIP-4844

--- src/ethereum/cancun/vm/precompiled_contracts/__init__.py
+++ src/ethereum/cancun/vm/precompiled_contracts/__init__.py
@@ -25,6 +25,7 @@ __all__ = (
     "ALT_BN128_MUL_ADDRESS",
     "ALT_BN128_PAIRING_CHECK_ADDRESS",
     "BLAKE2F_ADDRESS",
+    "POINT_EVALUATION_ADDRESS",
 )
 
 ECRECOVER_ADDRESS = hex_to_address("0x01")
@@ -36,3 +37,4 @@ ALT_BN128_ADD_ADDRESS = hex_to_address("0x06")
 ALT_BN128_MUL_ADDRESS = hex_to_address("0x07")
 ALT_BN128_PAIRING_CHECK_ADDRESS = hex_to_address("0x08")
 BLAKE2F_ADDRESS = hex_to_address("0x09")
+POINT_EVALUATION_ADDRESS = hex_to_address("0x0a")
