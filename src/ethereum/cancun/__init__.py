68ce1911f4de1ba8fc12fccaab907d408720e566
Sam Wilson <sam@binarycake.ca>
2024-02-05 11:37:21 -0500

    Apply #862 to cancun

--- src/ethereum/cancun/__init__.py
+++ src/ethereum/cancun/__init__.py
@@ -1,8 +1,8 @@
 """
-Ethereum Cancun Hardfork
-^^^^^^^^^^^^^^^^^^^^^^^^^^
-
-The Fifteenth  Ethereum hardfork.
+The Cancun fork introduces transient storage, exposes beacon chain roots,
+introduces a new blob-carrying transaction type, adds a memory copying
+instruction, limits self-destruct to only work for contracts created in the
+same transaction, and adds an instruction to read the blob base fee.
 """
 
 from ethereum.fork_criteria import Unscheduled
