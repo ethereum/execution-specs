ddf44a014b3ac0b8b7bcc2382c7c654d34bd59c5
Guruprasad Kamath <guru241987@gmail.com>
2024-02-16 15:40:40 +0100

    remove type ignore comment

--- src/ethereum/cancun/vm/precompiled_contracts/point_evaluation.py
+++ src/ethereum/cancun/vm/precompiled_contracts/point_evaluation.py
@@ -11,7 +11,7 @@ Introduction
 
 Implementation of the POINT EVALUATION precompiled contract.
 """
-from eth2spec.deneb.mainnet import (  # type: ignore
+from eth2spec.deneb.mainnet import (
     KZGCommitment,
     kzg_commitment_to_versioned_hash,
     verify_kzg_proof,
