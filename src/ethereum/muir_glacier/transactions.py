0e24f11f0bad1cb387ca375e891e667955eea39d
Md Amaan <intellectance022@gmail.com>
2024-02-22 17:31:32 +0530

    muir-glacier refactored

---
+++ src/ethereum/muir_glacier/transactions.py
@@ -0,0 +1,41 @@
+
+from dataclasses import dataclass
+from typing import  Union
+
+from ..base_types import (
+    U256,
+    Bytes,
+    Bytes0,
+    Bytes8,
+    Bytes20,
+    Bytes32,
+    Bytes256,
+    Uint,
+    slotted_freezable,
+)
+
+Address = Bytes20
+
+
+TX_BASE_COST = 21000
+TX_DATA_COST_PER_NON_ZERO = 16
+TX_DATA_COST_PER_ZERO = 4
+TX_CREATE_COST = 32000
+
+
+@slotted_freezable
+@dataclass
+class Transaction:
+    """
+    Atomic operation performed on the block chain.
+    """
+
+    nonce: U256
+    gas_price: Uint
+    gas: Uint
+    to: Union[Bytes0, Address]
+    value: U256
+    data: Bytes
+    v: U256
+    r: U256
+    s: U256
