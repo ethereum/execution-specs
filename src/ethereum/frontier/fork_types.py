ffd1b2170e2b8aff9fad5de2b2a47906d65d7393
Md Amaan <intellectance022@gmail.com>
2024-02-22 17:23:03 +0530

    frontier and gray-glacier  refactored

--- src/ethereum/frontier/fork_types.py
+++ src/ethereum/frontier/fork_types.py
@@ -14,6 +14,7 @@ Types re-used throughout the specification, which are specific to Ethereum.
 
 from dataclasses import dataclass
 from typing import Tuple, Union
+from .transactions import Transaction
 
 from .. import rlp
 from ..base_types import (
@@ -33,28 +34,6 @@ Address = Bytes20
 Root = Hash32
 
 Bloom = Bytes256
-
-TX_BASE_COST = 21000
-TX_DATA_COST_PER_NON_ZERO = 68
-TX_DATA_COST_PER_ZERO = 4
-
-
-@slotted_freezable
-@dataclass
-class Transaction:
-    """
-    Atomic operation performed on the block chain.
-    """
-
-    nonce: U256
-    gas_price: Uint
-    gas: Uint
-    to: Union[Bytes0, Address]
-    value: U256
-    data: Bytes
-    v: U256
-    r: U256
-    s: U256
 
 
 @slotted_freezable
