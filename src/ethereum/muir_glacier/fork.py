bb48616ba3fe9b67462d3e105ab3697fca4b0844
Richard Gregory <richardgrecoson@gmail.com>
2024-02-28 16:20:37 +0100

    Refactor includability check for Muir Glacier.

--- src/ethereum/muir_glacier/fork.py
+++ src/ethereum/muir_glacier/fork.py
@@ -307,6 +307,7 @@ def validate_proof_of_work(header: Header) -> None:
 
 
 def check_transaction(
+    env: vm.Environment,
     tx: Transaction,
     gas_available: Uint,
     chain_id: U64,
@@ -316,6 +317,8 @@ def check_transaction(
 
     Parameters
     ----------
+    env :
+        Environment for the Ethereum Virtual Machine.
     tx :
         The transaction.
     gas_available :
@@ -333,6 +336,13 @@ def check_transaction(
     InvalidBlock :
         If the transaction is not includable.
     """
+    sender = env.origin
+    sender_account = get_account(env.state, sender)
+    gas_fee = tx.gas * tx.gas_price
+    ensure(sender_account.nonce == tx.nonce, InvalidBlock)
+    ensure(sender_account.balance >= gas_fee + tx.value, InvalidBlock)
+    ensure(sender_account.code == bytearray(), InvalidBlock)
+
     ensure(tx.gas <= gas_available, InvalidBlock)
     sender_address = recover_sender(chain_id, tx)
 
@@ -650,9 +660,6 @@ def process_transaction(
     sender = env.origin
     sender_account = get_account(env.state, sender)
     gas_fee = tx.gas * tx.gas_price
-    ensure(sender_account.nonce == tx.nonce, InvalidBlock)
-    ensure(sender_account.balance >= gas_fee + tx.value, InvalidBlock)
-    ensure(sender_account.code == bytearray(), InvalidBlock)
 
     gas = tx.gas - calculate_intrinsic_cost(tx)
     increment_nonce(env.state, sender)
