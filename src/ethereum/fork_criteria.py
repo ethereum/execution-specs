4d53e69314d8e0b9cc1bd09b511b35f1bf863235
Sam Wilson <sam@binarycake.ca>
2024-04-09 20:15:58 -0400

    Markdown-ify fork_criteria.py

--- src/ethereum/fork_criteria.py
+++ src/ethereum/fork_criteria.py
@@ -1,15 +1,27 @@
 """
-Fork Criteria
-^^^^^^^^^^^^^
+Activation criteria for forks.
 
-.. contents:: Table of Contents
-    :backlinks: none
-    :local:
+Most generally, a _fork_ is a divergence in a blockchain resulting in multiple
+tips. Most forks are short lived, and can be caused by networking issues or the
+behavior of block creators. These short-lived forks resolve themselves
+according to the rules of the protocol, eventually settling back to a single
+tip of the chain.
 
-Introduction
-------------
+A second class of forks are intentionally created by changing the rules of the
+protocol, and never resolve back to a single tip. Older software will continue
+to follow the original fork, while updated software will create and follow a
+new fork.
 
-Classes for specifying criteria for Mainnet forks.
+For these intentional forks to succeed, all participants need to agree on
+exactly when to switch rules. The agreed upon criteria are represented by
+subclasses of [`ForkCriteria`], like [`ByBlockNumber`] and [`ByTimestamp`]. The
+special type of [`Unscheduled`] is used for forks in active development that do
+not yet have a scheduled deployment.
+
+[`ForkCriteria`]: ref:ethereum.fork_criteria.ForkCriteria
+[`ByBlockNumber`]: ref:ethereum.fork_criteria.ByBlockNumber
+[`ByTimestamp`]: ref:ethereum.fork_criteria.ByTimestamp
+[`Unscheduled`]: ref:ethereum.fork_criteria.Unscheduled
 """
 
 import functools
@@ -20,12 +32,37 @@ from typing import Final, Tuple
 @functools.total_ordering
 class ForkCriteria(ABC):
     """
-    Type that represents the condition required for a fork to occur.
+    Abstract base class for conditions specifying when a fork activates.
+
+    Subclasses override the comparison methods ([`__eq__`] and [`__lt__`]) to
+    provide an ordering for forks, and override [`check`] to determine whether
+    a particular block meets the activation criteria.
+
+    [`__eq__`]: ref:ethereum.fork_criteria.ForkCriteria.__eq__
+    [`__lt__`]: ref:ethereum.fork_criteria.ForkCriteria.__lt__
+    [`check`]: ref:ethereum.fork_criteria.ForkCriteria.check
     """
 
     BLOCK_NUMBER: Final[int] = 0
+    """
+    Value representing a fork criteria based on the block's number.
+
+    Used for pre-merge blocks.
+    """
+
     TIMESTAMP: Final[int] = 1
+    """
+    Value representing a fork criteria based on the block's timestamp.
+
+    Used for post-merge blocks.
+    """
+
     UNSCHEDULED: Final[int] = 2
+    """
+    Value representing a fork criteria that will never be satisfied.
+
+    Used for in-development forks.
+    """
 
     _internal: Tuple[int, int]
 
@@ -39,8 +76,15 @@ class ForkCriteria(ABC):
 
     def __lt__(self, other: object) -> bool:
         """
-        Ordering for fork criteria. Block number forks are before timestamp
-        forks and scheduled forks are before unscheduled forks.
+        Less-than comparison function, with earlier forks being less than later
+        forks.
+
+        All [`BLOCK_NUMBER`] forks come before [`TIMESTAMP`] forks, and all
+        scheduled forks come before [`UNSCHEDULED`] forks.
+
+        [`BLOCK_NUMBER`]: ref:ethereum.fork_criteria.ForkCriteria.BLOCK_NUMBER
+        [`TIMESTAMP`]: ref:ethereum.fork_criteria.ForkCriteria.TIMESTAMP
+        [`UNSCHEDULED`]: ref:ethereum.fork_criteria.ForkCriteria.UNSCHEDULED
         """
         if isinstance(other, ForkCriteria):
             return self._internal < other._internal
@@ -48,7 +92,7 @@ class ForkCriteria(ABC):
 
     def __hash__(self) -> int:
         """
-        `ForkCriteria` is hashable, so it can be stored in dictionaries.
+        Compute a hash for this instance, so it can be stored in dictionaries.
         """
         return hash(self._internal)
 
@@ -56,8 +100,11 @@ class ForkCriteria(ABC):
     def check(self, block_number: int, timestamp: int) -> bool:
         """
         Check whether fork criteria have been met.
+
+        Returns `True` when the current block meets or exceeds the criteria,
+        and `False` otherwise.
         """
-        ...
+        raise NotImplementedError()
 
     @abstractmethod
     def __repr__(self) -> str:
@@ -73,6 +120,9 @@ class ByBlockNumber(ForkCriteria):
     """
 
     block_number: int
+    """
+    Number of the first block in this fork.
+    """
 
     def __init__(self, block_number: int):
         self._internal = (ForkCriteria.BLOCK_NUMBER, block_number)
@@ -81,6 +131,11 @@ class ByBlockNumber(ForkCriteria):
     def check(self, block_number: int, timestamp: int) -> bool:
         """
         Check whether the block number has been reached.
+
+        Returns `True` when the given `block_number` is equal to or greater
+        than [`block_number`], and `False` otherwise.
+
+        [`block_number`]: ref:ethereum.fork_criteria.ByBlockNumber.block_number
         """
         return block_number >= self.block_number
 
@@ -97,6 +152,9 @@ class ByTimestamp(ForkCriteria):
     """
 
     timestamp: int
+    """
+    First instance of time that is part of this fork.
+    """
 
     def __init__(self, timestamp: int):
         self._internal = (ForkCriteria.TIMESTAMP, timestamp)
@@ -105,6 +163,11 @@ class ByTimestamp(ForkCriteria):
     def check(self, block_number: int, timestamp: int) -> bool:
         """
         Check whether the timestamp has been reached.
+
+        Returns `True` when the given `timestamp` is equal to or greater than
+        [`timestamp`], and `False` otherwise.
+
+        [`timestamp`]: ref:ethereum.fork_criteria.ByTimestamp.timestamp
         """
         return timestamp >= self.timestamp
 
@@ -125,7 +188,7 @@ class Unscheduled(ForkCriteria):
 
     def check(self, block_number: int, timestamp: int) -> bool:
         """
-        Unscheduled forks never occur.
+        Unscheduled forks never occur; always returns `False`.
         """
         return False
 
