ba456ea46da6fc48751c1c052b9a593d4fb5b376
Danno Ferrin <danno@numisight.com>
2024-04-22 19:13:41 -0600

    new(tests): EOF - Simple tests for DATA* series opcodes (#518)
    Creates a set of containers and tests DATA* operations along
    the test boundaries of zero, too little, just right, and
    more than enough.  Including validation tests.
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    Co-authored-by: Mario Vega <marioevz@gmail.com>

---
+++ prague/eip7480_data_section/test_code_validation.py
@@ -0,0 +1,210 @@
+"""
+EOF V1 Code Validation tests
+"""
+
+from typing import List
+
+import pytest
+
+from ethereum_test_tools import EOFException, EOFTestFiller
+from ethereum_test_tools.eof.v1 import Container, Section
+from ethereum_test_tools.eof.v1.constants import NON_RETURNING_SECTION
+from ethereum_test_tools.vm.opcode import Opcodes as Op
+
+from .spec import EOF_FORK_NAME
+
+REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7480.md"
+REFERENCE_SPEC_VERSION = "3ee1334ef110420685f1c8ed63e80f9e1766c251"
+
+pytestmark = pytest.mark.valid_from(EOF_FORK_NAME)
+
+VALID: List[Container] = [
+    Container(
+        name="empty_data_section",
+        sections=[
+            Section.Code(
+                code=Op.ADDRESS + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data=""),
+        ],
+    ),
+    Container(
+        name="small_data_section",
+        sections=[
+            Section.Code(
+                code=Op.ADDRESS + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 4),
+        ],
+    ),
+    Container(
+        name="large_data_section",
+        sections=[
+            Section.Code(
+                code=Op.ADDRESS + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 3 * 1024),
+        ],
+    ),
+    Container(
+        name="max_data_section",
+        sections=[
+            Section.Code(
+                code=Op.ADDRESS + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data=("1122334455667788" * 8 * 1024)[2:]),
+        ],
+    ),
+    Container(
+        name="DATALOADN_zero",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[0] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 16),
+        ],
+    ),
+    Container(
+        name="DATALOADN_middle",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[16] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 16),
+        ],
+    ),
+    Container(
+        name="DATALOADN_edge",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[128 - 32] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 16),
+        ],
+    ),
+    Container(
+        name="DATALOADN_max",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[0xFFFF - 32] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data=("1122334455667788" * 8 * 1024)[2:]),
+        ],
+    ),
+]
+
+INVALID: List[Container] = [
+    Container(
+        name="DATALOADN_max_empty_data",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[0xFFFF - 32] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+        ],
+        validity_error=EOFException.DEFAULT_EXCEPTION,
+    ),
+    Container(
+        name="DATALOADN_max_small_data",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[0xFFFF - 32] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data="1122334455667788" * 16),
+        ],
+        validity_error=EOFException.DEFAULT_EXCEPTION,
+    ),
+    Container(
+        name="DATALOADN_max_half_data",
+        sections=[
+            Section.Code(
+                code=Op.DATALOADN[0xFFFF - 32] + Op.POP + Op.STOP,
+                code_inputs=0,
+                code_outputs=NON_RETURNING_SECTION,
+                max_stack_height=1,
+            ),
+            Section.Data(data=("1122334455667788" * 4 * 1024)[2:]),
+        ],
+        validity_error=EOFException.DEFAULT_EXCEPTION,
+    ),
+]
+
+
+def container_name(c: Container):
+    """
+    Return the name of the container for use in pytest ids.
+    """
+    if hasattr(c, "name"):
+        return c.name
+    else:
+        return c.__class__.__name__
+
+
+@pytest.mark.parametrize(
+    "container",
+    VALID,
+    ids=container_name,
+)
+def test_legacy_initcode_valid_eof_v1_contract(
+    eof_test: EOFTestFiller,
+    container: Container,
+):
+    """
+    Test creating various types of valid EOF V1 contracts using legacy
+    initcode and a contract creating transaction.
+    """
+    assert (
+        container.validity_error is None
+    ), f"Valid container with validity error: {container.validity_error}"
+    eof_test(
+        data=container,
+    )
+
+
+@pytest.mark.parametrize(
+    "container",
+    INVALID,
+    ids=container_name,
+)
+def test_legacy_initcode_invalid_eof_v1_contract(
+    eof_test: EOFTestFiller,
+    container: Container,
+):
+    """
+    Test creating various types of valid EOF V1 contracts using legacy
+    initcode and a contract creating transaction.
+    """
+    assert container.validity_error is not None, "Invalid container without validity error"
+    eof_test(
+        data=container,
+        expect_exception=container.validity_error,
+    )
---
+++ prague/eip7480_data_section/test_data_opcodes.py
@@ -0,0 +1,205 @@
+"""
+Execution of CALLF, RETF opcodes within EOF V1 containers tests
+"""
+
+import pytest
+
+from ethereum_test_tools import (
+    Account,
+    Address,
+    Environment,
+    StateTestFiller,
+    TestAddress,
+    Transaction,
+)
+from ethereum_test_tools.eof.v1 import Container, Section
+from ethereum_test_tools.eof.v1.constants import MAX_CODE_SECTIONS, NON_RETURNING_SECTION
+from ethereum_test_tools.vm.opcode import Opcodes as Op
+
+from .spec import EOF_FORK_NAME
+
+REFERENCE_SPEC_GIT_PATH = "EIPS/eip-7480.md"
+REFERENCE_SPEC_VERSION = "3ee1334ef110420685f1c8ed63e80f9e1766c251"
+
+pytestmark = pytest.mark.valid_from(EOF_FORK_NAME)
+
+contract_call_within_deep_nested_callf = Container(
+    name="contract_call_within_deep_nested_callf",
+    sections=[
+        Section.Code(
+            code=(Op.CALLF[1] + Op.SSTORE(0, 1) + Op.STOP),
+            code_inputs=0,
+            code_outputs=NON_RETURNING_SECTION,
+            max_stack_height=2,
+        )
+    ]
+    + [
+        # All sections call next section and on return, store a 1
+        # to their call stack height key
+        Section.Code(
+            code=(Op.CALLF[i] + Op.SSTORE(i - 1, 1) + Op.RETF),
+            code_inputs=0,
+            code_outputs=0,
+            max_stack_height=2,
+        )
+        for i in range(2, MAX_CODE_SECTIONS)
+    ]
+    + [
+        # Last section makes external contract call
+        Section.Code(
+            code=(
+                Op.EXTCALL(0x200, 0, 0, 0) + Op.SSTORE(MAX_CODE_SECTIONS - 1, Op.ISZERO) + Op.RETF
+            ),
+            code_inputs=0,
+            code_outputs=0,
+            max_stack_height=4,
+        )
+    ],
+)
+
+recursive_contract_call_within_deep_nested_callf = Container(
+    name="recursive_contract_call_within_deep_nested_callf",
+    sections=[
+        # All sections call next section and on return, store a 1
+        # to their call stack height key
+        Section.Code(
+            code=(Op.CALLF[i + 1] + Op.PUSH1(1) + Op.PUSH2(i) + Op.SSTORE + Op.STOP),
+            code_inputs=0,
+            code_outputs=NON_RETURNING_SECTION,
+            max_stack_height=2,
+        )
+        for i in range(MAX_CODE_SECTIONS - 1)
+    ]
+    + [
+        # Last section makes external contract call
+        Section.Code(
+            code=(
+                Op.SSTORE(MAX_CODE_SECTIONS - 1, Op.CALL(Op.GAS, 0x200, 0, 0, 0, 0, 0)) + Op.RETF
+            ),
+            code_inputs=0,
+            code_outputs=0,
+            max_stack_height=7,
+        )
+    ],
+)
+
+
+def create_data_test(offset: int, datasize: int):
+    """
+    Generates data load operators test cases based on load offset and data section size.
+    """
+    data = b"".join(i.to_bytes(length=2, byteorder="big") for i in range(1, datasize // 2 + 1))
+    assert len(data) == datasize
+    overhang = min(32, offset + 32 - datasize)
+    answer = data[offset : offset + 32] if overhang <= 0 else data[offset:] + b"\x00" * overhang
+    dataloadn_op = Op.DATALOADN[offset] if overhang <= 0 else Op.PUSH32[answer]
+
+    return (
+        Container(
+            sections=[
+                Section.Code(
+                    code=(
+                        Op.CALLF[1]
+                        + Op.CALLF[2]
+                        + Op.CALLF[3]
+                        + Op.CALLF[4]
+                        + Op.SSTORE(0, 1)
+                        + Op.STOP
+                    ),
+                    code_inputs=0,
+                    code_outputs=NON_RETURNING_SECTION,
+                    max_stack_height=2,
+                ),
+                Section.Code(
+                    code=(Op.PUSH2(offset) + Op.DATALOAD + Op.PUSH1(1) + Op.SSTORE + Op.RETF),
+                    code_inputs=0,
+                    code_outputs=0,
+                    max_stack_height=2,
+                ),
+                Section.Code(
+                    code=(dataloadn_op + Op.PUSH1(2) + Op.SSTORE + Op.RETF),
+                    code_inputs=0,
+                    code_outputs=0,
+                    max_stack_height=2,
+                ),
+                Section.Code(
+                    code=(Op.DATASIZE + Op.PUSH1(3) + Op.SSTORE + Op.RETF),
+                    code_inputs=0,
+                    code_outputs=0,
+                    max_stack_height=2,
+                ),
+                Section.Code(
+                    code=(Op.DATACOPY(0, offset, 32) + Op.SSTORE(4, Op.MLOAD(0)) + Op.RETF),
+                    code_inputs=0,
+                    code_outputs=0,
+                    max_stack_height=3,
+                ),
+                Section.Data(data),
+            ],
+        ),
+        {0: 1, 1: answer, 2: answer, 3: datasize, 4: answer},
+    )
+
+
+@pytest.mark.parametrize(
+    ["offset", "datasize"],
+    [
+        pytest.param(0, 0, id="empty_zero"),
+        pytest.param(0, 2, id="short_zero"),
+        pytest.param(0, 32, id="exact_zero"),
+        pytest.param(0, 64, id="large_zero"),
+        pytest.param(32, 0, id="empty_32"),
+        pytest.param(32, 34, id="short_32"),
+        pytest.param(32, 64, id="exact_32"),
+        pytest.param(32, 96, id="large_32"),
+        pytest.param(0x5BFE, 0, id="empty_23k"),
+        pytest.param(0x5BFE, 0x5C00, id="short_23k"),
+        pytest.param(0x5BE0, 0x5D00, id="exact_23k"),
+        pytest.param(0x2345, 0x5C00, id="large_23k"),
+    ],
+)
+def test_data_section_succeed(
+    state_test: StateTestFiller,
+    offset: int,
+    datasize: int,
+):
+    """
+    Test simple contracts that are simply expected to succeed on call.
+    """
+    env = Environment()
+
+    caller_contract = Op.SSTORE(0, Op.DELEGATECALL(Op.GAS, 0x200, 0, 0, 0, 0)) + Op.STOP()
+    (container, expected_storage) = create_data_test(offset, datasize)
+
+    pre = {
+        TestAddress: Account(
+            balance=1000000000000000000000,
+            nonce=1,
+        ),
+        Address(0x100): Account(
+            code=caller_contract,
+            nonce=1,
+        ),
+        Address(0x200): Account(
+            code=container,
+            nonce=1,
+        ),
+    }
+
+    tx = Transaction(
+        nonce=1,
+        to=Address(0x100),
+        gas_limit=50000000,
+        gas_price=10,
+        protected=False,
+        data="",
+    )
+
+    post = {Address(0x100): Account(storage=expected_storage)}
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
