8232b60ef6e2df1eadf4319e8539fb696d0392d8
Guruprasad Kamath <guru241987@gmail.com>
2024-06-24 11:06:10 +0200

    prague-eof initial commit

---
+++ src/ethereum/prague/vm/__init__.py
@@ -0,0 +1,149 @@
+"""
+Ethereum Virtual Machine (EVM)
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. contents:: Table of Contents
+    :backlinks: none
+    :local:
+
+Introduction
+------------
+
+The abstract computer which runs the code stored in an
+`.fork_types.Account`.
+"""
+
+from dataclasses import dataclass
+from typing import List, Optional, Set, Tuple, Union
+
+from ethereum.base_types import U64, U256, Bytes, Bytes0, Bytes32, Uint
+from ethereum.crypto.hash import Hash32
+
+from ..blocks import Log
+from ..fork_types import Address, VersionedHash
+from ..state import State, TransientStorage, account_exists_and_is_empty
+from .precompiled_contracts import RIPEMD160_ADDRESS
+
+__all__ = ("Environment", "Evm", "Message")
+
+
+@dataclass
+class Environment:
+    """
+    Items external to the virtual machine itself, provided by the environment.
+    """
+
+    caller: Address
+    block_hashes: List[Hash32]
+    origin: Address
+    coinbase: Address
+    number: Uint
+    base_fee_per_gas: Uint
+    gas_limit: Uint
+    gas_price: Uint
+    time: U256
+    prev_randao: Bytes32
+    state: State
+    chain_id: U64
+    traces: List[dict]
+    excess_blob_gas: U64
+    blob_versioned_hashes: Tuple[VersionedHash, ...]
+    transient_storage: TransientStorage
+
+
+@dataclass
+class Message:
+    """
+    Items that are used by contract creation or message call.
+    """
+
+    caller: Address
+    target: Union[Bytes0, Address]
+    current_target: Address
+    gas: Uint
+    value: U256
+    data: Bytes
+    code_address: Optional[Address]
+    code: Bytes
+    depth: Uint
+    should_transfer_value: bool
+    is_static: bool
+    accessed_addresses: Set[Address]
+    accessed_storage_keys: Set[Tuple[Address, Bytes32]]
+    parent_evm: Optional["Evm"]
+
+
+@dataclass
+class Evm:
+    """The internal state of the virtual machine."""
+
+    pc: Uint
+    stack: List[U256]
+    memory: bytearray
+    code: Bytes
+    gas_left: Uint
+    env: Environment
+    valid_jump_destinations: Set[Uint]
+    logs: Tuple[Log, ...]
+    refund_counter: int
+    running: bool
+    message: Message
+    output: Bytes
+    accounts_to_delete: Set[Address]
+    touched_accounts: Set[Address]
+    return_data: Bytes
+    error: Optional[Exception]
+    accessed_addresses: Set[Address]
+    accessed_storage_keys: Set[Tuple[Address, Bytes32]]
+
+
+def incorporate_child_on_success(evm: Evm, child_evm: Evm) -> None:
+    """
+    Incorporate the state of a successful `child_evm` into the parent `evm`.
+
+    Parameters
+    ----------
+    evm :
+        The parent `EVM`.
+    child_evm :
+        The child evm to incorporate.
+    """
+    evm.gas_left += child_evm.gas_left
+    evm.logs += child_evm.logs
+    evm.refund_counter += child_evm.refund_counter
+    evm.accounts_to_delete.update(child_evm.accounts_to_delete)
+    evm.touched_accounts.update(child_evm.touched_accounts)
+    if account_exists_and_is_empty(
+        evm.env.state, child_evm.message.current_target
+    ):
+        evm.touched_accounts.add(child_evm.message.current_target)
+    evm.accessed_addresses.update(child_evm.accessed_addresses)
+    evm.accessed_storage_keys.update(child_evm.accessed_storage_keys)
+
+
+def incorporate_child_on_error(evm: Evm, child_evm: Evm) -> None:
+    """
+    Incorporate the state of an unsuccessful `child_evm` into the parent `evm`.
+
+    Parameters
+    ----------
+    evm :
+        The parent `EVM`.
+    child_evm :
+        The child evm to incorporate.
+    """
+    # In block 2675119, the empty account at 0x3 (the RIPEMD160 precompile) was
+    # cleared despite running out of gas. This is an obscure edge case that can
+    # only happen to a precompile.
+    # According to the general rules governing clearing of empty accounts, the
+    # touch should have been reverted. Due to client bugs, this event went
+    # unnoticed and 0x3 has been exempted from the rule that touches are
+    # reverted in order to preserve this historical behaviour.
+    if RIPEMD160_ADDRESS in child_evm.touched_accounts:
+        evm.touched_accounts.add(RIPEMD160_ADDRESS)
+    if child_evm.message.current_target == RIPEMD160_ADDRESS:
+        if account_exists_and_is_empty(
+            evm.env.state, child_evm.message.current_target
+        ):
+            evm.touched_accounts.add(RIPEMD160_ADDRESS)
+    evm.gas_left += child_evm.gas_left
---
+++ src/ethereum/prague/vm/precompiled_contracts/point_evaluation.py
@@ -0,0 +1,71 @@
+"""
+Ethereum Virtual Machine (EVM) POINT EVALUATION PRECOMPILED CONTRACT
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+.. contents:: Table of Contents
+    :backlinks: none
+    :local:
+
+Introduction
+------------
+
+Implementation of the POINT EVALUATION precompiled contract.
+"""
+from eth2spec.deneb.mainnet import (
+    KZGCommitment,
+    kzg_commitment_to_versioned_hash,
+    verify_kzg_proof,
+)
+
+from ethereum.base_types import U256, Bytes
+
+from ...vm import Evm
+from ...vm.exceptions import KZGProofError
+from ...vm.gas import GAS_POINT_EVALUATION, charge_gas
+
+FIELD_ELEMENTS_PER_BLOB = 4096
+BLS_MODULUS = 52435875175126190479447740508185965837690552500527637822603658699938581184513  # noqa: E501
+VERSIONED_HASH_VERSION_KZG = b"\x01"
+
+
+def point_evaluation(evm: Evm) -> None:
+    """
+    A pre-compile that verifies a KZG proof which claims that a blob
+    (represented by a commitment) evaluates to a given value at a given point.
+
+    Parameters
+    ----------
+    evm :
+        The current EVM frame.
+
+    """
+    data = evm.message.data
+    if len(data) != 192:
+        raise KZGProofError
+
+    versioned_hash = data[:32]
+    z = data[32:64]
+    y = data[64:96]
+    commitment = KZGCommitment(data[96:144])
+    proof = data[144:192]
+
+    # GAS
+    charge_gas(evm, GAS_POINT_EVALUATION)
+    if kzg_commitment_to_versioned_hash(commitment) != versioned_hash:
+        raise KZGProofError
+
+    # Verify KZG proof with z and y in big endian format
+    try:
+        kzg_proof_verification = verify_kzg_proof(commitment, z, y, proof)
+    except Exception as e:
+        raise KZGProofError from e
+
+    if not kzg_proof_verification:
+        raise KZGProofError
+
+    # Return FIELD_ELEMENTS_PER_BLOB and BLS_MODULUS as padded
+    # 32 byte big endian values
+    evm.output = Bytes(
+        U256(FIELD_ELEMENTS_PER_BLOB).to_be_bytes32()
+        + U256(BLS_MODULUS).to_be_bytes32()
+    )
