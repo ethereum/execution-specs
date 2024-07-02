2119bdf3b19348984c3fc6eaa74283ee53ef06b4
Guruprasad Kamath <guru241987@gmail.com>
2024-07-02 09:43:39 +0200

    Implement EIP-4200

--- src/ethereum/prague/vm/instructions/control_flow.py
+++ src/ethereum/prague/vm/instructions/control_flow.py
@@ -14,7 +14,16 @@ Implementations of the EVM control flow instructions.
 
 from ethereum.base_types import U256, Uint
 
-from ...vm.gas import GAS_BASE, GAS_HIGH, GAS_JUMPDEST, GAS_MID, charge_gas
+from ...vm.gas import (
+    GAS_BASE,
+    GAS_HIGH,
+    GAS_JUMPDEST,
+    GAS_MID,
+    GAS_RJUMP,
+    GAS_RJUMPI,
+    GAS_RJUMPV,
+    charge_gas,
+)
 from .. import Evm
 from ..exceptions import InvalidJumpDestError
 from ..stack import pop, push
@@ -169,3 +178,102 @@ def jumpdest(evm: Evm) -> None:
 
     # PROGRAM COUNTER
     evm.pc += 1
+
+
+def rjump(evm: Evm) -> None:
+    """
+    Jump to a relative offset.
+
+    Parameters
+    ----------
+    evm :
+        The current EVM frame.
+
+    """
+    # STACK
+    pass
+
+    # GAS
+    charge_gas(evm, GAS_RJUMP)
+
+    # OPERATION
+    pass
+
+    # PROGRAM COUNTER
+    relative_offset = int.from_bytes(
+        evm.code[evm.pc + 1 : evm.pc + 3], "big", signed=True
+    )
+    # pc + 1 + 2 bytes of relative offset
+    pc_post_instruction = int(evm.pc) + 3
+    evm.pc = Uint(pc_post_instruction + relative_offset)
+
+
+def rjumpi(evm: Evm) -> None:
+    """
+    Jump to a relative offset given a condition.
+
+    Parameters
+    ----------
+    evm :
+        The current EVM frame.
+
+    """
+    # STACK
+    condition = pop(evm.stack)
+
+    # GAS
+    charge_gas(evm, GAS_RJUMPI)
+
+    # OPERATION
+    pass
+
+    # PROGRAM COUNTER
+    relative_offset = int.from_bytes(
+        evm.code[evm.pc + 1 : evm.pc + 3], "big", signed=True
+    )
+    # pc + 1 + 2 bytes of relative offset
+    pc_post_instruction = int(evm.pc) + 3
+    if condition == 0:
+        evm.pc = Uint(pc_post_instruction)
+    else:
+        evm.pc = Uint(pc_post_instruction + relative_offset)
+
+
+def rjumpv(evm: Evm) -> None:
+    """
+    Jump to a relative offset via jump table.
+
+    Parameters
+    ----------
+    evm :
+        The current EVM frame.
+
+    """
+    # STACK
+    case = pop(evm.stack)
+
+    # GAS
+    charge_gas(evm, GAS_RJUMPV)
+
+    # OPERATION
+    pass
+
+    # PROGRAM COUNTER
+    max_index = evm.code[evm.pc + 1]
+    num_relative_indices = max_index + 1
+    # pc_post_instruction will be
+    # counter + 1 <- for normal pc increment to next opcode
+    # + 1 <- for the 1 byte max_index
+    # + 2 * num_relative_indices <- for the 2 bytes of each offset
+    pc_post_instruction = int(evm.pc) + 2 + 2 * num_relative_indices
+
+    if case > max_index:
+        evm.pc = Uint(pc_post_instruction)
+    else:
+        relative_offset_position = evm.pc + 2 + 2 * case
+        relative_offset = int.from_bytes(
+            evm.code[relative_offset_position : relative_offset_position + 2],
+            "big",
+            signed=True,
+        )
+        evm.pc = Uint(pc_post_instruction + relative_offset)
