a91b66b472fae9b2087295d006345e158c45cf39
Richard Gregory <richardgrecoson@gmail.com>
2024-08-07 13:47:23 +0100

    Modify Cancun's test_rlp and fork to use new Access type

--- src/ethereum/cancun/transactions.py
+++ src/ethereum/cancun/transactions.py
@@ -1,8 +1,3 @@
-"""
-Transactions are atomic units of work created externally to Ethereum and
-submitted to be executed. If Ethereum is viewed as a state machine,
-transactions are the events that move between states.
-"""
 from dataclasses import dataclass
 from typing import Tuple, Union
 
@@ -19,21 +14,17 @@ from ..base_types import (
 from ..exceptions import InvalidBlock
 from .fork_types import Address, VersionedHash
 
-TX_BASE_COST = 21000
-TX_DATA_COST_PER_NON_ZERO = 16
-TX_DATA_COST_PER_ZERO = 4
-TX_CREATE_COST = 32000
-TX_ACCESS_LIST_ADDRESS_COST = 2400
-TX_ACCESS_LIST_STORAGE_KEY_COST = 1900
+
+@slotted_freezable
+@dataclass
+class Access:
+    account: Address
+    slots: Tuple[Bytes32, ...]
 
 
 @slotted_freezable
 @dataclass
 class LegacyTransaction:
-    """
-    Atomic operation performed on the block chain.
-    """
-
     nonce: U256
     gas_price: Uint
     gas: Uint
@@ -47,18 +38,7 @@ class LegacyTransaction:
 
 @slotted_freezable
 @dataclass
-class Access:
-    account: Address
-    slots: Tuple[Bytes32, ...]
-
-
-@slotted_freezable
-@dataclass
 class AccessListTransaction:
-    """
-    The transaction type added in EIP-2930 to support access lists.
-    """
-
     chain_id: U64
     nonce: U256
     gas_price: Uint
@@ -75,10 +55,6 @@ class AccessListTransaction:
 @slotted_freezable
 @dataclass
 class FeeMarketTransaction:
-    """
-    The transaction type added in EIP-1559.
-    """
-
     chain_id: U64
     nonce: U256
     max_priority_fee_per_gas: Uint
@@ -96,10 +72,6 @@ class FeeMarketTransaction:
 @slotted_freezable
 @dataclass
 class BlobTransaction:
-    """
-    The transaction type added in EIP-4844.
-    """
-
     chain_id: U64
     nonce: U256
     max_priority_fee_per_gas: Uint
@@ -116,28 +88,6 @@ class BlobTransaction:
     s: U256
 
 
-# Helper function to handle the RLP encoding of the Access class instances.
-def encode_access_list(
-        access_list: Tuple[Access, ...]
-) -> Tuple[Tuple[Address, Tuple[Bytes32, ...]], ...]:
-    """
-    Encode the Access list for RLP encoding.
-    """
-    return tuple((access.account, access.slots) for access in access_list)
-
-
-# Helper function to handle the RLP decoding of the Access class instances.
-def decode_access_list(
-        encoded_access_list: Tuple[Tuple[Address, Tuple[Bytes32, ...]], ...]
-) -> Tuple[Access, ...]:
-    """
-    Decode the Access list from RLP encoding.
-    """
-    return tuple(
-        Access(account=encoded[0], slots=encoded[1]
-               ) for encoded in encoded_access_list)
-
-
 Transaction = Union[
     LegacyTransaction,
     AccessListTransaction,
@@ -147,44 +97,26 @@ Transaction = Union[
 
 
 def encode_transaction(tx: Transaction) -> Union[LegacyTransaction, Bytes]:
-    """
-    Encode a transaction. Needed because non-legacy transactions aren't RLP.
-    """
     if isinstance(tx, LegacyTransaction):
         return tx
     elif isinstance(tx, AccessListTransaction):
-        encoded_access_list = encode_access_list(tx.access_list)
-        return b"\x01" + rlp.encode(tx._replace(
-            access_list=encoded_access_list))
+        return b"\x01" + rlp.encode(tx)
     elif isinstance(tx, FeeMarketTransaction):
-        encoded_access_list = encode_access_list(tx.access_list)
-        return b"\x02" + rlp.encode(tx._replace(
-            access_list=encoded_access_list))
+        return b"\x02" + rlp.encode(tx)
     elif isinstance(tx, BlobTransaction):
-        encoded_access_list = encode_access_list(tx.access_list)
-        return b"\x03" + rlp.encode(tx._replace(
-            access_list=encoded_access_list))
+        return b"\x03" + rlp.encode(tx)
     else:
         raise Exception(f"Unable to encode transaction of type {type(tx)}")
 
 
 def decode_transaction(tx: Union[LegacyTransaction, Bytes]) -> Transaction:
-    """
-    Decode a transaction. Needed because non-legacy transactions aren't RLP.
-    """
     if isinstance(tx, Bytes):
         if tx[0] == 1:
-            decoded_tx = rlp.decode_to(AccessListTransaction, tx[1:])
-            decoded_access_list = decode_access_list(decoded_tx.access_list)
-            return decoded_tx._replace(access_list=decoded_access_list)
+            return rlp.decode_to(AccessListTransaction, tx[1:])
         elif tx[0] == 2:
-            decoded_tx = rlp.decode_to(FeeMarketTransaction, tx[1:])
-            decoded_access_list = decode_access_list(decoded_tx.access_list)
-            return decoded_tx._replace(access_list=decoded_access_list)
+            return rlp.decode_to(FeeMarketTransaction, tx[1:])
         elif tx[0] == 3:
-            decoded_tx = rlp.decode_to(BlobTransaction, tx[1:])
-            decoded_access_list = decode_access_list(decoded_tx.access_list)
-            return decoded_tx._replace(access_list=decoded_access_list)
+            return rlp.decode_to(BlobTransaction, tx[1:])
         else:
             raise InvalidBlock
     else:
--- tests/cancun/test_rlp.py
+++ tests/cancun/test_rlp.py
@@ -10,6 +10,7 @@ from ethereum.cancun.transactions import (
     Transaction,
     decode_transaction,
     encode_transaction,
+    Access
 )
 from ethereum.cancun.utils.hexadecimal import hex_to_address
 from ethereum.crypto.hash import keccak256
@@ -59,7 +60,7 @@ access_list_transaction = AccessListTransaction(
     Bytes0(),
     U256(4),
     Bytes(b"bar"),
-    ((address1, (hash1, hash2)), (address2, tuple())),
+    (Access(account=address1, slots=(hash1, hash2)), Access(account=address2, slots=tuple())),
     U256(27),
     U256(5),
     U256(6),
@@ -74,7 +75,7 @@ transaction_1559 = FeeMarketTransaction(
     Bytes0(),
     U256(4),
     Bytes(b"bar"),
-    ((address1, (hash1, hash2)), (address2, tuple())),
+    (Access(account=address1, slots=(hash1, hash2)), Access(account=address2, slots=tuple())),
     U256(27),
     U256(5),
     U256(6),
