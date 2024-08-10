31c3c0681d107e576e3313f9d0709ee03b6c7445
pdobacz <5735525+pdobacz@users.noreply.github.com>
2024-08-10 02:16:03 +0200

    new(tests): EOF - EIP-7069: Add tests, part 2. (#730)
    * new(tests) EOF - EIP-7069 - static call with value
    
    * new(tests) EOF - EIP-7069 - max msg depth
    
    * new(tests) EOF - EIP-7069 - not enough balance
    
    * new(tests) EOF - EIP-7069 - min_callee_gas/min_retained_gas
    
    * review suggestions
    
    ---------
    
    Co-authored-by: Mario Vega <marioevz@gmail.com>

--- prague/eip7692_eof_v1/eip7069_extcall/test_calls.py
+++ prague/eip7692_eof_v1/eip7069_extcall/test_calls.py
@@ -32,6 +32,7 @@ slot_returndata = next(_slot)
 slot_returndatasize = next(_slot)
 slot_caller = next(_slot)
 slot_returndatasize_before_clear = next(_slot)
+slot_max_depth = next(_slot)
 slot_last_slot = next(_slot)
 
 """Storage values for common testing fields"""
@@ -636,6 +637,288 @@ def test_eof_calls_clear_return_buffer(
     post = {
         calling_contract_address: Account(storage=calling_storage),
         filling_callee_address: Account(storage={}),
+    }
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
+
+
+@pytest.mark.parametrize(
+    "opcode",
+    [
+        Op.CALL,
+        Op.EXTCALL,
+    ],
+)
+def test_eof_calls_static_flag_with_value(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    sender: EOA,
+    opcode: Op,
+):
+    """Test EOF contracts calls handle static flag and sending value correctly"""
+    env = Environment()
+
+    noop_callee_address = pre.deploy_contract(Container.Code(Op.STOP))
+
+    failing_contract_code = opcode(address=noop_callee_address, value=1) + Op.STOP
+    failing_contract_address = pre.deploy_contract(
+        Container.Code(
+            failing_contract_code,
+        )
+        if opcode == Op.EXTCALL
+        else failing_contract_code
+    )
+
+    calling_contract_address = pre.deploy_contract(
+        Container.Code(
+            Op.SSTORE(slot_call_result, Op.EXTSTATICCALL(address=failing_contract_address))
+            + Op.SSTORE(slot_code_worked, value_code_worked)
+            + Op.STOP
+        )
+    )
+    tx = Transaction(
+        sender=sender,
+        to=Address(calling_contract_address),
+        gas_limit=5_000_000,
+        data="",
+    )
+
+    calling_storage = {
+        slot_code_worked: value_code_worked,
+        slot_call_result: value_eof_call_failed,
+    }
+
+    post = {
+        calling_contract_address: Account(storage=calling_storage),
+    }
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
+
+
+min_retained_gas = 2300
+min_callee_gas = 5000
+
+
+@pytest.mark.parametrize(
+    "opcode",
+    [
+        Op.EXTCALL,
+        Op.EXTSTATICCALL,
+        Op.EXTDELEGATECALL,
+    ],
+)
+@pytest.mark.parametrize(
+    ["extra_gas_limit", "reverts"],
+    [
+        [0, False],
+        [min_retained_gas, False],
+        [min_callee_gas, False],
+        [min_retained_gas + min_callee_gas, True],
+    ],
+    ids=["no_allowances", "only_retained", "only_callee", "both_allowances"],
+)
+def test_eof_calls_min_callee_gas(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    sender: EOA,
+    opcode: Op,
+    extra_gas_limit: int,
+    reverts: bool,
+):
+    """
+    Test EOF contracts calls do light failure when retained/callee gas is not enough.
+
+    Premise of the test is that there exists a range of `gas_limit` values, which are enough
+    for all instructions to execute, but call's returned value is 1, meaning not enough for gas
+    allowances (MIN_RETAINED_GAS and MIN_CALLEE_GAS) - ones marked with `reverts==False`.
+
+    Once we provide both allowances, the RJUMPI condition is no longer met and `reverts==True`.
+    """
+    env = Environment()
+
+    noop_callee_address = pre.deploy_contract(Container.Code(Op.STOP))
+
+    revert_block = Op.REVERT(0, 0)
+    calling_contract_address = pre.deploy_contract(
+        Container.Code(
+            Op.SSTORE(slot_code_worked, value_code_worked)
+            + Op.EQ(opcode(address=noop_callee_address), value_eof_call_reverted)
+            # If the return code isn't 1, it means gas was enough to cover the allowances.
+            + Op.RJUMPI[len(revert_block)]
+            + revert_block
+            + Op.STOP
+        )
+    )
+
+    # `no_oog_gas` is minimum amount of gas_limit which makes the transaction not go oog.
+    push_operations = 3 + len(opcode.kwargs)  # type: ignore
+    no_oog_gas = (
+        21_000
+        + 20_000  # SSTORE
+        + 2_100  # SSTORE COLD_SLOAD_COST
+        + push_operations * 3  # PUSH operations
+        + 100  # WARM_STORAGE_READ_COST
+        + 2500  # COLD_ACCOUNT_ACCESS - WARM_STORAGE_READ_COST
+        + 4  # RJUMPI
+        + 3  # EQ
+    )
+
+    tx = Transaction(
+        sender=sender,
+        to=Address(calling_contract_address),
+        gas_limit=no_oog_gas + extra_gas_limit,
+        data="",
+    )
+
+    calling_storage = {
+        slot_code_worked: 0 if reverts else value_code_worked,
+    }
+
+    post = {
+        calling_contract_address: Account(storage=calling_storage),
+    }
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
+
+
+def test_eof_calls_no_balance(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    sender: EOA,
+):
+    """Test EOF contracts calls handle value calls with not enough balance"""
+    env = Environment()
+
+    noop_callee_address = pre.deploy_contract(Container.Code(Op.STOP))
+
+    calling_contract_address = pre.deploy_contract(
+        Container.Code(
+            Op.SSTORE(slot_call_result, Op.EXTCALL(address=noop_callee_address, value=1))
+            + Op.SSTORE(slot_code_worked, value_code_worked)
+            + Op.STOP
+        )
+    )
+    tx = Transaction(
+        sender=sender,
+        to=Address(calling_contract_address),
+        gas_limit=50000000,
+        data="",
+    )
+
+    calling_storage = {
+        slot_code_worked: value_code_worked,
+        slot_call_result: value_eof_call_reverted,
+    }
+
+    post = {
+        calling_contract_address: Account(storage=calling_storage),
+    }
+
+    state_test(
+        env=env,
+        pre=pre,
+        post=post,
+        tx=tx,
+    )
+
+
+@pytest.mark.parametrize(
+    "opcode",
+    [
+        Op.EXTCALL,
+        Op.EXTDELEGATECALL,
+        Op.EXTSTATICCALL,
+    ],
+)
+def test_eof_calls_msg_depth(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    sender: EOA,
+    opcode: Op,
+):
+    """
+    Test EOF contracts calls handle msg depth limit correctly (1024).
+    NOTE: due to block gas limit and the 63/64th rule this limit is unlikely to be hit
+          on mainnet.
+    """
+    # Not a precise gas_limit formula, but enough to exclude risk of gas causing the failure.
+    gas_limit = int(200000 * (64 / 63) ** 1024)
+    env = Environment(gas_limit=gas_limit)
+
+    # Flow of the test:
+    # `callee_code` is recursively calling itself, passing msg depth as calldata
+    # (kept with the `MSTORE(0, ADD(...))`). When maximum msg depth is reached
+    # the call fails and starts returning. The deep-most frame returns:
+    #   - current reached msg depth (expected to be the maximum 1024), with the
+    #     `MSTORE(32, ADD(...))`
+    #   - the respective return code of the EXT*CALL (expected to be 1 - light failure), with the
+    #     `MSTORE(64, NOOP)`. Note the `NOOP` is just to appease the `Op.MSTORE` call, the return
+    #     code value is actually coming from the `Op.DUP1`
+    # When unwinding the msg call stack, the intermediate frames return whatever the deeper callee
+    # returned with the `RETURNDATACOPY` instruction.
+    returndatacopy_block = Op.RETURNDATACOPY(32, 0, 64) + Op.RETURN(32, 64)
+    deep_most_result_block = (
+        Op.MSTORE(32, Op.ADD(Op.CALLDATALOAD(0), 1)) + Op.MSTORE(64, Op.NOOP) + Op.RETURN(32, 64)
+    )
+    rjump_offset = len(returndatacopy_block)
+
+    callee_code = Container.Code(
+        # current stack depth in memory bytes 0-31
+        Op.MSTORE(0, Op.ADD(Op.CALLDATALOAD(0), 1))
+        # pass it along deeper as calldata
+        + opcode(address=Op.ADDRESS, args_size=32)
+        # duplicate return code for the `returndatacopy_block` below
+        + Op.DUP1
+        # if return code was:
+        #  - 1, we're in the deep-most frame, `deep_most_result_block` returns the actual result
+        #  - 0, we're in an intermediate frame, `returndatacopy_block` only passes on the result
+        + Op.RJUMPI[rjump_offset]
+        + returndatacopy_block
+        + deep_most_result_block
+    )
+
+    callee_address = pre.deploy_contract(callee_code)
+
+    calling_contract_address = pre.deploy_contract(
+        Container.Code(
+            Op.MSTORE(0, Op.CALLDATALOAD(0))
+            + Op.EXTCALL(address=callee_address, args_size=32)
+            + Op.SSTORE(slot_max_depth, Op.RETURNDATALOAD(0))
+            + Op.SSTORE(slot_call_result, Op.RETURNDATALOAD(32))
+            + Op.SSTORE(slot_code_worked, value_code_worked)
+            + Op.STOP
+        )
+    )
+    tx = Transaction(
+        sender=sender,
+        to=Address(calling_contract_address),
+        gas_limit=gas_limit,
+        data="",
+    )
+
+    calling_storage = {
+        slot_max_depth: 1024,
+        slot_code_worked: value_code_worked,
+        slot_call_result: value_eof_call_reverted,
+    }
+
+    post = {
+        calling_contract_address: Account(storage=calling_storage),
     }
 
     state_test(
