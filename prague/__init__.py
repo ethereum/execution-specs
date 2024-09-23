0c8310e9526bf6cc5c5886f700673a8f40a5bdb6
danceratopz <danceratopz@gmail.com>
2024-09-23 08:24:21 +0200

    feat(docs): re-write test gen doc flow with pytest plugin (#801)
    Co-authored-by: Mario Vega <marioevz@gmail.com>

--- prague/__init__.py
+++ prague/__init__.py
@@ -1,3 +1,10 @@
 """
-Test cases for EVM functionality introduced in Prague.
-"""
+Test cases for EVM functionality introduced in Prague, [EIP-7600: Hardfork Meta - Pectra](https://eip.directory/eips/eip-7600).
+
+Devnet Specifications:
+
+- [ethpandaops/pectra-devnet-4](https://notes.ethereum.org/@ethpandaops/pectra-devnet-4).
+- [ethpandaops/pectra-devnet-3](https://notes.ethereum.org/@ethpandaops/pectra-devnet-3).
+- [ethpandaops/pectra-devnet-2](https://notes.ethereum.org/@ethpandaops/pectra-devnet-2).
+- [ethpandaops/pectra-devnet-1](https://notes.ethereum.org/@ethpandaops/pectra-devnet-1).
+"""  # noqa: E501
