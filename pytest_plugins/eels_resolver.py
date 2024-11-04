daaaa70e6b7c059801328123eb0a9608e5843015
Mario Vega <marioevz@gmail.com>
2024-11-04 14:31:45 -0600

    fix(plugins/eels_resolver): add `tryfirst` (#937)

--- pytest_plugins/eels_resolver.py
+++ pytest_plugins/eels_resolver.py
@@ -14,6 +14,7 @@ import pytest
 from pytest_metadata.plugin import metadata_key  # type: ignore
 
 
+@pytest.hookimpl(tryfirst=True)
 def pytest_configure(config: pytest.Config) -> None:
     """
     Set the EELS_RESOLUTIONS_FILE environment variable.
