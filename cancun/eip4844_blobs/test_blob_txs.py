42e76897b01b4237a39566a30843d6cac1d4e6ab
spencer <spencer.taylor-brown@ethereum.org>
2024-12-05 07:01:58 +0700

    feat(fw,tests): EIP-7742: Framework changes (#931)
    * feat(fw,tests): framework changes for eip7742.
    
    * chore: tox fixes.
    
    * fix(tests): don't collect blob tx tests pre-cancun
    
    * fix: don't write target_blobs_per_block to the payload params
    
    * fix: fix-up new payload fixture tests
    
    * fix: bug in execution payload test.
    
    * docs: add changelog.
    
    * feat: updates for payload attributes v4.
    
    * fix: filling with EELS correctly.
    
    * fix(tests): EIP-4844: Invalid blob count tests in Prague
    
    * fix(specs): Propagate `target_blobs_per_block` from `Block` to `Environment`
    
    * fix(tests): EIP-4844: fix fixture
    
    * fix(plugins/execute): Merge error
    
    ---------
    
    Co-authored-by: danceratopz <danceratopz@gmail.com>
    Co-authored-by: Mario Vega <marioevz@gmail.com>

--- cancun/eip4844_blobs/test_blob_txs.py
+++ cancun/eip4844_blobs/test_blob_txs.py
@@ -20,7 +20,7 @@ from typing import List, Optional, Tuple
 
 import pytest
 
-from ethereum_test_forks import Fork
+from ethereum_test_forks import Fork, Prague
 from ethereum_test_tools import (
     EOA,
     AccessList,
@@ -290,14 +290,24 @@ def tx_access_list() -> List[AccessList]:
 
 
 @pytest.fixture
-def tx_error() -> Optional[TransactionException]:
+def tx_error(
+    request: pytest.FixtureRequest,
+    fork: Fork,
+) -> Optional[TransactionException]:
     """
     Default expected error produced by the block transactions (no error).
 
     Can be overloaded on test cases where the transactions are expected
     to fail.
     """
-    return None
+    if not hasattr(request, "param"):
+        return None
+    if fork >= Prague and request.param in [
+        TransactionException.TYPE_3_TX_BLOB_COUNT_EXCEEDED,
+        TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED,
+    ]:
+        return None
+    return request.param
 
 
 @pytest.fixture
@@ -736,7 +746,10 @@ def test_invalid_normal_gas(
     invalid_blob_combinations(),
 )
 @pytest.mark.parametrize(
-    "tx_error", [TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED], ids=[""]
+    "tx_error",
+    [TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED],
+    ids=[""],
+    indirect=True,
 )
 @pytest.mark.valid_from("Cancun")
 def test_invalid_block_blob_count(
@@ -1007,6 +1020,7 @@ def test_insufficient_balance_blob_tx_combinations(
         ),
     ],
     ids=["too_few_blobs", "too_many_blobs"],
+    indirect=["tx_error"],
 )
 @pytest.mark.valid_from("Cancun")
 def test_invalid_tx_blob_count(
