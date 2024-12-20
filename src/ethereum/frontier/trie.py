678325284038d9aafca8a7594e97f8544d698947
enitrat <msaug@protonmail.com>
2024-12-20 14:23:23 +0100

    dev: change type of branch node subnodes

--- src/ethereum/frontier/trie.py
+++ src/ethereum/frontier/trie.py
@@ -24,6 +24,7 @@ from typing import (
     MutableMapping,
     Optional,
     Sequence,
+    Tuple,
     TypeVar,
     Union,
 )
@@ -96,7 +97,7 @@ class ExtensionNode:
 class BranchNode:
     """Branch node in the Merkle Trie"""
 
-    subnodes: List[rlp.Extended]
+    subnodes: Tuple[rlp.Extended, ...]
     value: rlp.Extended
 
 
@@ -136,7 +137,7 @@ def encode_internal_node(node: Optional[InternalNode]) -> rlp.Extended:
             node.subnode,
         )
     elif isinstance(node, BranchNode):
-        unencoded = node.subnodes + [node.value]
+        unencoded = list(node.subnodes) + [node.value]
     else:
         raise AssertionError(f"Invalid internal node type {type(node)}!")
 
@@ -460,9 +461,9 @@ def patricialize(
             branches[key[level]][key] = obj[key]
 
     return BranchNode(
-        [
+        (
             encode_internal_node(patricialize(branches[k], level + Uint(1)))
             for k in range(16)
-        ],
+        ),
         value,
     )
