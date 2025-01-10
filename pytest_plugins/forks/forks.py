63a3c55aa70e8322ed71a95be5cfea3ba81197e4
danceratopz <danceratopz@gmail.com>
2025-01-10 20:02:58 +0100

    fix(pytest): add the parametrize_by_fork marker to the ini config (#1069)
    This fixes warning of the type:
    ```
      ./tests/cancun/eip4844_blobs/test_blob_txs.py:373: PytestUnknownMarkWarning: Unknown pytest.mark.parametrize_by_fork - is this a typo?  You can register custom marks to avoid this warning - for details, see https://docs.pytest.org/en/stable/how-to/mark.html
        @pytest.mark.parametrize_by_fork
    ```

--- pytest_plugins/forks/forks.py
+++ pytest_plugins/forks/forks.py
@@ -405,7 +405,13 @@ def pytest_configure(config: pytest.Config):
         "markers",
         "valid_until(fork): specifies until which fork a test case is valid",
     )
-
+    config.addinivalue_line(
+        "markers",
+        (
+            "parametrize_by_fork(names, values_fn): parametrize a test case by fork using the "
+            "specified names and values returned by the function values_fn(fork)"
+        ),
+    )
     for d in fork_covariant_decorators:
         config.addinivalue_line("markers", f"{d.marker_name}: {d.description}")
 
