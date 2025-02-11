83e050908c572ab9de8945c5905e12853dd4d7bd
Guruprasad Kamath <guru241987@gmail.com>
2025-02-11 12:29:35 +0100

    add note about prague timestamp

--- src/ethereum/prague/__init__.py
+++ src/ethereum/prague/__init__.py
@@ -4,4 +4,7 @@ The Prague fork.
 
 from ethereum.fork_criteria import ByTimestamp
 
+# TODO: Note that this is a dummy timestamp since the
+# timestamp for Prague is not finalised at the time of creating this PR.
+# The timestamp will have to be updated once Prague goes live.
 FORK_CRITERIA = ByTimestamp(1710338138)
