d892758f796a3c59a4bc870cdf23bb60d5db5b34
Guruprasad Kamath <guru241987@gmail.com>
2025-02-16 10:58:03 +0100

    port byzantium

--- src/ethereum/byzantium/utils/message.py
+++ src/ethereum/byzantium/utils/message.py
@@ -12,87 +12,68 @@ Introduction
 Message specific functions used in this byzantium version of
 specification.
 """
-from typing import Optional, Union
-
 from ethereum_types.bytes import Bytes, Bytes0
-from ethereum_types.numeric import U256, Uint
+from ethereum_types.numeric import Uint
 
 from ..fork_types import Address
 from ..state import get_account
-from ..vm import Environment, Message
+from ..transactions import Transaction
+from ..vm import BlockEnvironment, Message, TransactionEnvironment
 from .address import compute_contract_address
 
 
 def prepare_message(
-    caller: Address,
-    target: Union[Bytes0, Address],
-    value: U256,
-    data: Bytes,
-    gas: Uint,
-    env: Environment,
-    code_address: Optional[Address] = None,
-    should_transfer_value: bool = True,
-    is_static: bool = False,
+    block_env: BlockEnvironment,
+    tx_env: TransactionEnvironment,
+    tx: Transaction,
 ) -> Message:
     """
     Execute a transaction against the provided environment.
 
     Parameters
     ----------
-    caller :
-        Address which initiated the transaction
-    target :
-        Address whose code will be executed
-    value :
-        Value to be transferred.
-    data :
-        Array of bytes provided to the code in `target`.
-    gas :
-        Gas provided for the code in `target`.
-    env :
+    block_env :
         Environment for the Ethereum Virtual Machine.
-    code_address :
-        This is usually same as the `target` address except when an alternative
-        accounts code needs to be executed.
-        eg. `CALLCODE` calling a precompile.
-    should_transfer_value :
-        if True ETH should be transferred while executing a message call.
-    is_static:
-        if True then it prevents all state-changing operations from being
-        executed.
+    tx_env :
+        Environment for the transaction.
+    tx :
+        Transaction to be executed.
 
     Returns
     -------
     message: `ethereum.byzantium.vm.Message`
         Items containing contract creation or message call specific data.
     """
-    if isinstance(target, Bytes0):
+    if isinstance(tx.to, Bytes0):
         current_target = compute_contract_address(
-            caller,
-            get_account(env.state, caller).nonce - Uint(1),
+            tx_env.origin,
+            get_account(block_env.state, tx_env.origin).nonce - Uint(1),
         )
         msg_data = Bytes(b"")
-        code = data
-    elif isinstance(target, Address):
-        current_target = target
-        msg_data = data
-        code = get_account(env.state, target).code
-        if code_address is None:
-            code_address = target
+        code = tx.data
+        code_address = None
+    elif isinstance(tx.to, Address):
+        current_target = tx.to
+        msg_data = tx.data
+        code = get_account(block_env.state, tx.to).code
+
+        code_address = tx.to
     else:
         raise AssertionError("Target must be address or empty bytes")
 
     return Message(
-        caller=caller,
-        target=target,
-        gas=gas,
-        value=value,
+        block_env=block_env,
+        tx_env=tx_env,
+        caller=tx_env.origin,
+        target=tx.to,
+        gas=tx_env.gas,
+        value=tx.value,
         data=msg_data,
         code=code,
         depth=Uint(0),
         current_target=current_target,
         code_address=code_address,
-        should_transfer_value=should_transfer_value,
-        is_static=is_static,
+        should_transfer_value=True,
+        is_static=False,
         parent_evm=None,
     )
--- tests/byzantium/test_state_transition.py
+++ tests/byzantium/test_state_transition.py
@@ -2,12 +2,7 @@ from functools import partial
 from typing import Dict
 
 import pytest
-from ethereum_rlp import rlp
-from ethereum_types.bytes import Bytes, Bytes8, Bytes32
-from ethereum_types.numeric import U256, Uint
 
-from ethereum.crypto.hash import Hash32, keccak256
-from ethereum.exceptions import InvalidBlock
 from tests.helpers import TEST_FIXTURES
 from tests.helpers.load_state_tests import (
     Load,
@@ -101,90 +96,3 @@ non_legacy_only_in = (
 )
 def test_non_legacy_state_tests(test_case: Dict) -> None:
     run_byzantium_blockchain_st_tests(test_case)
-
-
-def test_transaction_with_insufficient_balance_for_value() -> None:
-    genesis_header = FIXTURES_LOADER.fork.Header(
-        parent_hash=Hash32([0] * 32),
-        ommers_hash=Hash32.fromhex(
-            "1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347"
-        ),
-        coinbase=FIXTURES_LOADER.fork.hex_to_address(
-            "8888f1f195afa192cfee860698584c030f4c9db1"
-        ),
-        state_root=FIXTURES_LOADER.fork.hex_to_root(
-            "d84598d90e2a72125c111171717f5508fd40ed0d0cd067ceb4e734d4da3a810a"
-        ),
-        transactions_root=FIXTURES_LOADER.fork.hex_to_root(
-            "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
-        ),
-        receipt_root=FIXTURES_LOADER.fork.hex_to_root(
-            "56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421"
-        ),
-        bloom=FIXTURES_LOADER.fork.Bloom([0] * 256),
-        difficulty=Uint(0x020000),
-        number=Uint(0x00),
-        gas_limit=Uint(0x2FEFD8),
-        gas_used=Uint(0x00),
-        timestamp=U256(0x54C98C81),
-        extra_data=Bytes([0x42]),
-        mix_digest=Bytes32([0] * 32),
-        nonce=Bytes8([0] * 8),
-    )
-
-    genesis_header_hash = bytes.fromhex(
-        "0b22b0d49035cb4f8a969d584f36126e0ac6996b9db7264ac5a192b8698177eb"
-    )
-
-    assert keccak256(rlp.encode(genesis_header)) == genesis_header_hash
-
-    genesis_block = FIXTURES_LOADER.fork.Block(
-        genesis_header,
-        (),
-        (),
-    )
-
-    state = FIXTURES_LOADER.fork.State()
-
-    address = FIXTURES_LOADER.fork.hex_to_address(
-        "a94f5374fce5edbc8e2a8697c15331677e6ebf0b"
-    )
-
-    account = FIXTURES_LOADER.fork.Account(
-        nonce=Uint(0),
-        balance=U256(0x056BC75E2D63100000),
-        code=Bytes(),
-    )
-
-    FIXTURES_LOADER.fork.set_account(state, address, account)
-
-    tx = FIXTURES_LOADER.fork.Transaction(
-        nonce=U256(0x00),
-        gas_price=Uint(1000),
-        gas=Uint(150000),
-        to=FIXTURES_LOADER.fork.hex_to_address(
-            "c94f5374fce5edbc8e2a8697c15331677e6ebf0b"
-        ),
-        value=U256(1000000000000000000000),
-        data=Bytes(),
-        v=U256(0),
-        r=U256(0),
-        s=U256(0),
-    )
-
-    env = FIXTURES_LOADER.fork.Environment(
-        caller=address,
-        origin=address,
-        block_hashes=[genesis_header_hash],
-        coinbase=genesis_block.header.coinbase,
-        number=genesis_block.header.number + Uint(1),
-        gas_limit=genesis_block.header.gas_limit,
-        gas_price=tx.gas_price,
-        time=genesis_block.header.timestamp,
-        difficulty=genesis_block.header.difficulty,
-        state=state,
-        traces=[],
-    )
-
-    with pytest.raises(InvalidBlock):
-        FIXTURES_LOADER.fork.process_transaction(env, tx)
