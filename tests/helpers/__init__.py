3ebdf314909ab880b1e9a80fbbf9e516977a80ed
Guruprasad Kamath <guru241987@gmail.com>
2025-02-25 16:14:45 +0100

    remove execution_spec_tests entry
    The tests from execution_spec_tests should now be a part of EEST

--- tests/helpers/__init__.py
+++ tests/helpers/__init__.py
@@ -1,10 +1,6 @@
 # Update the links and commit has in order to consume
 # newer/other tests
 TEST_FIXTURES = {
-    "execution_spec_tests": {
-        "url": "https://github.com/ethereum/execution-spec-tests/releases/download/v0.2.5/fixtures.tar.gz",
-        "fixture_path": "tests/fixtures/execution_spec_tests",
-    },
     "evm_tools_testdata": {
         "url": "https://github.com/gurukamath/evm-tools-testdata.git",
         "commit_hash": "792422d",
