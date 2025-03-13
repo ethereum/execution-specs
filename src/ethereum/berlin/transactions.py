1049c73d3ea9421e4b85dd8cded19601b356f577
Carson <carsonwx@gmail.com>
2025-03-13 17:12:21 -0400

    AccessListTransaction now uses the new Access class

--- src/ethereum/berlin/transactions.py
+++ src/ethereum/berlin/transactions.py
@@ -70,7 +70,7 @@ class AccessListTransaction:
     to: Union[Bytes0, Address]
     value: U256
     data: Bytes
-    access_list: Tuple[Tuple[Address, Tuple[Bytes32, ...]], ...]
+    access_list: Tuple[Access, ...]
     y_parity: U256
     r: U256
     s: U256
