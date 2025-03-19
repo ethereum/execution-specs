051f2ee7b39f58fcbe7d3f80889ca6990ffeec04
Guruprasad Kamath <guru241987@gmail.com>
2025-03-19 15:31:28 +0100

    update documentation in interpreter

--- src/ethereum/frontier/vm/interpreter.py
+++ src/ethereum/frontier/vm/interpreter.py
@@ -140,8 +140,6 @@ def process_create_message(message: Message) -> Evm:
     ----------
     message :
         Transaction specific items.
-    env :
-        External items required for EVM execution.
 
     Returns
     -------
@@ -177,14 +175,12 @@ def process_create_message(message: Message) -> Evm:
 
 def process_message(message: Message) -> Evm:
     """
-    Executes a call to create a smart contract.
+    Move ether and execute the relevant code.
 
     Parameters
     ----------
     message :
         Transaction specific items.
-    env :
-        External items required for EVM execution.
 
     Returns
     -------
@@ -223,8 +219,6 @@ def execute_code(message: Message) -> Evm:
     ----------
     message :
         Transaction specific items.
-    env :
-        External items required for EVM execution.
 
     Returns
     -------
