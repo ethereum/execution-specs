90e915e7a705b0f23d1e2968d8f9785d71c749b9
Guruprasad Kamath <guru241987@gmail.com>
2025-03-25 09:53:25 +0100

    osaka initial commit

---
+++ src/ethereum/osaka/fork_types.py
@@ -0,0 +1,79 @@
+"""
+Ethereum Types
+^^^^^^^^^^^^^^
+
+.. contents:: Table of Contents
+    :backlinks: none
+    :local:
+
+Introduction
+------------
+
+Types re-used throughout the specification, which are specific to Ethereum.
+"""
+
+from dataclasses import dataclass
+
+from ethereum_rlp import rlp
+from ethereum_types.bytes import Bytes, Bytes20, Bytes256
+from ethereum_types.frozen import slotted_freezable
+from ethereum_types.numeric import U8, U64, U256, Uint
+
+from ..crypto.hash import Hash32, keccak256
+
+Address = Bytes20
+Root = Hash32
+VersionedHash = Hash32
+
+Bloom = Bytes256
+
+
+@slotted_freezable
+@dataclass
+class Account:
+    """
+    State associated with an address.
+    """
+
+    nonce: Uint
+    balance: U256
+    code: bytes
+
+
+EMPTY_ACCOUNT = Account(
+    nonce=Uint(0),
+    balance=U256(0),
+    code=bytearray(),
+)
+
+
+def encode_account(raw_account_data: Account, storage_root: Bytes) -> Bytes:
+    """
+    Encode `Account` dataclass.
+
+    Storage is not stored in the `Account` dataclass, so `Accounts` cannot be
+    encoded without providing a storage root.
+    """
+    return rlp.encode(
+        (
+            raw_account_data.nonce,
+            raw_account_data.balance,
+            storage_root,
+            keccak256(raw_account_data.code),
+        )
+    )
+
+
+@slotted_freezable
+@dataclass
+class Authorization:
+    """
+    The authorization for a set code transaction.
+    """
+
+    chain_id: U256
+    address: Address
+    nonce: U64
+    y_parity: U8
+    r: U256
+    s: U256
