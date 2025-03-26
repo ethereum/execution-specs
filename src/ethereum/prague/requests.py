1a716a51e0a9a6592c5914dc74c81a40433c5350
Guruprasad Kamath <guru241987@gmail.com>
2025-03-26 15:37:49 +0100

    check deposit log validity

--- src/ethereum/prague/requests.py
+++ src/ethereum/prague/requests.py
@@ -13,6 +13,7 @@ from typing import List, Union
 
 from ethereum_types.bytes import Bytes
 
+from ethereum.exceptions import InvalidBlock
 from ethereum.utils.hexadecimal import hex_to_bytes32
 
 from .blocks import Receipt, decode_receipt
@@ -29,17 +30,108 @@ WITHDRAWAL_REQUEST_TYPE = b"\x01"
 CONSOLIDATION_REQUEST_TYPE = b"\x02"
 
 
+DEPOSIT_EVENT_LENGTH = 576
+
+PUBKEY_OFFSET = 160
+WITHDRAWAL_CREDENTIALS_OFFSET = 256
+AMOUNT_OFFSET = 320
+SIGNATURE_OFFSET = 384
+INDEX_OFFSET = 512
+
+PUBKEY_SIZE = 48
+WITHDRAWAL_CREDENTIALS_SIZE = 32
+AMOUNT_SIZE = 8
+SIGNATURE_SIZE = 96
+INDEX_SIZE = 8
+
+
 def extract_deposit_data(data: Bytes) -> Bytes:
     """
     Extracts Deposit Request from the DepositContract.DepositEvent data.
+
+    Raises
+    ------
+    InvalidBlock :
+        If the deposit contract did not produce a valid log.
     """
-    return (
-        data[192:240]  # public_key
-        + data[288:320]  # withdrawal_credentials
-        + data[352:360]  # amount
-        + data[416:512]  # signature
-        + data[544:552]  # index
+    if len(data) != DEPOSIT_EVENT_LENGTH:
+        raise InvalidBlock("Invalid deposit event data length")
+
+    # Check that all the offsets are in order
+    pubkey_offset = int.from_bytes(data[0:32], "big")
+    if pubkey_offset != PUBKEY_OFFSET:
+        raise InvalidBlock("Invalid pubkey offset in deposit log")
+
+    withdrawal_credentials_offset = int.from_bytes(data[32:64], "big")
+    if withdrawal_credentials_offset != WITHDRAWAL_CREDENTIALS_OFFSET:
+        raise InvalidBlock(
+            "Invalid withdrawal credentials offset in deposit log"
+        )
+
+    amount_offset = int.from_bytes(data[64:96], "big")
+    if amount_offset != AMOUNT_OFFSET:
+        raise InvalidBlock("Invalid amount offset in deposit log")
+
+    signature_offset = int.from_bytes(data[96:128], "big")
+    if signature_offset != SIGNATURE_OFFSET:
+        raise InvalidBlock("Invalid signature offset in deposit log")
+
+    index_offset = int.from_bytes(data[128:160], "big")
+    if index_offset != INDEX_OFFSET:
+        raise InvalidBlock("Invalid index offset in deposit log")
+
+    # Check that all the sizes are in order
+    pubkey_size = int.from_bytes(
+        data[pubkey_offset : pubkey_offset + 32], "big"
     )
+    if pubkey_size != PUBKEY_SIZE:
+        raise InvalidBlock("Invalid pubkey size in deposit log")
+
+    pubkey = data[pubkey_offset + 32 : pubkey_offset + 32 + PUBKEY_SIZE]
+
+    withdrawal_credentials_size = int.from_bytes(
+        data[
+            withdrawal_credentials_offset : withdrawal_credentials_offset + 32
+        ],
+        "big",
+    )
+    if withdrawal_credentials_size != WITHDRAWAL_CREDENTIALS_SIZE:
+        raise InvalidBlock(
+            "Invalid withdrawal credentials size in deposit log"
+        )
+
+    withdrawal_credentials = data[
+        withdrawal_credentials_offset
+        + 32 : withdrawal_credentials_offset
+        + 32
+        + WITHDRAWAL_CREDENTIALS_SIZE
+    ]
+
+    amount_size = int.from_bytes(
+        data[amount_offset : amount_offset + 32], "big"
+    )
+    if amount_size != AMOUNT_SIZE:
+        raise InvalidBlock("Invalid amount size in deposit log")
+
+    amount = data[amount_offset + 32 : amount_offset + 32 + AMOUNT_SIZE]
+
+    signature_size = int.from_bytes(
+        data[signature_offset : signature_offset + 32], "big"
+    )
+    if signature_size != SIGNATURE_SIZE:
+        raise InvalidBlock("Invalid signature size in deposit log")
+
+    signature = data[
+        signature_offset + 32 : signature_offset + 32 + SIGNATURE_SIZE
+    ]
+
+    index_size = int.from_bytes(data[index_offset : index_offset + 32], "big")
+    if index_size != INDEX_SIZE:
+        raise InvalidBlock("Invalid index size in deposit log")
+
+    index = data[index_offset + 32 : index_offset + 32 + INDEX_SIZE]
+
+    return pubkey + withdrawal_credentials + amount + signature + index
 
 
 def parse_deposit_requests_from_receipt(
@@ -51,14 +143,13 @@ def parse_deposit_requests_from_receipt(
     deposit_requests: Bytes = b""
     decoded_receipt = decode_receipt(receipt)
     for log in decoded_receipt.logs:
-        is_deposit_event = (
-            len(log.topics) > 0
-            and log.topics[0] == DEPOSIT_EVENT_SIGNATURE_HASH
-        )
-
-        if log.address == DEPOSIT_CONTRACT_ADDRESS and is_deposit_event:
-            request = extract_deposit_data(log.data)
-            deposit_requests += request
+        if log.address == DEPOSIT_CONTRACT_ADDRESS:
+            if (
+                len(log.topics) > 0
+                and log.topics[0] == DEPOSIT_EVENT_SIGNATURE_HASH
+            ):
+                request = extract_deposit_data(log.data)
+                deposit_requests += request
 
     return deposit_requests
 
