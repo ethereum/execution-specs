9f2dc72aae7987926e4220993ea9bb9098eaf626
Mario Vega <marioevz@gmail.com>
2025-04-08 14:16:22 -0600

    refactor(exceptions,specs): Support general block exceptions - Pydantic context to parse using mapper (#1396)
    * feat(clis,exceptions,specs): Pydantic validation using exception mapper
    
    * fix(specs): Skip block exception check on requests modifications
    
    * fix(docs): Links
    
    * fix(tests): EIP-4844: mark block as skip block exception verification
    
    * refactor(exceptions): Mapper mappings allow regex, convert from `property` to `ClassVar`
    
    * fix(docs): tox
    
    * chore(deps): remove now unused bdict dependency
    
    * Suggestions for #1396, refactor(exceptions,specs): Support general block exceptions - Pydantic context to parse using mapper (#1404)
    
    * refactor(fw): use enum helper; rename `ty` to `execution_context`
    
    * refactor(fw): use more specific names for exception classes
    
    * fix(tests): Add comment explaining `skip_exception_verification`
    
    * feat(clis): Add `reliable` flag to exception mapper class
    
    * fix: tests
    
    * fix(specs,clis,tools): Use `default_t8n` and remove `run_in_serial` from most unit tests
    
    * fix(specs): Bug and variable names
    
    * fix: bug
    
    * feat(clis/evmone): Add insufficient-blob-fee error
    
    * fix(tests): Allow similar blob exceptions
    
    * Apply suggestions from code review
    
    Co-authored-by: danceratopz <danceratopz@gmail.com>
    
    ---------
    
    Co-authored-by: danceratopz <danceratopz@gmail.com>

--- cancun/eip4844_blobs/test_blob_txs.py
+++ cancun/eip4844_blobs/test_blob_txs.py
@@ -563,7 +563,14 @@ def test_invalid_normal_gas(
     SpecHelpers.invalid_blob_combinations,
 )
 @pytest.mark.parametrize(
-    "tx_error", [TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED], ids=[""]
+    "tx_error",
+    [
+        [
+            TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED,
+            TransactionException.TYPE_3_TX_BLOB_COUNT_EXCEEDED,
+        ]
+    ],
+    ids=[""],
 )
 @pytest.mark.valid_from("Cancun")
 def test_invalid_block_blob_count(
@@ -857,7 +864,10 @@ def generate_invalid_tx_blob_count_tests(
         ),
         pytest.param(
             [fork.max_blobs_per_block() + 1],
-            TransactionException.TYPE_3_TX_BLOB_COUNT_EXCEEDED,
+            [
+                TransactionException.TYPE_3_TX_MAX_BLOB_GAS_ALLOWANCE_EXCEEDED,
+                TransactionException.TYPE_3_TX_BLOB_COUNT_EXCEEDED,
+            ],
             id="too_many_blobs",
         ),
     ]
@@ -1020,6 +1030,10 @@ def test_invalid_blob_tx_contract_creation(
                     TransactionException.TYPE_3_TX_CONTRACT_CREATION,
                 ],
                 header_verify=header_verify,
+                # Skipped due to the T8N not receiving the invalid transaction,
+                # instead we are passing a valid transaction to T8N and then the transaction
+                # is replaced directly in the block RLP.
+                skip_exception_verification=True,
             )
         ],
         genesis_environment=env,
