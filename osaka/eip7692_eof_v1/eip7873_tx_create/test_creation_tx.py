81d86f1a3b560473578e62bbf6aa237a9e9652bf
Danno Ferrin <danno@numisight.com>
2025-04-17 17:20:33 -0600

    fix(tests): Create transaction sarting with `0xef` is a valid tx, but should fail execution (#1480)
    * create transaction with `ef` is a valid tx, fails execution
    
    The EIP-3541 handling of EOF code was to execute it as uncontained code,
    executing the first 0xef byte and reverting.
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    
    * ruff format
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>
    
    ---------
    
    Signed-off-by: Danno Ferrin <danno@numisight.com>

--- osaka/eip7692_eof_v1/eip7873_tx_create/test_creation_tx.py
+++ osaka/eip7692_eof_v1/eip7873_tx_create/test_creation_tx.py
@@ -2,7 +2,6 @@
 
 import pytest
 
-from ethereum_test_exceptions.exceptions import TransactionException
 from ethereum_test_tools import (
     Account,
     Alloc,
@@ -60,7 +59,6 @@ def test_legacy_create_tx_legacy_initcode_eof_bytecode(
 
 @pytest.mark.with_all_contract_creating_tx_types(selector=lambda tx_type: tx_type != 6)
 @pytest.mark.xfail(reason="evmone incorrectly deploys the contract")
-@pytest.mark.exception_test
 def test_legacy_create_tx_eof_initcode(
     state_test: StateTestFiller,
     pre: Alloc,
@@ -76,7 +74,6 @@ def test_legacy_create_tx_eof_initcode(
         to=None,
         gas_limit=100_000,
         data=smallest_initcode_subcontainer,
-        error=TransactionException.EOF_CREATION_TRANSACTION,
     )
 
     destination_contract_address = tx.created_contract
