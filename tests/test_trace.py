8e0ed343dfe90c81d8c9ad66c9c391834b8152a0
Sam Wilson <sam@binarycake.ca>
2025-04-22 13:24:51 -0400

    Add test for changing evm_trace (#994)

---
+++ tests/test_trace.py
@@ -0,0 +1,73 @@
+from typing import Optional, cast
+
+from ethereum_types.numeric import Uint
+
+import ethereum.trace
+
+
+def test_modify_evm_trace() -> None:
+    trace1: Optional[ethereum.trace.TraceEvent] = None
+    trace2: Optional[ethereum.trace.TraceEvent] = None
+
+    def tracer1(
+        evm: object,
+        event: ethereum.trace.TraceEvent,
+        trace_memory: bool = False,
+        trace_stack: bool = True,
+        trace_return_data: bool = False,
+    ) -> None:
+        nonlocal trace1
+        trace1 = event
+
+    def tracer2(
+        evm: object,
+        event: ethereum.trace.TraceEvent,
+        trace_memory: bool = False,
+        trace_stack: bool = True,
+        trace_return_data: bool = False,
+    ) -> None:
+        nonlocal trace2
+        trace2 = event
+
+    ethereum.trace.evm_trace = tracer1
+
+    from ethereum.prague.vm import Evm, Message
+    from ethereum.prague.vm.gas import charge_gas
+
+    evm = Evm(
+        pc=Uint(1),
+        stack=[],
+        memory=bytearray(),
+        code=b"",
+        gas_left=Uint(100),
+        valid_jump_destinations=set(),
+        logs=(),
+        refund_counter=0,
+        running=True,
+        message=cast(Message, object()),
+        output=b"",
+        accounts_to_delete=set(),
+        touched_accounts=set(),
+        return_data=b"",
+        error=None,
+        accessed_addresses=set(),
+        accessed_storage_keys=set(),
+    )
+
+    charge_gas(evm, Uint(5))
+
+    assert trace2 is None
+    assert isinstance(trace1, ethereum.trace.GasAndRefund)
+    assert trace1.gas_cost == 5
+
+    ethereum.trace.evm_trace = tracer2
+
+    charge_gas(evm, Uint(6))
+
+    # Check that the old event is unmodified.
+    assert isinstance(trace1, ethereum.trace.GasAndRefund)
+    assert trace1.gas_cost == 5
+
+    # Check that the new event is populated.
+    assert isinstance(trace2, ethereum.trace.GasAndRefund)
+    assert trace2.gas_cost == 6
