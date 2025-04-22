936ec948a6b51011fc984344ee617a78e8b403d7
Sam Wilson <sam@binarycake.ca>
2025-04-22 19:27:54 -0400

    Bump version

--- src/ethereum/__init__.py
+++ src/ethereum/__init__.py
@@ -18,7 +18,7 @@ possible, to aid in defining the behavior of Ethereum clients.
 """
 import sys
 
-__version__ = "1.17.0rc6.dev1"
+__version__ = "1.17.0rc6"
 
 #
 #  Ensure we can reach 1024 frames of recursion
