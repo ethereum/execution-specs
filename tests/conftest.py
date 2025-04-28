98b5e3941dc9b749b0609a780bc6cd8db4ad88fd
carsons-eels <carson@binarycake.ca>
2025-04-28 16:10:57 -0400

    Remove all fixture based tests
    - removed fixture tests from ethash tests
    - remove fixture code from test config
    - evm_tools tests all seem to be fixture based
    - remove test_evm_tools which is fixture based
    - remove test_stat_transition which is fixture based
    - remove fixtures from test_trie
    - remove evm_tools test, all fixture based
    - fix import order
    - remove all trie tests, all fixtures
    - remove all fixture based tests for test_block_operations
    - remove all fixture based tests for test_system_operations
    - remove all fixture based tests for test_transaction
    - remove all fixture based tests for test_stack_operations
    - remove all fixture based tests for test_arithmetic_operations
    - remove all fixture based tests for test_bitwise_logic_operations
    - forgot the berlin test_transaction during removal
    - remove all fixture based tests for test_control_flow_operations
    - remove all fixture based tests for test_environmental_operations.py
    - remove all fixture based tests for test_keccak.py
    - remove all fixture based tests for test_logging_operations.py
    - remove all fixture based tests for test_memory_operations.py
    - remove all fixture based tests for test_storage_operations.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,5 @@
-import os
-import shutil
-import tarfile
-import tempfile
-import urllib.request
-
-import git
 from _pytest.config import Config
 from _pytest.config.argparsing import Parser
-from filelock import SoftFileLock
-from git.exc import GitCommandError, InvalidGitRepositoryError
-from pytest import Session
-
-from tests.helpers import TEST_FIXTURES
 
 
 def pytest_addoption(parser: Parser) -> None:
@@ -56,76 +44,6 @@ def pytest_configure(config: Config) -> None:
         ethereum.trace.set_evm_trace(new_trace_function)
 
 
-def download_fixtures(url: str, location: str) -> None:
-    # xdist processes will all try to download the fixtures.
-    # Using lockfile to make it parallel safe
-    with SoftFileLock(f"{location}.lock"):
-        if not os.path.exists(location):
-            print(f"Downloading {location}...")
-            with tempfile.TemporaryFile() as tfile:
-                with urllib.request.urlopen(url) as response:
-                    shutil.copyfileobj(response, tfile)
-
-                tfile.seek(0)
-
-                with tarfile.open(fileobj=tfile, mode="r:gz") as tar:
-                    tar.extractall(location)
-        else:
-            print(f"{location} already available.")
-
-
-def git_clone_fixtures(url: str, commit_hash: str, location: str) -> None:
-    # xdist processes will all try to download the fixtures.
-    # Using lockfile to make it parallel safe
-    with SoftFileLock(f"{location}.lock"):
-        if not os.path.exists(location):
-            print(f"Cloning {location}...")
-            repo = git.Repo.clone_from(url, to_path=location)
-        else:
-            print(f"{location} already available.")
-            repo = git.Repo(location)
-
-        print(f"Checking out the correct commit {commit_hash}...")
-        branch = repo.heads["develop"]
-        # Try to checkout the relevant commit hash and if that fails
-        # fetch the latest changes and checkout the commit hash
-        try:
-            repo.git.checkout(commit_hash)
-        except GitCommandError:
-            repo.remotes.origin.fetch(branch.name)
-            repo.git.checkout(commit_hash)
-
-        # Check if the submodule head matches the parent commit
-        # If not, update the submodule
-        for submodule in repo.submodules:
-            # Initialize the submodule if not already initialized
-            try:
-                submodule_repo = submodule.module()
-            except InvalidGitRepositoryError:
-                submodule.update(init=True, recursive=True)
-                continue
-
-            # Commit expected by the parent repo
-            parent_commit = submodule.hexsha
-
-            # Actual submodule head
-            submodule_head = submodule_repo.head.commit.hexsha
-            if parent_commit != submodule_head:
-                submodule.update(init=True, recursive=True)
-
-
-def pytest_sessionstart(session: Session) -> None:  # noqa: U100
-    for _, props in TEST_FIXTURES.items():
-        fixture_path = props["fixture_path"]
-
-        try:
-            os.makedirs(os.path.dirname(fixture_path))
-        except FileExistsError:
-            pass
-
-        if "commit_hash" in props:
-            git_clone_fixtures(
-                props["url"], props["commit_hash"], fixture_path
-            )
-        else:
-            download_fixtures(props["url"], fixture_path)
+# def pytest_sessionstart() -> None:
+# TODO do we still need to do something here?
+#    return
