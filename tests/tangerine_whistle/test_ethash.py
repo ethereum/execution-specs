4b854e36ed639782a39f4924f8310cb304361aa5
carsons-eels <carson@binarycake.ca>
2025-04-28 16:10:57 -0400

    Remove all fixture based tests
    - removed fixture tests from ethash tests
    - remove fixture code from test config
    - evm_tools tests all seem to be fixture based
    - remove test_evm_tools which is fixture based
    - remove test_stat_transition which is fixture based
    - remove fixtures from test_trie
    - remove evm_tools test, all fixture based
    - fix import order
    - remove all trie tests, all fixtures
    - remove all fixture based tests for test_block_operations
    - remove all fixture based tests for test_system_operations
    - remove all fixture based tests for test_transaction
    - remove all fixture based tests for test_stack_operations
    - remove all fixture based tests for test_arithmetic_operations
    - remove all fixture based tests for test_bitwise_logic_operations
    - forgot the berlin test_transaction during removal
    - remove all fixture based tests for test_control_flow_operations
    - remove all fixture based tests for test_environmental_operations.py
    - remove all fixture based tests for test_keccak.py
    - remove all fixture based tests for test_logging_operations.py
    - remove all fixture based tests for test_memory_operations.py
    - remove all fixture based tests for test_storage_operations.py

--- tests/tangerine_whistle/test_ethash.py
+++ tests/tangerine_whistle/test_ethash.py
@@ -1,84 +1,12 @@
 import json
 import pkgutil
-from typing import Any, Dict, List, cast
+from typing import cast
 
 import pytest
-from ethereum_rlp import rlp
-from ethereum_types.numeric import Uint
 
-from ethereum.crypto.hash import keccak256
-from ethereum.ethash import (
-    cache_size,
-    dataset_size,
-    generate_cache,
-    generate_seed,
-    hashimoto_light,
-)
 from ethereum.tangerine_whistle.blocks import Header
-from ethereum.tangerine_whistle.fork import (
-    generate_header_hash_for_pow,
-    validate_proof_of_work,
-)
-from ethereum.utils.hexadecimal import (
-    hex_to_bytes,
-    hex_to_bytes8,
-    hex_to_bytes32,
-)
-from ethereum.utils.numeric import le_uint32_sequence_to_bytes
-from tests.helpers import TEST_FIXTURES
+from ethereum.tangerine_whistle.fork import validate_proof_of_work
 from tests.helpers.load_state_tests import Load
-
-ETHEREUM_TESTS_PATH = TEST_FIXTURES["ethereum_tests"]["fixture_path"]
-
-
-def test_ethtest_fixtures() -> None:
-    ethereum_tests = load_pow_test_fixtures()
-    for test in ethereum_tests:
-        header = test["header"]
-        assert header.nonce == test["nonce"]
-        assert header.mix_digest == test["mix_digest"]
-        assert generate_seed(header.number) == test["seed"]
-        assert cache_size(header.number) == test["cache_size"]
-        assert dataset_size(header.number) == test["dataset_size"]
-
-        header_hash = generate_header_hash_for_pow(header)
-        assert header_hash == test["header_hash"]
-
-        cache = generate_cache(header.number)
-        cache_hash = keccak256(
-            b"".join(
-                le_uint32_sequence_to_bytes(cache_item) for cache_item in cache
-            )
-        )
-        assert cache_hash == test["cache_hash"]
-
-        mix_digest, result = hashimoto_light(
-            header_hash, header.nonce, cache, dataset_size(header.number)
-        )
-        assert mix_digest == test["mix_digest"]
-        assert result == test["result"]
-
-
-def load_pow_test_fixtures() -> List[Dict[str, Any]]:
-    with open(
-        f"{ETHEREUM_TESTS_PATH}/PoWTests/ethash_tests.json"
-    ) as pow_test_file_handler:
-        return [
-            {
-                "nonce": hex_to_bytes8(raw_fixture["nonce"]),
-                "mix_digest": hex_to_bytes32(raw_fixture["mixHash"]),
-                "header": rlp.decode_to(
-                    Header, hex_to_bytes(raw_fixture["header"])
-                ),
-                "seed": hex_to_bytes32(raw_fixture["seed"]),
-                "result": hex_to_bytes32(raw_fixture["result"]),
-                "cache_size": Uint(raw_fixture["cache_size"]),
-                "dataset_size": Uint(raw_fixture["full_size"]),
-                "header_hash": hex_to_bytes32(raw_fixture["header_hash"]),
-                "cache_hash": hex_to_bytes32(raw_fixture["cache_hash"]),
-            }
-            for raw_fixture in json.load(pow_test_file_handler).values()
-        ]
 
 
 @pytest.mark.slow
