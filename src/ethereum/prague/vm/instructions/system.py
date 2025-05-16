ef8c79c978f757b04879fabb68048b3d0e57bdb5
Sam Wilson <sam@binarycake.ca>
2025-05-16 10:31:49 -0400

    Improve MessageCallGas documentation in Prague

--- src/ethereum/prague/vm/instructions/system.py
+++ src/ethereum/prague/vm/instructions/system.py
@@ -408,11 +408,11 @@ def call(evm: Evm) -> None:
     if sender_balance < value:
         push(evm.stack, U256(0))
         evm.return_data = b""
-        evm.gas_left += message_call_gas.stipend
+        evm.gas_left += message_call_gas.sub_call
     else:
         generic_call(
             evm,
-            message_call_gas.stipend,
+            message_call_gas.sub_call,
             value,
             evm.message.current_target,
             to,
@@ -492,11 +492,11 @@ def callcode(evm: Evm) -> None:
     if sender_balance < value:
         push(evm.stack, U256(0))
         evm.return_data = b""
-        evm.gas_left += message_call_gas.stipend
+        evm.gas_left += message_call_gas.sub_call
     else:
         generic_call(
             evm,
-            message_call_gas.stipend,
+            message_call_gas.sub_call,
             value,
             evm.message.current_target,
             to,
@@ -622,7 +622,7 @@ def delegatecall(evm: Evm) -> None:
     evm.memory += b"\x00" * extend_memory.expand_by
     generic_call(
         evm,
-        message_call_gas.stipend,
+        message_call_gas.sub_call,
         evm.message.value,
         evm.message.caller,
         evm.message.current_target,
@@ -695,7 +695,7 @@ def staticcall(evm: Evm) -> None:
     evm.memory += b"\x00" * extend_memory.expand_by
     generic_call(
         evm,
-        message_call_gas.stipend,
+        message_call_gas.sub_call,
         U256(0),
         evm.message.current_target,
         to,
