f7be94d51492abdbe2d504edccc395ad37daadfa
Antriksh Gwal <antrikshgwal@gmail.com>
2025-05-23 19:18:10 +0530

    Changed to_address to to_address_masked

--- src/ethereum/constantinople/vm/instructions/system.py
+++ src/ethereum/constantinople/vm/instructions/system.py
@@ -29,7 +29,7 @@ from ...state import (
 from ...utils.address import (
     compute_contract_address,
     compute_create2_contract_address,
-    to_address,
+    to_address_masked,
 )
 from .. import (
     Evm,
@@ -326,7 +326,7 @@ def call(evm: Evm) -> None:
     """
     # STACK
     gas = Uint(pop(evm.stack))
-    to = to_address(pop(evm.stack))
+    to = to_address_masked(pop(evm.stack))
     value = pop(evm.stack)
     memory_input_start_position = pop(evm.stack)
     memory_input_size = pop(evm.stack)
@@ -399,7 +399,7 @@ def callcode(evm: Evm) -> None:
     """
     # STACK
     gas = Uint(pop(evm.stack))
-    code_address = to_address(pop(evm.stack))
+    code_address = to_address_masked(pop(evm.stack))
     value = pop(evm.stack)
     memory_input_start_position = pop(evm.stack)
     memory_input_size = pop(evm.stack)
@@ -465,7 +465,7 @@ def selfdestruct(evm: Evm) -> None:
         The current EVM frame.
     """
     # STACK
-    beneficiary = to_address(pop(evm.stack))
+    beneficiary = to_address_masked(pop(evm.stack))
 
     # GAS
     gas_cost = GAS_SELF_DESTRUCT
@@ -537,7 +537,7 @@ def delegatecall(evm: Evm) -> None:
     """
     # STACK
     gas = Uint(pop(evm.stack))
-    code_address = to_address(pop(evm.stack))
+    code_address = to_address_masked(pop(evm.stack))
     memory_input_start_position = pop(evm.stack)
     memory_input_size = pop(evm.stack)
     memory_output_start_position = pop(evm.stack)
@@ -588,7 +588,7 @@ def staticcall(evm: Evm) -> None:
     """
     # STACK
     gas = Uint(pop(evm.stack))
-    to = to_address(pop(evm.stack))
+    to = to_address_masked(pop(evm.stack))
     memory_input_start_position = pop(evm.stack)
     memory_input_size = pop(evm.stack)
     memory_output_start_position = pop(evm.stack)
