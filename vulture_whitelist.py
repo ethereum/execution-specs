62f0a640fdad706159247e0de61457a5c5853abf
carsons-eels <carson@binarycake.ca>
2025-06-02 15:25:21 -0400

    Turn vulture fully on, remove dead code, whitelist
    - ignore all pytest functions
    - remove unused variables, functions, methods, and classes
    - create vulture whitelist and mark some code that either implicitly
      gets called or only gets called during test runs for vulture to ignore

---
+++ vulture_whitelist.py
@@ -0,0 +1,99 @@
+from ethereum.cancun.blocks import Withdrawal
+from ethereum.ethash import *
+from ethereum.fork_criteria import Unscheduled
+from ethereum_optimized.state_db import State
+from ethereum_spec_tools.docc import *
+from ethereum_spec_tools.evm_tools.daemon import _EvmToolHandler
+from ethereum_spec_tools.evm_tools.loaders.fixture_loader import Load
+from ethereum_spec_tools.evm_tools.loaders.transaction_loader import (
+    TransactionLoad,
+)
+from ethereum_spec_tools.evm_tools.t8n.env import Ommer
+from ethereum_spec_tools.evm_tools.t8n.evm_trace import Trace, FinalTrace
+from ethereum_spec_tools.lint.lints.glacier_forks_hygiene import (
+    GlacierForksHygiene,
+)
+from ethereum_spec_tools.lint.lints.import_hygiene import ImportHygiene
+from ethereum.trace import EvmTracer
+
+# src/ethereum/cancun/blocks.py
+Withdrawal.validator_index
+
+# src/ethereum/fork_criteria.py
+Unscheduled
+
+# src/ethereum/ethash.py
+ethash.generate_dataset
+
+# src/ethereum/trace.py
+EvmTracer.__call__
+
+# src/ethereum/optimized/state_db.py
+State.rollback_db_transaction
+
+# src/ethereum_spec_tools/docc.py
+docc.EthereumDiscover
+docc.EthereumBuilder
+docc.DiffSource.show_in_listing
+docc.FixIndexTransform
+docc.FixIndexTransform.transform
+docc.MinimizeDiffsTransform
+docc.MinimizeDiffsTransform.transform
+docc._FixIndexVisitor.enter
+docc._DoccAdapter.shallow_equals
+docc._DoccAdapter.shallow_hash
+docc._DoccApply.ascend
+docc._DoccApply.descend
+docc._DoccApply.insert
+docc._HardenVisitor.enter
+docc._MinimizeDiffsVisitor.enter
+docc.render_diff
+docc.render_before_after
+
+# src/ethereum_spec_tools/evm_tools/daemon.py
+_EvmToolHandler.do_POST
+_EvmToolHandler.log_request
+
+# src/ethereum_spec_tools/loaders/fixture_loader.py
+Load._network
+
+# src/ethereum_spec_tools/loaders/transaction_loader.py
+TransactionLoad.json_to_authorizations
+TransactionLoad.json_to_chain_id
+TransactionLoad.json_to_nonce
+TransactionLoad.json_to_gas
+TransactionLoad.json_to_to
+TransactionLoad.json_to_value
+TransactionLoad.json_to_data
+TransactionLoad.json_to_access_list
+TransactionLoad.json_to_gas_price
+TransactionLoad.json_to_max_fee_per_gas
+TransactionLoad.json_to_max_priority_fee_per_gas
+TransactionLoad.json_to_max_fee_per_blob_gas
+TransactionLoad.json_to_blob_versioned_hashes
+TransactionLoad.json_to_v
+TransactionLoad.json_to_y_parity
+TransactionLoad.json_to_r
+TransactionLoad.json_to_s
+
+# src/ethereum_spec_tools/evm_tools/t8n/env.py
+Ommer.delta
+
+# src/ethereum_spec_tools/evm_tools/t8n/evm_trace.py
+Trace.gasCost
+Trace.memSize
+Trace.returnData
+Trace.refund
+Trace.opName
+FinalTrace.gasUsed
+
+# src/ethereum_spec_tools/lint/lints/glacier_forks_hygiene.py
+GlacierForksHygiene
+GlacierForksHygiene.visit_AnnAssign
+
+# src/ethereum_spec_tools/lint/lints/glacier_forks_hygiene.py
+ImportHygiene
+ImportHygiene.visit_AnnAssign
+
+
+_children  # unused attribute (src/ethereum_spec_tools/docc.py:751)
