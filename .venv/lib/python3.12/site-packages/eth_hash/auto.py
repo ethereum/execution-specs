faa76c19c66e04426d4d7ba807036d48fc062da7
souradeep-das <dsouradeep2@gmail.com>
2025-06-05 15:52:34 +0200

    remove ignored files

--- .venv/lib/python3.12/site-packages/eth_hash/auto.py
+++
@@ -1,8 +0,0 @@
-from eth_hash.backends.auto import (
-    AutoBackend,
-)
-from eth_hash.main import (
-    Keccak256,
-)
-
-keccak = Keccak256(AutoBackend())
--- .venv/lib/python3.12/site-packages/py_ecc/secp256k1/__init__.py
+++
@@ -1,8 +0,0 @@
-from .secp256k1 import (
-    G,
-    N,
-    P,
-    ecdsa_raw_recover,
-    ecdsa_raw_sign,
-    privtopub,
-)
