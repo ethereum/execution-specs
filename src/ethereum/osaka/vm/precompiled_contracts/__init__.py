a100a35dd76aab57730e4881efbd7988cf4ec1b2
LouisTsai <q1030176@gmail.com>
2025-06-20 17:15:42 +0200

    feat: implement eip-7951

--- src/ethereum/osaka/vm/precompiled_contracts/__init__.py
+++ src/ethereum/osaka/vm/precompiled_contracts/__init__.py
@@ -33,6 +33,7 @@ __all__ = (
     "BLS12_PAIRING_ADDRESS",
     "BLS12_MAP_FP_TO_G1_ADDRESS",
     "BLS12_MAP_FP2_TO_G2_ADDRESS",
+    "P256VERIFY_ADDRESS",
 )
 
 ECRECOVER_ADDRESS = hex_to_address("0x01")
@@ -52,3 +53,4 @@ BLS12_G2_MSM_ADDRESS = hex_to_address("0x0e")
 BLS12_PAIRING_ADDRESS = hex_to_address("0x0f")
 BLS12_MAP_FP_TO_G1_ADDRESS = hex_to_address("0x10")
 BLS12_MAP_FP2_TO_G2_ADDRESS = hex_to_address("0x11")
+P256VERIFY_ADDRESS = hex_to_address("0x100")
