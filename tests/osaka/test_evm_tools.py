1f5b6212d1767eb1ce06b3c9a05dad999d5586a3
Sam Wilson <sam@binarycake.ca>
2025-06-25 19:53:53 -0400

    Initial Osaka commit

---
+++ tests/osaka/test_evm_tools.py
@@ -0,0 +1,64 @@
+from functools import partial
+from typing import Dict
+
+import pytest
+
+from tests.helpers import EEST_TESTS_PATH, ETHEREUM_TESTS_PATH
+from tests.helpers.load_evm_tools_tests import (
+    fetch_evm_tools_tests,
+    idfn,
+    load_evm_tools_test,
+)
+
+ETHEREUM_STATE_TESTS_DIR = f"{ETHEREUM_TESTS_PATH}/GeneralStateTests/"
+EEST_STATE_TESTS_DIR = f"{EEST_TESTS_PATH}/state_tests/"
+FORK_NAME = "Osaka"
+
+
+SLOW_TESTS = (
+    "GeneralStateTests/stTimeConsuming/CALLBlake2f_MaxRounds.json::CALLBlake2f_MaxRounds-fork_[Cancun-Osaka]-d0g0v0",
+    "GeneralStateTests/VMTests/vmPerformance/loopExp.json::loopExp-fork_[Cancun-Osaka]-d[0-14]g0v0",
+    "GeneralStateTests/VMTests/vmPerformance/loopMul.json::loopMul-fork_[Cancun-Osaka]-d[0-2]g0v0",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-bls_pairing_non-degeneracy-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-bls_pairing_bilinearity-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-bls_pairing_e(G1,-G2)=e(-G1,G2)-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-bls_pairing_e(aG1,bG2)=e(abG1,G2)-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-bls_pairing_e(aG1,bG2)=e(G1,abG2)-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-inf_pair-]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing.py::test_valid[fork_Osaka-state_test-multi_inf_pair-]",
+)
+
+
+# Define tests
+fetch_tests = partial(
+    fetch_evm_tools_tests,
+    fork_name=FORK_NAME,
+    slow_tests=SLOW_TESTS,
+)
+
+run_tests = partial(
+    load_evm_tools_test,
+    fork_name=FORK_NAME,
+)
+
+
+# Run tests from ethereum/tests
+@pytest.mark.evm_tools
+@pytest.mark.parametrize(
+    "test_case",
+    fetch_tests(ETHEREUM_STATE_TESTS_DIR),
+    ids=idfn,
+)
+def test_ethereum_tests_evm_tools(test_case: Dict) -> None:
+    run_tests(test_case)
+
+
+# Run EEST test fixtures
+@pytest.mark.evm_tools
+@pytest.mark.parametrize(
+    "test_case",
+    fetch_tests(EEST_STATE_TESTS_DIR),
+    ids=idfn,
+)
+def test_eest_evm_tools(test_case: Dict) -> None:
+    run_tests(test_case)
---
+++ tests/osaka/test_state_transition.py
@@ -0,0 +1,104 @@
+from functools import partial
+from typing import Dict
+
+import pytest
+
+from tests.helpers import EEST_TESTS_PATH, ETHEREUM_TESTS_PATH
+from tests.helpers.load_state_tests import (
+    Load,
+    fetch_state_test_files,
+    idfn,
+    run_blockchain_st_test,
+)
+
+ETHEREUM_BLOCKCHAIN_TESTS_DIR = f"{ETHEREUM_TESTS_PATH}/BlockchainTests/"
+EEST_BLOCKCHAIN_TESTS_DIR = f"{EEST_TESTS_PATH}/blockchain_tests/"
+NETWORK = "Osaka"
+PACKAGE = "osaka"
+
+SLOW_TESTS = (
+    # GeneralStateTests
+    "stTimeConsuming/CALLBlake2f_MaxRounds.json",
+    "stTimeConsuming/static_Call50000_sha256.json",
+    "vmPerformance/loopExp.json",
+    "vmPerformance/loopMul.json",
+    "QuadraticComplexitySolidity_CallDataCopy_d0g1v0_Osaka",
+    "CALLBlake2f_d9g0v0_Osaka",
+    "CALLCODEBlake2f_d9g0v0",
+    # GeneralStateTests
+    "stRandom/randomStatetest177.json",
+    "stCreateTest/CreateOOGafterMaxCodesize.json",
+    # ValidBlockTest
+    "bcExploitTest/DelegateCallSpam.json",
+    # InvalidBlockTest
+    "bcUncleHeaderValidity/nonceWrong.json",
+    "bcUncleHeaderValidity/wrongMixHash.json",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-bls_pairing_non-degeneracy-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-bls_pairing_bilinearity-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-bls_pairing_e\\(G1,-G2\\)=e\\(-G1,G2\\)-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-bls_pairing_e\\(aG1,bG2\\)=e\\(abG1,G2\\)-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-bls_pairing_e\\(aG1,bG2\\)=e\\(G1,abG2\\)-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-inf_pair-\\]",
+    "tests/osaka/eip2537_bls_12_381_precompiles/test_bls12_pairing\\.py\\:\\:test_valid\\[fork_Osaka-blockchain_test-multi_inf_pair-\\]",
+    "tests/osaka/eip2935_historical_block_hashes_from_state/test_block_hashes\\.py\\:\\:test_block_hashes_history\\[fork_Osaka-blockchain_test-full_history_plus_one_check_blockhash_first\\]",
+)
+
+# These are tests that are considered to be incorrect,
+# Please provide an explanation when adding entries
+IGNORE_TESTS = (
+    # ValidBlockTest
+    "bcForkStressTest/ForkStressTest.json",
+    "bcGasPricerTest/RPC_API_Test.json",
+    "bcMultiChainTest",
+    "bcTotalDifficultyTest",
+    # InvalidBlockTest
+    "bcForgedTest",
+    "bcMultiChainTest",
+    "GasLimitHigherThan2p63m1_Osaka",
+)
+
+# All tests that recursively create a large number of frames (50000)
+BIG_MEMORY_TESTS = (
+    # GeneralStateTests
+    "50000_",
+    "/stQuadraticComplexityTest/",
+    "/stRandom2/",
+    "/stRandom/",
+    "/stSpecialTest/",
+    "stTimeConsuming/",
+    "stBadOpcode/",
+    "stStaticCall/",
+)
+
+# Define Tests
+fetch_tests = partial(
+    fetch_state_test_files,
+    network=NETWORK,
+    ignore_list=IGNORE_TESTS,
+    slow_list=SLOW_TESTS,
+    big_memory_list=BIG_MEMORY_TESTS,
+)
+
+FIXTURES_LOADER = Load(NETWORK, PACKAGE)
+
+run_tests = partial(run_blockchain_st_test, load=FIXTURES_LOADER)
+
+
+# Run tests from ethereum/tests
+@pytest.mark.parametrize(
+    "test_case",
+    fetch_tests(ETHEREUM_BLOCKCHAIN_TESTS_DIR),
+    ids=idfn,
+)
+def test_ethereum_tests(test_case: Dict) -> None:
+    run_tests(test_case)
+
+
+# Run EEST test fixtures
+@pytest.mark.parametrize(
+    "test_case",
+    fetch_tests(EEST_BLOCKCHAIN_TESTS_DIR),
+    ids=idfn,
+)
+def test_eest_tests(test_case: Dict) -> None:
+    run_tests(test_case)
