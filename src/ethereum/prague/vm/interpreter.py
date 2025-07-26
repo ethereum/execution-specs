f4a53e5b58c7417f3d4e2d88a9b423bb34e4e056
Ezequiel Perez <eperez@itba.edu.ar>
2025-07-25 22:17:01 -0300

    fix: redundant storage destruction before account creation

--- src/ethereum/prague/vm/interpreter.py
+++ src/ethereum/prague/vm/interpreter.py
@@ -177,18 +177,6 @@ def process_create_message(message: Message) -> Evm:
     # take snapshot of state before processing the message
     begin_transaction(state, transient_storage)
 
-    # If the address where the account is being created has storage, it is
-    # destroyed. This can only happen in the following highly unlikely
-    # circumstances:
-    # * The address created by a `CREATE` call collides with a subsequent
-    #   `CREATE` or `CREATE2` call.
-    # * The first `CREATE` happened before Spurious Dragon and left empty
-    #   code.
-    destroy_storage(state, message.current_target)
-
-    # In the previously mentioned edge case the preexisting storage is ignored
-    # for gas refund purposes. In order to do this we must track created
-    # accounts.
     mark_account_created(state, message.current_target)
 
     increment_nonce(state, message.current_target)
