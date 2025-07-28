a49edf18876e32429bdcfb9306e891330413c7b0
spencer <spencer.taylor-brown@ethereum.org>
2025-07-28 14:20:34 +0100

    chore(docs): add benchmark marker to test case reference (#1960)
    * chore(docs): add benchmark marker to test case ref.
    
    * chore(docs): update benchmark conftest instead.

--- benchmark/conftest.py
+++ benchmark/conftest.py
@@ -7,26 +7,33 @@ import pytest
 
 def pytest_collection_modifyitems(config, items):
     """Add the `benchmark` marker to all tests under `./tests/benchmark`."""
+    benchmark_dir = Path(__file__).parent
+    benchmark_marker = pytest.mark.benchmark
+    gen_docs = config.getoption("--gen-docs", default=False)
+
+    if gen_docs:
+        for item in items:
+            if benchmark_dir in Path(item.fspath).parents and not item.get_closest_marker(
+                "benchmark"
+            ):
+                item.add_marker(benchmark_marker)
+        return
+
     marker_expr = config.getoption("-m", default="")
-    gas_benchmark_values = config.getoption("--gas-benchmark-values", default=None)
-    run_benchmarks = marker_expr and (
-        "benchmark" in marker_expr and "not benchmark" not in marker_expr
-    )
-    if gas_benchmark_values:
-        run_benchmarks = True
+    run_benchmarks = (
+        marker_expr and "benchmark" in marker_expr and "not benchmark" not in marker_expr
+    ) or config.getoption("--gas-benchmark-values", default=None)
+
     items_for_removal = []
     for i, item in enumerate(items):
-        is_in_benchmark_dir = Path(__file__).parent in Path(item.fspath).parents
-        has_benchmark_marker = item.get_closest_marker("benchmark") is not None
-        is_benchmark_test = is_in_benchmark_dir or has_benchmark_marker
-        if is_benchmark_test:
-            if is_in_benchmark_dir and not has_benchmark_marker:
-                benchmark_marker = pytest.mark.benchmark
+        is_in_benchmark_dir = benchmark_dir in Path(item.fspath).parents
+        is_benchmark_test = is_in_benchmark_dir or item.get_closest_marker("benchmark")
 
+        if is_benchmark_test:
+            if is_in_benchmark_dir and not item.get_closest_marker("benchmark"):
                 item.add_marker(benchmark_marker)
             if not run_benchmarks:
                 items_for_removal.append(i)
-
         elif run_benchmarks:
             items_for_removal.append(i)
 
