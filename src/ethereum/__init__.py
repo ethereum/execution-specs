0a92be7c2b10a831f62cf882c5c11ac9f337c099
Sam Wilson <sam@binarycake.ca>
2025-07-28 12:44:06 -0400

    Bump version to v2.18.0rc3.dev1

--- src/ethereum/__init__.py
+++ src/ethereum/__init__.py
@@ -18,7 +18,7 @@ possible, to aid in defining the behavior of Ethereum clients.
 """
 import sys
 
-__version__ = "2.18.0rc2"
+__version__ = "2.18.0rc3.dev1"
 
 #
 #  Ensure we can reach 1024 frames of recursion
