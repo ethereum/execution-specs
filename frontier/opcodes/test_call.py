915e175c7ece2e83f57e3f3492005b051b44460e
Jeremías Salomón <48994069+JereSalo@users.noreply.github.com>
2025-08-05 17:53:39 -0300

    feat(test): add test for memory expansion on early revert in CALL (#1967)
    * add initial test version
    
    * fix test and add comments

--- frontier/opcodes/test_call.py
+++ frontier/opcodes/test_call.py
@@ -75,3 +75,75 @@ def test_call_large_offset_mstore(
             )
         },
     )
+
+
+# TODO: There's an issue with gas definitions on forks previous to Berlin, remove this when fixed.
+# https://github.com/ethereum/execution-spec-tests/pull/1952#discussion_r2237634275
+@pytest.mark.valid_from("Berlin")
+def test_call_memory_expands_on_early_revert(
+    state_test: StateTestFiller,
+    pre: Alloc,
+    fork: Fork,
+):
+    """
+    When CALL reverts early (e.g. because of not enough balance by the sender),
+    memory should be expanded anyway.
+    We check this with an MSTORE.
+
+    This is for a bug in an EVM implementation where memory is expanded after executing a CALL, but
+    not when an early revert happens.
+    """
+    sender = pre.fund_eoa()
+
+    gsc = fork.gas_costs()
+    ret_size = 128  # arbitrary number, greater than memory size to trigger an expansion
+
+    call_measure = CodeGasMeasure(
+        code=Op.CALL(gas=0, value=100, ret_size=ret_size),  # CALL with value
+        overhead_cost=gsc.G_VERY_LOW * len(Op.CALL.kwargs),  # Cost of pushing CALL args
+        extra_stack_items=1,  # Because CALL pushes 1 item to the stack
+        sstore_key=0,
+        stop=False,  # Because it's the first CodeGasMeasure
+    )
+    mstore_measure = CodeGasMeasure(
+        code=Op.MSTORE(offset=ret_size // 2, value=1),  # Low offset for not expanding memory
+        overhead_cost=gsc.G_VERY_LOW * len(Op.MSTORE.kwargs),  # Cost of pushing MSTORE args
+        extra_stack_items=0,
+        sstore_key=1,
+    )
+
+    # Contract without enough balance to send value transfer
+    contract = pre.deploy_contract(code=call_measure + mstore_measure, balance=0)
+
+    tx = Transaction(
+        gas_limit=500_000,
+        to=contract,
+        value=0,
+        sender=sender,
+    )
+
+    memory_expansion_gas_calc = fork.memory_expansion_gas_calculator()
+    # call cost: address_access_cost + new_acc_cost + memory_expansion_cost + value - stipend
+    call_cost = (
+        gsc.G_COLD_ACCOUNT_ACCESS
+        + gsc.G_NEW_ACCOUNT
+        + memory_expansion_gas_calc(new_bytes=ret_size)
+        + gsc.G_CALL_VALUE
+        - gsc.G_CALL_STIPEND
+    )
+
+    # mstore cost: base cost. No memory expansion cost needed, it was expanded on CALL.
+    mstore_cost = gsc.G_MEMORY
+    state_test(
+        env=Environment(),
+        pre=pre,
+        tx=tx,
+        post={
+            contract: Account(
+                storage={
+                    0: call_cost,
+                    1: mstore_cost,
+                },
+            )
+        },
+    )
