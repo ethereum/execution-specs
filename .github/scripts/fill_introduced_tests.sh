1e32a51f7439cb84c6a98b0dc9487ebcbb8d69b2
spencer-tb <spencer@spencertaylorbrown.uk>
2025-10-13 14:57:55 +0100

    chore(ci): update filepaths/locations.

--- .github/scripts/fill_introduced_tests.sh
+++ .github/scripts/fill_introduced_tests.sh
@@ -14,7 +14,7 @@ FILL_UNTIL="${4:-Cancun}"
 
 # Include basic evm operations into coverage by default
 # As when we translate from yul/solidity some dup/push opcodes could become untouched
-files="$CHANGED_TEST_FILES tests/homestead/coverage/test_coverage.py"
+files="$CHANGED_TEST_FILES tests/eest/homestead/coverage/test_coverage.py"
 
 uv run fill $files --clean --until=$FILL_UNTIL --evm-bin evmone-t8n --block-gas-limit $BLOCK_GAS_LIMIT -m "state_test or blockchain_test" --output $PATCH_TEST_PATH > >(tee -a filloutput.log) 2> >(tee -a filloutput.log >&2)
 
