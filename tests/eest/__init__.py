30d9b16e032fde5d099f724a15a36b9b87501ab6
kclowes <kclowes@users.noreply.github.com>
2025-10-20 12:30:20 -0600

    chore(tests): Move tests out of eest directory

--- tests/eest/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-Cross-client test cases organized by fork. Each directory underneath `tests/`
-contains test cases corresponding [to the fork](https://ethereum.org/en/history)
-in which the functionality-under-test was introduced.
-"""
