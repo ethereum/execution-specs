dd723c70f95e226ad3d57ff272bab31c97a1d79e
Mario Vega <marioevz@gmail.com>
2025-10-24 20:57:06 +0000

    fix(testing): Fix consume commands

--- packages/testing/src/execution_testing/cli/pytest_commands/consume.py
+++ packages/testing/src/execution_testing/cli/pytest_commands/consume.py
@@ -42,7 +42,7 @@ def create_consume_command(
 
 def get_command_logic_test_paths(command_name: str) -> List[Path]:
     """Determine the command paths based on the command name and hive flag."""
-    base_path = Path("execution_testing/cli/pytest_commands/plugins/consume")
+    base_path = Path("cli/pytest_commands/plugins/consume")
     if command_name in ["engine", "rlp"]:
         command_logic_test_paths = [
             base_path
