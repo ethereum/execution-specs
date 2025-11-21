bccccdfc88c3ff33301698e16e27ce00c932c980
fselmo <fselmo2@gmail.com>
2025-11-20 22:59:11 -0300

    chore(spec-specs): Add Amsterdam docstring; update prepare msg

--- src/ethereum/forks/amsterdam/__init__.py
+++ src/ethereum/forks/amsterdam/__init__.py
@@ -1,3 +1,14 @@
+"""
+The Amsterdam fork ([EIP-7773]) includes block-level access lists.
+
+### Changes
+
+- [EIP-7928: Block-Level Access Lists][EIP-7928]
+
+### Releases
+
+[EIP-7928]: https://eips.ethereum.org/EIPS/eip-7928
+"""  # noqa: E501
 
 from ethereum.fork_criteria import Unscheduled
 
