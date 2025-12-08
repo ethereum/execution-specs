5720c941034966661cfc12c84ac566cf273e71ee
spencer-tb <spencer.tb@ethereum.org>
2025-12-08 14:11:08 +0000

    chore(test-client-clis): re-add `NONCE_MISMATCH_TOO_LOW` mapping back for nethermind

--- packages/testing/src/execution_testing/client_clis/clis/nethermind.py
+++ packages/testing/src/execution_testing/client_clis/clis/nethermind.py
@@ -391,6 +391,7 @@ class NethermindExceptionMapper(ExceptionMapper):
         TransactionException.GAS_ALLOWANCE_EXCEEDED: "Block gas limit exceeded",
         TransactionException.NONCE_IS_MAX: "NonceTooHigh",
         TransactionException.INITCODE_SIZE_EXCEEDED: "max initcode size exceeded",
+        TransactionException.NONCE_MISMATCH_TOO_LOW: "wrong transaction nonce",
         TransactionException.NONCE_MISMATCH_TOO_HIGH: "wrong transaction nonce",
         TransactionException.INSUFFICIENT_MAX_FEE_PER_BLOB_GAS: (
             "InsufficientMaxFeePerBlobGas: Not enough to cover blob gas fee"
