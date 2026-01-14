b0ec9b6c3be4b1d67dcc51175f281b7d7a7e0763
Toni Wahrstätter <info@toniwahrstaetter.com>
2026-01-14 10:16:24 +0100

    revert receipt_gas_used

--- src/ethereum/forks/amsterdam/fork.py
+++ src/ethereum/forks/amsterdam/fork.py
@@ -981,11 +981,10 @@ def process_transaction(
         destroy_account(block_env.state, address)
 
     block_output.block_gas_used += tx_gas_used_before_refund
-    block_output.receipt_gas_used += tx_gas_used_after_refund
     block_output.blob_gas_used += tx_blob_gas_used
 
     receipt = make_receipt(
-        tx, tx_output.error, block_output.receipt_gas_used, tx_output.logs
+        tx, tx_output.error, block_output.block_gas_used, tx_output.logs
     )
 
     receipt_key = rlp.encode(Uint(index))
