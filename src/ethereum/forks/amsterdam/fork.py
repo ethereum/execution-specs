fb0f1f24ca04d4287968c695c0796d2e5504d45f
Toni Wahrstätter <info@toniwahrstaetter.com>
2026-01-15 11:43:15 +0100

    make sure that the calldata floor cost overwrites the tx gas before refund

--- src/ethereum/forks/amsterdam/fork.py
+++ src/ethereum/forks/amsterdam/fork.py
@@ -980,7 +980,9 @@ def process_transaction(
     for address in tx_output.accounts_to_delete:
         destroy_account(block_env.state, address)
 
-    block_output.block_gas_used += tx_gas_used_before_refund
+    block_output.block_gas_used += max(
+        tx_gas_used_before_refund, calldata_floor_gas_cost
+    )
     block_output.blob_gas_used += tx_blob_gas_used
 
     receipt = make_receipt(
