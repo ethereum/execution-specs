0e8ffbd49cc7eb8ad22e28643515e9452c82a9a8
Mario Vega <marioevz@gmail.com>
2026-01-22 00:12:53 +0000

    revert removal of docstring (prob. mistake)

--- packages/testing/src/execution_testing/specs/blockchain.py
+++ packages/testing/src/execution_testing/specs/blockchain.py
@@ -479,6 +479,10 @@ class BlockchainTest(BaseTest):
     genesis_environment: Environment = Field(default_factory=Environment)
     chain_id: int = 1
     exclude_full_post_state_in_output: bool = False
+    """
+    Exclude the post state from the fixture output. In this case, the state
+    verification is only performed based on the state root.
+    """
 
     supported_fixture_formats: ClassVar[
         Sequence[FixtureFormat | LabeledFixtureFormat]
