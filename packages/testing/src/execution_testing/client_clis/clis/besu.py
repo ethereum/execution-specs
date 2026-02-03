90a1f927f38a44649c9aef7d45cfd5273022a316
fselmo <fselmo2@gmail.com>
2026-02-03 14:48:35 -0700

    fix(test): lint

--- packages/testing/src/execution_testing/client_clis/clis/besu.py
+++ packages/testing/src/execution_testing/client_clis/clis/besu.py
@@ -236,9 +236,7 @@ class BesuExceptionMapper(ExceptionMapper):
         TransactionException.INSUFFICIENT_MAX_FEE_PER_GAS: (
             "transaction invalid gasPrice is less than the current BaseFee"
         ),
-        BlockException.GAS_USED_OVERFLOW: (
-            "provided gas insufficient"
-        ),
+        BlockException.GAS_USED_OVERFLOW: "provided gas insufficient",
         TransactionException.GAS_ALLOWANCE_EXCEEDED: (
             "provided gas insufficient"
         ),
