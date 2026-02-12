da3ebeffb3c08728836b88739560827a9ecdafcd
LouisTsai <q1030176@gmail.com>
2026-02-12 22:58:29 +0800

    fix: apply changes

--- tests/benchmark/compute/precompile/test_alt_bn128.py
+++ tests/benchmark/compute/precompile/test_alt_bn128.py
@@ -423,10 +423,11 @@ def test_bn128_pairings_amortized(
     tx_gas_limit: int,
 ) -> None:
     """Test running a block with as many BN128 pairings as possible."""
-    base_cost = 45_000
-    pairing_cost = 34_000
     size_per_pairing = 192
 
+    gsc = fork.gas_costs()
+    base_cost = gsc.G_PRECOMPILE_ECPAIRING_BASE
+    pairing_cost = gsc.G_PRECOMPILE_ECPAIRING_PER_POINT
     intrinsic_gas_calculator = fork.transaction_intrinsic_cost_calculator()
     mem_exp_gas_calculator = fork.memory_expansion_gas_calculator()
     warm_account_access_cost = Op.STATICCALL(
