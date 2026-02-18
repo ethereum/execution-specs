08f95774ad8348aa02cfa8d3f9a205b9e8b411fa
Sam Wilson <sam@binarycake.ca>
2026-02-18 09:38:22 -0500

    chore(specs): update bpo2 fork table

--- src/ethereum/forks/bpo2/__init__.py
+++ src/ethereum/forks/bpo2/__init__.py
@@ -1,6 +1,6 @@
 """
-The second blob parameter only (BPO) fork, BPO2, includes only changes to the
-blob fee schedule.
+The second blob parameter only (BPO) fork, BPO2 ([EIP-8135]), includes only
+changes to the blob fee schedule.
 
 ### Changes
 
@@ -10,11 +10,12 @@ blob fee schedule.
 
 | Network | Timestamp    | Date & Time (UTC)       | Fork Hash    | Beacon Chain Epoch |
 |---------|--------------|-------------------------|--------------|--------------------|
-| Holesky | `          ` |                         | `          ` | `      `           |
-| Sepolia | `          ` |                         | `          ` | `      `           |
-| Hoodi   | `          ` |                         | `          ` |  `     `           |
-| Mainnet | `1767747671` | 2025-12-11 21:15:54     | `0x07c9462e` | `      `           |
+| Holesky | `1760389824` | 2025-10-13 21:10:24     | `0x9bc6cb31` | `167936`           |
+| Sepolia | `1761607008` | 2025-10-27 23:16:48     | `0x268956b6` | `275712`           |
+| Hoodi   | `1762955544` | 2025-11-12 13:52:24     | `0x23aa1351` |  `54016`           |
+| Mainnet | `1767747671` | 2026-01-07 01:01:11     | `0x07c9462e` | `419072`           |
 
+[EIP-8135]: https://eips.ethereum.org/EIPS/eip-8135
 [EIP-7892]: https://eips.ethereum.org/EIPS/eip-7892
 """  # noqa: E501
 
