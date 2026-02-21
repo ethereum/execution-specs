09bd3470bad129011f6c113abca4a76ed37698a8
kevaundray <kevtheappdev@gmail.com>
2026-02-21 21:13:33 +0000

    fix(zkevm): Fix linting errors (#2269)
    * fix
    
    * tox

--- src/ethereum/forks/amsterdam/execution_engine/types.py
+++ src/ethereum/forks/amsterdam/execution_engine/types.py
@@ -10,10 +10,11 @@ from ethereum_types.frozen import slotted_freezable
 from ethereum_types.numeric import U64, U256, Uint
 
 from ethereum.crypto.hash import Hash32
+from ethereum.state import Address, Root
 
 from ..blocks import Withdrawal
 from ..fork import BlockChain
-from ..fork_types import Address, Bloom, Root, VersionedHash
+from ..fork_types import Bloom, VersionedHash
 from ..transactions import LegacyTransaction
 
 # In this module, the execution engine is the chain/state container used by
