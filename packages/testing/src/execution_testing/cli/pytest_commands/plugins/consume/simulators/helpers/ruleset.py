77bef4c1f8d3f7c9d7a8ff51959dc7a8beb178ff
Shoham Chakraborty <shhmchk@gmail.com>
2026-02-25 11:17:41 +0000

    missing fork

--- packages/testing/src/execution_testing/cli/pytest_commands/plugins/consume/simulators/helpers/ruleset.py
+++ packages/testing/src/execution_testing/cli/pytest_commands/plugins/consume/simulators/helpers/ruleset.py
@@ -490,6 +490,7 @@ ruleset: Dict[Fork, Dict[str, int]] = {
         "HIVE_FORK_PETERSBURG": 0,
         "HIVE_FORK_ISTANBUL": 0,
         "HIVE_FORK_BERLIN": 0,
+        "HIVE_FORK_LONDON": 0,
         "HIVE_FORK_MERGE": 0,
         "HIVE_TERMINAL_TOTAL_DIFFICULTY": 0,
         "HIVE_SHANGHAI_TIMESTAMP": 0,
