108672217f111834bf5d62324706d6ff3e958c37
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-03-01 17:34:28 +0000

    ignore bigmem tests

--- tests/osaka/eip7825_transaction_gas_limit_cap/test_tx_gas_limit.py
+++ tests/osaka/eip7825_transaction_gas_limit_cap/test_tx_gas_limit.py
@@ -331,6 +331,7 @@ def total_cost_floor_per_token(fork: Fork) -> int:
     return gas_costs.G_TX_DATA_FLOOR_TOKEN_COST
 
 
+@pytest.mark.bigmem
 @pytest.mark.xdist_group(name="bigmem")
 @pytest.mark.parametrize(
     "exceed_tx_gas_limit,correct_intrinsic_cost_in_transaction_gas_limit",
