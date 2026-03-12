64592b033d177a962faf85e14905402b13d9d510
Ignacio Hagopian <jsign.uy@gmail.com>
2026-03-12 09:31:04 -0300

    zkevm/tests: bytecode call variants dedup and revert

--- tests/amsterdam/eip8025_optional_proofs/test_witness_bytecodes_call_variants.py
+++ tests/amsterdam/eip8025_optional_proofs/test_witness_bytecodes_call_variants.py
@@ -111,3 +111,125 @@ def test_witness_codes_nested_calls(
             sender: Account(nonce=1),
         },
     )
+
+
+def test_witness_codes_dedup_identical_bytecode(
+    pre: Alloc,
+    blockchain_test: BlockchainTestFiller,
+) -> None:
+    """
+    Two pre-state contracts with identical bytecode, both CALLed.
+
+    Only one copy of the bytecode should appear in executionWitness.codes
+    because get_witness_codes() deduplicates by code hash.
+    """
+    shared_code = Op.PUSH1(0x42) + Op.POP + Op.STOP
+    contract_a = pre.deploy_contract(code=shared_code)
+    contract_b = pre.deploy_contract(code=shared_code)
+
+    caller_code = (
+        Op.CALL(Op.GAS, contract_a, 0, 0, 0, 0, 0)
+        + Op.POP
+        + Op.CALL(Op.GAS, contract_b, 0, 0, 0, 0, 0)
+        + Op.POP
+        + Op.STOP
+    )
+    caller = pre.deploy_contract(code=caller_code)
+
+    sender = pre.fund_eoa()
+    tx = Transaction(sender=sender, to=caller, gas_limit=500_000)
+
+    blockchain_test(
+        pre=pre,
+        blocks=[
+            Block(
+                txs=[tx],
+                expected_execution_witness_codes=(
+                    ExecutionWitnessCodesExpectation(
+                        codes_present=[Bytes(bytes(shared_code))],
+                    )
+                ),
+            )
+        ],
+        post={
+            sender: Account(nonce=1),
+        },
+    )
+
+
+def test_witness_codes_reverted_transaction(
+    pre: Alloc,
+    blockchain_test: BlockchainTestFiller,
+) -> None:
+    """
+    Transaction that fully reverts still records accessed code.
+    """
+    target_code = Op.SSTORE(0, 1) + Op.REVERT(0, 0)
+    target = pre.deploy_contract(code=target_code)
+
+    sender = pre.fund_eoa()
+    tx = Transaction(sender=sender, to=target, gas_limit=500_000)
+
+    blockchain_test(
+        pre=pre,
+        blocks=[
+            Block(
+                txs=[tx],
+                expected_execution_witness_codes=(
+                    ExecutionWitnessCodesExpectation(
+                        codes_present=[Bytes(bytes(target_code))],
+                    )
+                ),
+            )
+        ],
+        post={
+            sender: Account(nonce=1),
+            target: Account(storage={0: 0}),
+        },
+    )
+
+
+def test_witness_codes_reverted_inner_call(
+    pre: Alloc,
+    blockchain_test: BlockchainTestFiller,
+) -> None:
+    """
+    Inner CALL that reverts while outer transaction succeeds.
+
+    The reverted callee's code must still be in executionWitness.codes.
+    """
+    callee_code = Op.SSTORE(0, 1) + Op.REVERT(0, 0)
+    callee = pre.deploy_contract(code=callee_code)
+
+    caller_code = (
+        Op.CALL(Op.GAS, callee, 0, 0, 0, 0, 0)
+        + Op.POP
+        + Op.SSTORE(0, 1)
+        + Op.STOP
+    )
+    caller = pre.deploy_contract(code=caller_code)
+
+    sender = pre.fund_eoa()
+    tx = Transaction(sender=sender, to=caller, gas_limit=500_000)
+
+    blockchain_test(
+        pre=pre,
+        blocks=[
+            Block(
+                txs=[tx],
+                expected_execution_witness_codes=(
+                    ExecutionWitnessCodesExpectation(
+                        codes_present=[
+                            Bytes(bytes(caller_code)),
+                            Bytes(bytes(callee_code)),
+                        ],
+                    )
+                ),
+            )
+        ],
+        post={
+            sender: Account(nonce=1),
+            callee: Account(storage={0: 0}), # Check reverted storage change
+            caller: Account(storage={0: 1}),
+        },
+    )
