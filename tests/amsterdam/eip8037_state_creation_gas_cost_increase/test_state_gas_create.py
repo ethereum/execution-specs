8669d37636756cd3cbbd61372d54e6a04437c07a
Felix H <felix314159@users.noreply.github.com>
2026-03-19 12:27:25 +0100

    fix: ruff

--- tests/amsterdam/eip8037_state_creation_gas_cost_increase/test_state_gas_create.py
+++ tests/amsterdam/eip8037_state_creation_gas_cost_increase/test_state_gas_create.py
@@ -526,9 +526,10 @@ def test_max_initcode_size_gas_metering_via_create(
         + initcode.execution_gas(fork)
         + initcode.deployment_gas(fork)
     )
-    factory_state_gas = fork.create_state_gas(
-        code_size=len(initcode.deploy_code)
-    ) + fork.sstore_state_gas()
+    factory_state_gas = (
+        fork.create_state_gas(code_size=len(initcode.deploy_code))
+        + fork.sstore_state_gas()
+    )
     factory_regular_gas = factory_gas - factory_state_gas
 
     caller = pre.deploy_contract(
