5b8eca186f528d17a3c99ce74ca2fb05d83e2029
carsons-eels <carson@ethereum.org>
2026-03-21 00:27:07 -0400

    refactor: shanghai conversion to encapsulate Gas Constants in class

--- src/ethereum/forks/shanghai/vm/instructions/comparison.py
+++ src/ethereum/forks/shanghai/vm/instructions/comparison.py
@@ -13,10 +13,8 @@ Implementations of the EVM Comparison instructions.
 
 from ethereum_types.numeric import U256, Uint
 
-from .. import Evm, gas
-from ..gas import (
-    charge_gas,
-)
+from .. import Evm
+from ..gas import GasCosts, charge_gas
 from ..stack import pop, push
 
 
@@ -36,7 +34,7 @@ def less_than(evm: Evm) -> None:
     right = pop(evm.stack)
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_LT)
+    charge_gas(evm, GasCosts.GAS_OPCODE_LT)
 
     # OPERATION
     result = U256(left < right)
@@ -62,7 +60,7 @@ def signed_less_than(evm: Evm) -> None:
     right = pop(evm.stack).to_signed()
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_SLT)
+    charge_gas(evm, GasCosts.GAS_OPCODE_SLT)
 
     # OPERATION
     result = U256(left < right)
@@ -89,7 +87,7 @@ def greater_than(evm: Evm) -> None:
     right = pop(evm.stack)
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_GT)
+    charge_gas(evm, GasCosts.GAS_OPCODE_GT)
 
     # OPERATION
     result = U256(left > right)
@@ -115,7 +113,7 @@ def signed_greater_than(evm: Evm) -> None:
     right = pop(evm.stack).to_signed()
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_SGT)
+    charge_gas(evm, GasCosts.GAS_OPCODE_SGT)
 
     # OPERATION
     result = U256(left > right)
@@ -142,7 +140,7 @@ def equal(evm: Evm) -> None:
     right = pop(evm.stack)
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_EQ)
+    charge_gas(evm, GasCosts.GAS_OPCODE_EQ)
 
     # OPERATION
     result = U256(left == right)
@@ -168,7 +166,7 @@ def is_zero(evm: Evm) -> None:
     x = pop(evm.stack)
 
     # GAS
-    charge_gas(evm, gas.GAS_OPCODE_ISZERO)
+    charge_gas(evm, GasCosts.GAS_OPCODE_ISZERO)
 
     # OPERATION
     result = U256(x == 0)
