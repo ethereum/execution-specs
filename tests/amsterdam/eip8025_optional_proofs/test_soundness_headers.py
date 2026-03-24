84ded05c2591a201179efd5d5e28dcbfa97518b6
jsign <jsign.uy@gmail.com>
2026-03-23 21:13:36 -0300

    more header tests

--- tests/amsterdam/eip8025_optional_proofs/test_soundness_headers.py
+++ tests/amsterdam/eip8025_optional_proofs/test_soundness_headers.py
@@ -6,12 +6,15 @@ from execution_testing import (
     Alloc,
     Block,
     BlockchainTestFiller,
+    Bytes,
     ExecutionWitnessHeadersExpectation,
     Op,
     Transaction,
 )
 from execution_testing.test_types.execution_witness.modifiers import (
+    clear_headers,
     remove_header_at,
+    replace_header_at,
     reverse_headers,
 )
 
@@ -105,6 +108,60 @@ def test_soundness_headers_non_contiguous_chain(
                 ExecutionWitnessHeadersExpectation(
                     expected_count=offset,
                 ).modify(reverse_headers())
+            ),
+            expected_stateless_validation_success=False,
+        )
+    )
+
+    blockchain_test(
+        pre=pre,
+        blocks=blocks,
+        post={sender: Account(nonce=1)},
+    )
+
+
+def test_soundness_headers_empty_block_missing_mandatory_parent(
+    pre: Alloc,
+    blockchain_test: BlockchainTestFiller,
+) -> None:
+    """Removing the mandatory parent header from an empty block should fail."""
+    blockchain_test(
+        pre=pre,
+        blocks=[
+            Block(
+                txs=[],
+                expected_execution_witness_headers=(
+                    ExecutionWitnessHeadersExpectation(
+                        expected_count=1,
+                    ).modify(clear_headers())
+                ),
+                expected_stateless_validation_success=False,
+            )
+        ],
+        post={},
+    )
+
+
+def test_soundness_headers_malformed_rlp_header(
+    pre: Alloc,
+    blockchain_test: BlockchainTestFiller,
+) -> None:
+    """Replacing a required header with malformed RLP should fail."""
+    offset = 5
+    contract = pre.deploy_contract(
+        code=Op.BLOCKHASH(Op.SUB(Op.NUMBER, offset)) + Op.POP + Op.STOP
+    )
+    sender = pre.fund_eoa()
+    tx = Transaction(sender=sender, to=contract, gas_limit=500_000)
+
+    blocks = [Block(txs=[]) for _ in range(offset)]
+    blocks.append(
+        Block(
+            txs=[tx],
+            expected_execution_witness_headers=(
+                ExecutionWitnessHeadersExpectation(
+                    expected_count=offset,
+                ).modify(replace_header_at(-1, Bytes(b"\xff")))
             ),
             expected_stateless_validation_success=False,
         )
