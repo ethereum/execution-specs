6699806f6eb42ae192e79468cbcf961aaa98156f
kclowes <kclowes@users.noreply.github.com>
2026-04-03 12:34:51 -0600

    Bump gas for test_sstore_gas_left.py

--- tests/ported_static/stSStoreTest/test_sstore_gas_left.py
+++ tests/ported_static/stSStoreTest/test_sstore_gas_left.py
@@ -109,7 +109,7 @@ def test_sstore_gas_left(
         timestamp=1000,
         prev_randao=0x20000,
         base_fee_per_gas=10,
-        gas_limit=10000000,
+        gas_limit=100000000,
     )
 
     pre[sender] = Account(balance=0xE8D4A51000)
@@ -392,7 +392,7 @@ def test_sstore_gas_left(
         + Op.JUMPDEST
         + Op.STOP,
     ]
-    tx_gas = [200000]
+    tx_gas = [20000000]
     tx_value = [1]
 
     tx = Transaction(
