c5a49f156010fb1697047ff8e78ec58aa12a3db5
danceratopz <danceratopz@gmail.com>
2026-04-10 16:38:45 +0200

    chore(test-config): fix-up formatting

--- packages/testing/src/execution_testing/config/docs.py
+++ packages/testing/src/execution_testing/config/docs.py
@@ -21,6 +21,4 @@ class DocsConfig(BaseModel):
 
     # Documentation URLs prefixed with `DOCS_URL__` to avoid conflicts
     # with other URLs
-    DOCS_URL__WRITING_TESTS: str = (
-        f"{DOCS_BASE_URL}/writing_tests/"
-    )
+    DOCS_URL__WRITING_TESTS: str = f"{DOCS_BASE_URL}/writing_tests/"
