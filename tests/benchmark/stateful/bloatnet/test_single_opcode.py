c339b3b73ba43a96c5551f047c283bdcfcc2475c
CPerezz <37264926+CPerezz@users.noreply.github.com>
2026-04-30 13:09:33 +0200

    fix(test-benchmark): disable cache strats except NO_CACHE for now (#2786)

--- tests/benchmark/stateful/bloatnet/test_single_opcode.py
+++ tests/benchmark/stateful/bloatnet/test_single_opcode.py
@@ -197,7 +197,7 @@ def run_bloated_eoa_benchmark(
 @pytest.mark.repricing
 @pytest.mark.stub_parametrize("token_name", "bloated_eoa_")
 @pytest.mark.parametrize("existing_slots", [False, True])
-@pytest.mark.parametrize("cache_strategy", list(CacheStrategy))
+@pytest.mark.parametrize("cache_strategy", [CacheStrategy.NO_CACHE])
 def test_sload_bloated(
     benchmark_test: BenchmarkTestFiller,
     pre: Alloc,
@@ -578,7 +578,7 @@ def test_sload_bloated_multi_contract(
 @pytest.mark.stub_parametrize("token_name", "bloated_eoa_")
 @pytest.mark.parametrize("write_new_value", [False, True])
 @pytest.mark.parametrize("existing_slots", [True, False])
-@pytest.mark.parametrize("cache_strategy", list(CacheStrategy))
+@pytest.mark.parametrize("cache_strategy", [CacheStrategy.NO_CACHE])
 def test_sstore_bloated(
     benchmark_test: BenchmarkTestFiller,
     pre: Alloc,
@@ -1510,7 +1510,7 @@ class AccountMode(Enum):
 
 
 @pytest.mark.repricing
-@pytest.mark.parametrize("cache_strategy", list(CacheStrategy))
+@pytest.mark.parametrize("cache_strategy", [CacheStrategy.NO_CACHE])
 @pytest.mark.parametrize(
     "opcode,value_sent,account_mode", account_access_params()
 )
