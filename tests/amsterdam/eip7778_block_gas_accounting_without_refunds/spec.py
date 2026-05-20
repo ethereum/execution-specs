76f336f440c8cf005877d7b1d17390789fcf7bab
felix <felix314159@users.noreply.github.com>
2026-05-20 14:02:52 +0200

    chore(tests): create `spec.py` for EIP-7778 (#2887)

---
+++ tests/amsterdam/eip7778_block_gas_accounting_without_refunds/spec.py
@@ -0,0 +1,24 @@
+"""Defines EIP-7778 specification constants and functions."""
+
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class ReferenceSpec:
+    """Defines the reference spec version and git path."""
+
+    git_path: str
+    version: str
+
+
+ref_spec_7778 = ReferenceSpec(
+    "EIPS/eip-7778.md", "ce17d00b8341032a946301944124c4a6013032d6"
+)
+
+
+@dataclass(frozen=True)
+class Spec:
+    """
+    Parameters from the EIP-7778 specifications as defined at
+    https://eips.ethereum.org/EIPS/eip-7778.
+    """
