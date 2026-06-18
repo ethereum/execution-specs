f3aa7b7b3fb7c7da4aff2e9d6b2967e6f434bc59
marioevz <marioevz@gmail.com>
2026-06-18 14:59:12 -0600

    refactor(test-forks): Use pkgutil to load contract binaries

--- packages/testing/src/execution_testing/forks/forks/eips/prague/eip_7251.py
+++ packages/testing/src/execution_testing/forks/forks/eips/prague/eip_7251.py
@@ -6,21 +6,19 @@ Allow validators to consolidate via execution layer requests.
 https://eips.ethereum.org/EIPS/eip-7251
 """
 
-from os.path import realpath
-from pathlib import Path
 from typing import List, Mapping
 
 from execution_testing.base_types import Address
 
 from ....base_fork import BaseFork
+from ....bytecode import load_contract_bytecode
 
-BYTECODE_FILE = (
-    Path(realpath(__file__)).parent / "contracts" / "consolidation_request.bin"
-)
 CONSOLIDATION_REQUEST_PREDEPLOY_ADDRESS = (
     0x0000BBDDC7CE488642FB579F8B00F3A590007251
 )
-CONSOLIDATION_REQUEST_PREDEPLOY_BYTECODE = BYTECODE_FILE.read_bytes()
+CONSOLIDATION_REQUEST_PREDEPLOY_BYTECODE = load_contract_bytecode(
+    __name__, "consolidation_request.bin"
+)
 
 
 class EIP7251(BaseFork):
