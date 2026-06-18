4b13b2a7439ebfb6164082d3b6cdfa423f9d18bd
Mario Vega <marioevz@gmail.com>
2026-06-18 15:54:11 -0600

    Update tests/amsterdam/eip8282_builder_execution_requests/__init__.py
    Co-authored-by: Sam Wilson <57262657+SamWilsn@users.noreply.github.com>

--- tests/amsterdam/eip8282_builder_execution_requests/__init__.py
+++ tests/amsterdam/eip8282_builder_execution_requests/__init__.py
@@ -1,4 +1,5 @@
 """
-Test cases for
-[EIP-8282: Builder Execution Requests](https://eips.ethereum.org/EIPS/eip-8282).
+Test cases for [EIP-8282: Builder Execution Requests][EIP-8282].
+
+[EIP-8282]: https://eips.ethereum.org/EIPS/eip-8282
 """
