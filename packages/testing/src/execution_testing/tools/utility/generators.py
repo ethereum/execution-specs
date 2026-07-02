60a2c7ce4bcce19ef48696b98b08e6bc3537f62c
marioevz <marioevz@gmail.com>
2026-07-02 16:05:09 -0600

    fix(test-tools): Lint

--- packages/testing/src/execution_testing/tools/utility/generators.py
+++ packages/testing/src/execution_testing/tools/utility/generators.py
@@ -12,7 +12,6 @@ from execution_testing.base_types import Account, Address, Hash
 from execution_testing.exceptions import BlockException
 from execution_testing.forks import Berlin, Fork, TransitionFork
 from execution_testing.forks.base_fork import BaseFork
-from execution_testing.recipient_type import RecipientType
 from execution_testing.specs import BlockchainTestFiller, StateTestFiller
 from execution_testing.specs.blockchain import Block
 from execution_testing.test_types import Alloc, Transaction
