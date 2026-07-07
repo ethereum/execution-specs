a37d71bb2eba0f82ac63e21427e31afd2629fa55
marioevz <marioevz@gmail.com>
2026-07-06 19:25:54 -0600

    Update EIP-8282 contracts addresses

--- packages/testing/src/execution_testing/forks/forks/eips/amsterdam/eip_8282.py
+++ packages/testing/src/execution_testing/forks/forks/eips/amsterdam/eip_8282.py
@@ -14,12 +14,12 @@ from execution_testing.base_types import Address
 from ....base_fork import BaseFork
 from ....bytecode import load_contract_bytecode
 
-BUILDER_DEPOSIT_CONTRACT_ADDRESS = 0x00006AE84ED173D4394DE5E28F9ED56B28008282
+BUILDER_DEPOSIT_CONTRACT_ADDRESS = 0x82fd414bcC3d584692B631FdfF9fADc1dFE4dddd
 BUILDER_DEPOSIT_CONTRACT_BYTECODE = load_contract_bytecode(
     __name__, "builder_deposit_request.bin"
 )
 
-BUILDER_EXIT_CONTRACT_ADDRESS = 0x000014574A74C805590AFF9499FC7A690F008282
+BUILDER_EXIT_CONTRACT_ADDRESS = 0x82901077fe13BeCc6F5B2e84Cae8072751ADeEEe
 BUILDER_EXIT_CONTRACT_BYTECODE = load_contract_bytecode(
     __name__, "builder_exit_request.bin"
 )
