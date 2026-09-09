2c74fc1aee0acf479ede053710eaab5d8413b0d9
fselmo <fselmo2@gmail.com>
2026-09-09 15:09:49 -0600

    fix(test-t8n): run the pre-execution system calls in the spec's order

--- packages/testing/src/execution_testing/evm_tools/t8n/__init__.py
+++ packages/testing/src/execution_testing/evm_tools/t8n/__init__.py
@@ -370,18 +370,19 @@ class T8N(Load):
         )
 
     def _run_blockchain_test(self, block_env: Any, block_output: Any) -> None:
-        if self.fork.has_compute_requests_hash:
-            self.fork.process_unchecked_system_transaction(
-                block_env=block_env,
-                target_address=self.fork.HISTORY_STORAGE_ADDRESS,
-                data=block_env.block_hashes[-1],  # The parent hash
-            )
-
+        # Same order as the fork's `apply_body`: beacon roots, then history.
         if self.fork.has_beacon_roots_address:
             self.fork.process_unchecked_system_transaction(
                 block_env=block_env,
                 target_address=self.fork.BEACON_ROOTS_ADDRESS,
                 data=block_env.parent_beacon_block_root,
+            )
+
+        if self.fork.has_compute_requests_hash:
+            self.fork.process_unchecked_system_transaction(
+                block_env=block_env,
+                target_address=self.fork.HISTORY_STORAGE_ADDRESS,
+                data=block_env.block_hashes[-1],  # The parent hash
             )
 
         self._process_txs(block_env, block_output)
